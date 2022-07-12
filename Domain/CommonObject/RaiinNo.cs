0789ea5558d3e81fa73e430e8b5a00f71d2fc97a
DuongLe <duong.le@sotatek.com>
2022-07-12 10:37:16 +0700

    Add common value object

---
+++ Domain/CommonObject/RaiinNo.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.CommonObject
+{
+    public class RaiinNo : ValueObject
+    {
+        private readonly long _value;
+
+        public long Value => _value;
+
+        private RaiinNo(long value)
+        {
+            _value = value;
+        }
+
+        public static RaiinNo From(long value)
+        {
+            return new RaiinNo(value);
+        }
+    }
+}
---
+++ Domain/CommonObject/SinDate.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.CommonObject
+{
+    public class SinDate : ValueObject
+    {
+        private readonly int _value;
+
+        public int Value => _value;
+
+        private SinDate(int value)
+        {
+            _value = value;
+        }
+
+        public static SinDate From(int value)
+        {
+            return new SinDate(value);
+        }
+    }
+}
