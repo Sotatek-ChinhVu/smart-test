3cf7f15db094a410366c880841fd93551ce6fd81
DuongLe <duong.le@sotatek.com>
2022-07-13 17:10:08 +0700

    Add message and status code for get userlist api

--- EmrCloudApi/Tenant/Constants/ResponseMessage.cs
+++ EmrCloudApi/Tenant/Constants/ResponseMessage.cs
@@ -4,5 +4,7 @@
     {
         public static readonly string CreateUserInvalidName = "Please input user name";
         public static readonly string CreateUserSuccessed = "User created!!!";
+
+        public static readonly string GetUserListSuccessed = "Get userList successed!!!";
     }
 }
