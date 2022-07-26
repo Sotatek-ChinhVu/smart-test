15cbdb11635aa5f7deb441c7f25dad5c5ee43715
DuongLe <duong.le@sotatek.com>
2022-07-26 15:47:00 +0700

    Fix warning output

--- Domain/Models/Reception/ReceptionRowModel.cs
+++ Domain/Models/Reception/ReceptionRowModel.cs
@@ -8,11 +8,11 @@ namespace Domain.Models.Reception;
 public class ReceptionRowModel
 {
     public ReceptionRowModel(long raiinNo, long parentRaiinNo, int uketukeNo, bool hasLockInf, int raiinStatus,
-        long ptNum, string kanaName, string name, int sex, int birthday, string? yoyakuTime,
-        string? rsvFrameName, string? uketukeSbtName, string? uketukeTime, string sinStartTime,
-        string? sinEndTime, string? kaikeiTime, string? raiinCmt, string? ptComment,
-        string? tantoName, string? kaName, int lastVisitDate, string? sname, string? raiinRemark,
-        int confirmationState, string? confirmationResult, List<int> grpIds, List<DynamicCell> dynamicCells,
+        long ptNum, string kanaName, string name, int sex, int birthday, string yoyakuTime,
+        string rsvFrameName, string uketukeSbtName, string uketukeTime, string sinStartTime,
+        string sinEndTime, string kaikeiTime, string raiinCmt, string ptComment,
+        string tantoName, string kaName, int lastVisitDate, string sname, string raiinRemark,
+        int confirmationState, string confirmationResult, List<int> grpIds, List<DynamicCell> dynamicCells,
         int sinDate, UserConfCommon.DateTimeFormart dateTimeFormart = UserConfCommon.DateTimeFormart.JapaneseCalendar)
     {
         RaiinNo = raiinNo;
@@ -26,23 +26,23 @@ public class ReceptionRowModel
         Sex = GetGenderText(sex);
         Birthday = GetFormattedBirthDay(birthday, dateTimeFormart);
         Age = CIUtil.SDateToDecodeAge(birthday.AsString(), sinDate.AsString());
-        YoyakuTime = yoyakuTime ?? string.Empty;
-        ReservationName = rsvFrameName ?? string.Empty;
-        UketukeSbtName = uketukeSbtName ?? string.Empty;
-        UketukeTime = uketukeTime ?? string.Empty;
+        YoyakuTime = yoyakuTime;
+        ReservationName = rsvFrameName;
+        UketukeSbtName = uketukeSbtName;
+        UketukeTime = uketukeTime;
         SinStartTime = sinStartTime;
-        SinEndTime = sinEndTime ?? string.Empty;
-        KaikeiTime = kaikeiTime ?? string.Empty;
-        RaiinCmt = raiinCmt ?? string.Empty;
-        PtComment = ptComment ?? string.Empty;
+        SinEndTime = sinEndTime;
+        KaikeiTime = kaikeiTime;
+        RaiinCmt = raiinCmt;
+        PtComment = ptComment;
         HokenPatternName = "TODO";
-        TantoName = tantoName ?? string.Empty;
-        KaName = kaName ?? string.Empty;
+        TantoName = tantoName;
+        KaName = kaName;
         LastVisitDate = CIUtil.SDateToShowSDate(lastVisitDate);
-        Sname = sname ?? string.Empty;
-        RaiinRemark = raiinRemark ?? string.Empty;
+        Sname = sname;
+        RaiinRemark = raiinRemark;
         ConfirmationState = GetConfirmationStateText(confirmationState);
-        ConfirmationResult = confirmationResult ?? string.Empty;
+        ConfirmationResult = confirmationResult;
         GrpIdToDynamicCell = grpIds.ToDictionary(
             grpId => grpId,
             grpId => dynamicCells.FirstOrDefault(c => c.GrpId == grpId, new DynamicCell(grpId)));
@@ -52,58 +52,58 @@ public class ReceptionRowModel
     // 順番
     public int UketukeNo { get; private set; }
     // 同一来院
-    public string SameVisit { get; private set; } = string.Empty;
+    public string SameVisit { get; private set; }
     // 状態
     public string Status { get; private set; }
     public string OriginalStatus { get; private set; }
     // 患者番号
     public long PtNum { get; private set; }
     // カナ氏名
-    public string KanaName { get; private set; } = string.Empty;
+    public string KanaName { get; private set; }
     // 氏名
-    public string Name { get; private set; } = string.Empty;
+    public string Name { get; private set; }
     // 性
     public string Sex { get; private set; }
     // 生年月日
     public string Birthday { get; private set; }
     // 年齢
-    public string Age { get; private set; } = string.Empty;
+    public string Age { get; private set; }
     // 読
     public bool IsNameDuplicate { get; private set; }
     // 予約時間
-    public string YoyakuTime { get; private set; } = string.Empty;
+    public string YoyakuTime { get; private set; }
     // 予約名
-    public string ReservationName { get; private set; } = string.Empty;
+    public string ReservationName { get; private set; }
     // 受付種別
-    public string UketukeSbtName { get; private set; } = string.Empty;
+    public string UketukeSbtName { get; private set; }
     // 受付時間
-    public string UketukeTime { get; private set; } = string.Empty;
+    public string UketukeTime { get; private set; }
     // 診察開始
-    public string SinStartTime { get; private set; } = string.Empty;
+    public string SinStartTime { get; private set; }
     // 診察終了
-    public string SinEndTime { get; private set; } = string.Empty;
+    public string SinEndTime { get; private set; }
     // 精算時間
-    public string KaikeiTime { get; private set; } = string.Empty;
+    public string KaikeiTime { get; private set; }
     // 来院コメント
-    public string RaiinCmt { get; private set; } = string.Empty;
+    public string RaiinCmt { get; private set; }
     // 患者コメント
-    public string PtComment { get; private set; } = string.Empty;
+    public string PtComment { get; private set; }
     // 保険
-    public string HokenPatternName { get; private set; } = string.Empty;
+    public string HokenPatternName { get; private set; }
     // 担当医
-    public string TantoName { get; private set; } = string.Empty;
+    public string TantoName { get; private set; }
     // 診療科
-    public string KaName { get; private set; } = string.Empty;
+    public string KaName { get; private set; }
     // 前回来院
     public string LastVisitDate { get; private set; }
     // 主治医
-    public string Sname { get; private set; } = string.Empty;
+    public string Sname { get; private set; }
     // 備考
-    public string RaiinRemark { get; private set; } = string.Empty;
+    public string RaiinRemark { get; private set; }
     // 資格確認状況
     public string ConfirmationState { get; private set; }
     // 資格確認結果
-    public string ConfirmationResult { get; private set; } = string.Empty;
+    public string ConfirmationResult { get; private set; }
     // Dynamic cells
     public Dictionary<int, DynamicCell> GrpIdToDynamicCell { get; private set; }
 
