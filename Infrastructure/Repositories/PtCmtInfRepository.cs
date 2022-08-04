8d580341305a0623afcf3c305ebadbc333040969
SotaTek-TuanNguyen3 <tuan.nguyen3@sotatek.com>
2022-08-04 09:18:19 +0700

    allow updating ptComment cell value

---
+++ Infrastructure/Repositories/PtCmtInfRepository.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Models.PtCmtInf;
+using Entity.Tenant;
+using Infrastructure.Interfaces;
+using PostgreDataContext;
+
+namespace Infrastructure.Repositories;
+
+public class PtCmtInfRepository : IPtCmtInfRepository
+{
+    private readonly TenantDataContext _tenantDataContext;
+
+    public PtCmtInfRepository(ITenantProvider tenantProvider)
+    {
+        _tenantDataContext = tenantProvider.GetTrackingTenantDataContext();
+    }
+
+    public void Upsert(long ptId, string text)
+    {
+        var ptCmt = _tenantDataContext.PtCmtInfs
+            .OrderByDescending(p => p.UpdateDate)
+            .FirstOrDefault(p => p.PtId == ptId);
+        if (ptCmt is null)
+        {
+            _tenantDataContext.PtCmtInfs.Add(new PtCmtInf
+            {
+                HpId = 1,
+                PtId = ptId,
+                Text = text
+            });
+        }
+        else
+        {
+            ptCmt.Text = text;
+            ptCmt.UpdateDate = DateTime.UtcNow;
+        }
+
+        _tenantDataContext.SaveChanges();
+    }
+}
