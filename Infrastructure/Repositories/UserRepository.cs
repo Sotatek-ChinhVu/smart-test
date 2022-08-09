4a90e13bc66178d56eb23cd7a1159243c98d95ab
hungdao2 <manhhungsky1996@gmail.com>
2022-08-09 14:14:01 +0700

    Update check code

--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Models.User;
+﻿using Domain.Constant;
+using Domain.Models.Insurance;
+using Domain.Models.User;
 using Entity.Tenant;
 using Helper.Constants;
 using Infrastructure.Interfaces;
@@ -46,7 +48,73 @@ namespace Infrastructure.Repositories
                 query = query.Where(u => u.JobCd == JobCodes.Doctor);
             }
 
+            //rain infor
+            var hpId = 1;
+            long ptId = 99999546;
+            var listDataRaiinInf = _tenantDataContext.RaiinInfs.Where(x => x.HpId == hpId && x.PtId == ptId && x.SinDate == sinDate && x.IsDeleted == DeleteTypes.None).ToList();
+
+            var _doctors = _tenantDataContext.UserMsts.Where(p => p.StartDate <= sinDate && p.EndDate >= sinDate && p.JobCd == 1).OrderBy(p => p.SortNo).ToList();
+            var _departments = _tenantDataContext.KaMsts.Where(p => p.HpId == hpId && p.IsDeleted == DeleteTypes.None).ToList();
+            var _comments = _tenantDataContext.RaiinCmtInfs.Where(p => p.HpId == hpId && p.PtId == ptId && p.SinDate == sinDate && p.IsDelete == DeleteTypes.None).ToList();
+            var _timePeriodModels = _tenantDataContext.UketukeSbtMsts.Where(p => p.HpId == hpId && p.IsDeleted == DeleteTypes.None).OrderBy(p => p.SortNo).ToList();
+
+
+            var raiinInfRespo =
+                   dbService.RaiinInfRepository.FindListQueryable(item =>
+                       (item.PtId == ptId) &&
+                       item.HpId == hpId &&
+                       item.IsDeleted == DeleteTypes.None &&
+                       (fromDate == toDate
+                           ? item.SinDate == fromDate
+                           : (item.SinDate >= fromDate && item.SinDate <= toDate)));
+
+
+
+            _departments = _doraiFinder.GetKaNameByHpId();
+            _comments = _doraiFinder.GetRaiinCmtByPtId(_ptId, _sinday, _raiinNo);
+            _hokenPattern = _doraiFinder.GetHokenPattern(_sinday, _sinday, _ptId);
+            _timePeriodModels = _doraiFinder.GetTimePeriodModels();
+
+            if (listDataRaiinInf != null && listDataRaiinInf.Count > 0)
+            {
+                foreach (var item in listDataRaiinInf)
+                {
+
+                }
+            }
+
+
+
             return query.OrderBy(u => u.SortNo).AsEnumerable().Select(u => ToModel(u)).ToList();
+        }
+
+        private string NenkinBango(string? rousaiKofuNo)
+        {
+            string nenkinBango = "";
+            if (rousaiKofuNo != null && rousaiKofuNo.Length == 9)
+            {
+                nenkinBango = rousaiKofuNo.Substring(0, 2);
+            }
+            return nenkinBango;
+        }
+
+        private int GetConfirmDate(int hokenId, int typeHokenGroup)
+        {
+            var validHokenCheck = _tenantDataContext.PtHokenChecks.Where(x => x.IsDeleted == 0 && x.HokenId == hokenId && x.HokenGrp == typeHokenGroup)
+                .OrderByDescending(x => x.CheckDate)
+                .ToList();
+            if (!validHokenCheck.Any())
+            {
+                return 0;
+            }
+            return DateTimeToInt(validHokenCheck[0].CheckDate);
+        }
+
+        private static int DateTimeToInt(DateTime dateTime, string format = "yyyyMMdd")
+        {
+            int result = 0;
+            result = Int32.Parse(dateTime.ToString(format));
+            return result;
         }
 
         public UserMstModel? GetByUserId(int userId)
