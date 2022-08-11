23ca8a61687812e4d878c8356b12284e06d697ad
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-08-11 11:34:08 +0700

    temperaror 4

---
+++ Domain/Models/Insurance/HokenMstModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Insurance
+{
+    public class HokenMstModel
+    {
+        public HokenMstModel(int futanKbn, int futanRate)
+        {
+            FutanKbn = futanKbn;
+            FutanRate = futanRate;
+            HoubetsuNumber = String.Empty;
+            KaiLimitFutan = 0;
+            MonthLimitFutan = 0;
+            DayLimitFutan = 0;
+        }
+
+        public HokenMstModel(int futanKbn, int futanRate, string houbetsuNumber, int kaiLimitFutan, int monthLimitFutan, int dayLimitFutan)
+        {
+            FutanKbn = futanKbn;
+            FutanRate = futanRate;
+            HoubetsuNumber = houbetsuNumber;
+            KaiLimitFutan = kaiLimitFutan;
+            MonthLimitFutan = monthLimitFutan;
+            DayLimitFutan = dayLimitFutan;
+        }
+
+        public int FutanKbn { get; private set; }
+        public int FutanRate { get; private set; }
+        public string HoubetsuNumber { get; private set; }
+        public int KaiLimitFutan { get; private set; }
+        public int MonthLimitFutan { get; private set; }
+        public int DayLimitFutan { get; private set; }
+
+    }
+}
