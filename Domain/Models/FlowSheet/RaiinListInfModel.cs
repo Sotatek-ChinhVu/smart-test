25117afdc591875be9c775781fe84a999ca0e2e2
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-08-18 22:37:23 +0700

    Remove flowsheet

--- Domain/Models/FlowSheet/RaiinListInfModel.cs
+++ Domain/Models/FlowSheet/RaiinListInfModel.cs
@@ -11,17 +11,23 @@ namespace Domain.Models.FlowSheet
         public long RaiinNo { get; private set; }
 
         public int GrpId { get; private set; }
-        
+
         public int KbnCd { get; private set; }
-        
+
+        public string KbnName { get; private set; }
+
+        public string KbnColorCode { get; private set; }
+
         public int RaiinListKbn { get; private set; }
-        
-        public RaiinListInfModel(long raiinNo, int grpId, int kbnCd, int raiinListKbn)
+
+        public RaiinListInfModel(long raiinNo, int grpId, int kbnCd, int raiinListKbn, string kbnName, string kbnColorCode)
         {
             RaiinNo = raiinNo;
             GrpId = grpId;
             KbnCd = kbnCd;
             RaiinListKbn = raiinListKbn;
+            KbnName = kbnName;
+            KbnColorCode = kbnColorCode;
         }
     }
 }
