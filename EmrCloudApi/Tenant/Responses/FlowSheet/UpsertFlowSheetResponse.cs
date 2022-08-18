124ced14dfbd8af83eb0765e61b8c0bb9338479d
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-08-18 21:21:27 +0700

    Temperaror

---
+++ EmrCloudApi/Tenant/Responses/FlowSheet/UpsertFlowSheetResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace EmrCloudApi.Tenant.Responses.FlowSheet
+{
+    public class UpsertFlowSheetResponse
+    {
+        public UpsertFlowSheetResponse(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; private set; }
+    }
+}
