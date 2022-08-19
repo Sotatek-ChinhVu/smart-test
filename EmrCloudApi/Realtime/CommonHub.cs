afce83be7e5d6aba11c87ced428aae679bb28014
SotaTek-TuanNguyen3 <tuan.nguyen3@sotatek.com>
2022-08-19 11:49:39 +0700

    implement getting tenant id flow

--- EmrCloudApi/Realtime/CommonHub.cs
+++ EmrCloudApi/Realtime/CommonHub.cs
@@ -1,5 +1,4 @@
-﻿using Helper.Constants;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 
 namespace EmrCloudApi.Realtime;
 
@@ -7,13 +6,35 @@ public class CommonHub : Hub<ICommonClient>
 {
     public override async Task OnConnectedAsync()
     {
-        await Groups.AddToGroupAsync(Context.ConnectionId, TempIdentity.TenantId);
+        await IfTenantIdExists(tenantId =>
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, tenantId);
+        });
+
         await base.OnConnectedAsync();
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        await Groups.RemoveFromGroupAsync(Context.ConnectionId, TempIdentity.TenantId);
+        await IfTenantIdExists(tenantId =>
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, tenantId);
+        });
+
         await base.OnDisconnectedAsync(exception);
+    }
+
+    private async Task IfTenantIdExists(Func<string, Task> useTenantId)
+    {
+        var tenantId = GetTenantId();
+        if (!string.IsNullOrWhiteSpace(tenantId))
+        {
+            await useTenantId(tenantId);
+        }
+    }
+
+    private string GetTenantId()
+    {
+        return Context.GetHttpContext()!.Request.Query["tenantId"].ToString();
     }
 }
--- EmrCloudApi/Realtime/WebSocketService.cs
+++ EmrCloudApi/Realtime/WebSocketService.cs
@@ -1,4 +1,4 @@
-﻿using Helper.Constants;
+﻿using Infrastructure.Interfaces;
 using Microsoft.AspNetCore.SignalR;
 
 namespace EmrCloudApi.Realtime;
@@ -6,14 +6,18 @@ namespace EmrCloudApi.Realtime;
 public class WebSocketService : IWebSocketService
 {
     private readonly IHubContext<CommonHub, ICommonClient> _hubContext;
+    private readonly ITenantProvider _tenantProvider;
 
-    public WebSocketService(IHubContext<CommonHub, ICommonClient> hubContext)
+    public WebSocketService(IHubContext<CommonHub, ICommonClient> hubContext,
+        ITenantProvider tenantProvider)
     {
         _hubContext = hubContext;
+        _tenantProvider = tenantProvider;
     }
 
     public async Task SendMessageAsync(string functionCode, object message)
     {
-        await _hubContext.Clients.Group(TempIdentity.TenantId).ReceiveMessage(functionCode, message);
+        var tenantId = _tenantProvider.GetTenantId();
+        await _hubContext.Clients.Group(tenantId).ReceiveMessage(functionCode, message);
     }
 }
