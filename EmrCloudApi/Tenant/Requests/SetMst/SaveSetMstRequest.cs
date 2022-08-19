3815b3d8c47c798f4c9f868e6e221bc7bca5992f
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2022-08-19 16:23:33 +0700

    validate input

--- EmrCloudApi/Tenant/Requests/SetMst/SaveSetMstRequest.cs
+++ EmrCloudApi/Tenant/Requests/SetMst/SaveSetMstRequest.cs
@@ -5,33 +5,42 @@ namespace EmrCloudApi.Tenant.Requests.SetMst;
 public class SaveSetMstRequest
 {
     [Required]
+    [RegularExpression(@"^[0-9]{8}$", ErrorMessage = "{0} is Required")]
     public int SinDate { get; set; }
 
+    [RegularExpression(@"^[0-9]*$", ErrorMessage = "{0} >= 0")]
     public int SetCd { get; set; } = 0;
 
     [Required]
+    [RegularExpression(@"^[1-9][0-9]*$", ErrorMessage = "{0} > 0")]
     public int SetKbn { get; set; }
 
     [Required]
+    [RegularExpression(@"^[1-9][0-9]*$", ErrorMessage = "{0} > 0")]
     public int SetKbnEdaNo { get; set; }
 
     public int GenerationId { get; set; } = 0;
 
     [Required]
-    [RegularExpression(@"^[1-9][0-9]*$", ErrorMessage = "Level1 > 0")]
+    [RegularExpression(@"^[1-9][0-9]*$", ErrorMessage = "{0} > 0")]
     public int Level1 { get; set; }
 
+    [RegularExpression(@"^[0-9]*$", ErrorMessage = "{0} >= 0")]
     public int Level2 { get; set; } = 0;
 
+    [RegularExpression(@"^[0-9]*$", ErrorMessage = "{0} >= 0")]
     public int Level3 { get; set; } = 0;
 
     [MaxLength(60)]
     public string SetName { get; set; } = string.Empty;
 
+    [RegularExpression(@"^[0-9]*$", ErrorMessage = "{0} >= 0")]
     public int WeightKbn { get; set; } = 0;
 
+    [RegularExpression(@"^[0-9]*$", ErrorMessage = "{0} >= 0")]
     public int Color { get; set; } = 0;
 
+    [RegularExpression(@"^[0-1]$", ErrorMessage = "{0} = 0 or 1")]
     public int IsDeleted { get; set; } = 0;
 
     public bool IsGroup { get; set; } = false;
