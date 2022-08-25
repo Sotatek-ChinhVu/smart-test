b574ea5af1fa8f3fc2bffdb7107799ea66de7dad
SotaTek-TuanNguyen3 <tuan.nguyen3@sotatek.com>
2022-08-25 16:09:56 +0700

    add AmazonS3Service

---
+++ EmrCloudApi/Services/IAmazonS3Service.cs
@@ -0,0 +1,6 @@
+﻿namespace EmrCloudApi.Services;
+
+public interface IAmazonS3Service
+{
+    Task<string?> UploadAnObjectAsync(string fileName, Stream stream);
+}
---
+++ EmrCloudApi/Tenant/Controllers/FileController.cs
@@ -0,0 +1,39 @@
+﻿using EmrCloudApi.Services;
+using EmrCloudApi.Tenant.Constants;
+using EmrCloudApi.Tenant.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmrCloudApi.Tenant.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileController : ControllerBase
+{
+    private readonly IAmazonS3Service _amazonS3Service;
+
+    public FileController(IAmazonS3Service amazonS3Service)
+    {
+        _amazonS3Service = amazonS3Service;
+    }
+
+    [HttpPost("Upload")]
+    public async Task<Response<string>> UploadAsync([FromQuery] string fileName)
+    {
+        var accessUrl = await _amazonS3Service.UploadAnObjectAsync(fileName, Request.Body);
+        if (accessUrl is null)
+        {
+            return new Response<string>
+            {
+                Status = 0,
+                Message = ResponseMessage.Failed
+            };
+        }
+
+        return new Response<string>
+        {
+            Status = 1,
+            Data = accessUrl,
+            Message = ResponseMessage.Success
+        };
+    }
+}
