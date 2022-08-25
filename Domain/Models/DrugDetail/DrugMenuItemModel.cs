db0fb417c88d1f2962968e58003ab53b8c7b3a8a
hungdao2 <manhhungsky1996@gmail.com>
2022-08-25 15:04:22 +0700

    Update

---
+++ Domain/Models/DrugDetail/DrugMenuItemModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.DrugDetail
+{
+    public class DrugMenuItemModel
+    {
+        public string DrugMenuName { get; private set; }
+
+        public string RawDrugMenuName { get; private set; }
+
+        public int Level { get; private set; }
+
+        public int SeqNo { get; private set; }
+
+        public int DbLevel { get; private set; }
+
+        public string MenuName { get; private set; }
+    }
+}
