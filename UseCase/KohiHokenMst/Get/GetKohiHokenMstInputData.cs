a4443aa3437003cc1d8b9cb1e0bb2f19dfcc9fcf
hungdao2 <manhhungsky1996@gmail.com>
2022-08-30 15:44:58 +0700

    API Get Kohi HokenMst By FutansyaNo

---
+++ UseCase/KohiHokenMst/Get/GetKohiHokenMstInputData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase.Core.Sync.Core;
+
+namespace UseCase.KohiHokenMst.Get
+{
+    public class GetKohiHokenMstInputData : IInputData<GetKohiHokenMstOutputData>
+    {
+        public int HpId { get; private set; }
+
+        public int SinDate { get; private set; }
+
+        public string FutansyaNo { get; private set; }
+
+        public GetKohiHokenMstInputData(int hpId, int sinDate, string futansyaNo)
+        {
+            HpId = hpId;
+            SinDate = sinDate;
+            FutansyaNo = futansyaNo;
+        }
+    }
+}
