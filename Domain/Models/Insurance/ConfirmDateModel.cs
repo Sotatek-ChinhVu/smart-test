27f61c1ca242a63b4cde52fbfd8c29d0de941f3f
DuongLe <duong.le@sotatek.com>
2022-09-12 17:32:34 +0700

    Return checkName

--- Domain/Models/Insurance/ConfirmDateModel.cs
+++ Domain/Models/Insurance/ConfirmDateModel.cs
@@ -16,19 +16,19 @@ namespace Domain.Models.Insurance
 
         public int CheckId { get; private set; }
 
-        public string CheckMachine { get; private set; }
+        public string CheckName{ get; private set; }
 
         public string CheckComment { get; private set; }
 
         public int ConfirmDate { get; private set; }
 
-        public ConfirmDateModel(int hokenGrp, int hokenId, long seqNo, int checkId, string checkMachine, string checkComment, DateTime confirmDate)
+        public ConfirmDateModel(int hokenGrp, int hokenId, long seqNo, int checkId, string checkName, string checkComment, DateTime confirmDate)
         {
             HokenGrp = hokenGrp;
             HokenId = hokenId;
             SeqNo = seqNo;
             CheckId = checkId;
-            CheckMachine = checkMachine;
+            CheckName = checkName;
             CheckComment = checkComment;
             ConfirmDate = int.Parse(confirmDate.ToString("yyyyMMdd"));
         }
