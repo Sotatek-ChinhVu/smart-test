eecde659aa8e596d6328eafa6fd36047b02ca3a8
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-09-13 17:18:29 +0700

    Change public to private

--- Infrastructure/Repositories/OrdInfRepository.cs
+++ Infrastructure/Repositories/OrdInfRepository.cs
@@ -325,7 +325,7 @@ namespace Infrastructure.Repositories
             return KensaGaichuTextConst.NONE;
         }
 
-        public List<YohoSetMstModel> GetListYohoSetMstModelByUserID(List<YohoSetMst> listYohoSetMst, List<TenMst> listTenMst)
+        private List<YohoSetMstModel> GetListYohoSetMstModelByUserID(List<YohoSetMst> listYohoSetMst, List<TenMst> listTenMst)
         {
             var query = from yoho in listYohoSetMst
                         join ten in listTenMst on yoho.ItemCd.Trim() equals ten.ItemCd.Trim()
