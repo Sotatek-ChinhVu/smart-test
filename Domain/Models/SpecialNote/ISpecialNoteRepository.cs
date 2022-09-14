c533f52a59103e6816d331fb24ae4aaaa264d6d2
Phat.Phan2-Sotatek <Phat.Phan2@sotatek.com>
2022-09-14 14:14:57 +0700

    temp

---
+++ Domain/Models/SpecialNote/ISpecialNoteRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Models.SpecialNote
+{
+    public interface ISpecialNoteRepository
+    {
+
+    }
+}
---
+++ Infrastructure/Repositories/SpecialNote/SpecialNoteRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models.SpecialNote;
+using Infrastructure.Interfaces;
+using PostgreDataContext;
+
+namespace Infrastructure.Repositories.SpecialNote
+{
+    public class SpecialNoteRepository : ISpecialNoteRepository
+    {
+        private readonly TenantDataContext _tenantDataContextTracking;
+        private readonly TenantDataContext _tenantDataContextNoTracking;
+
+        public SpecialNoteRepository(ITenantProvider tenantProvider)
+        {
+            _tenantDataContextTracking = tenantProvider.GetTrackingTenantDataContext();
+            _tenantDataContextNoTracking = tenantProvider.GetNoTrackingDataContext();
+        }
+        public bool SaveSpecialNote()
+        {
+
+        }
+    }
+}
