a1ae3eb4522bc64bb5ae91883880e52c573789cd
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-09-22 00:06:07 +0700

    Fix sortno

--- Infrastructure/Repositories/SpecialNote/ImportantNoteRepository.cs
+++ Infrastructure/Repositories/SpecialNote/ImportantNoteRepository.cs
@@ -43,7 +43,6 @@ namespace Infrastructure.Repositories.SpecialNote
                         UpdateMachine = TempIdentity.ComputerName,
                     }
                );
-                maxSortNo = maxSortNo + 1;
             }
 
             _tenantTrackingDataContext.SaveChanges();
