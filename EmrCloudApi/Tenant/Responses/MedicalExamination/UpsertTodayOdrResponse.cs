4820ce1f89397b5d3154f07afd6883f61ef0f116
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-09-21 11:43:55 +0700

    Temperaror

--- EmrCloudApi/Tenant/Responses/MedicalExamination/UpsertTodayOdrResponse.cs
+++ EmrCloudApi/Tenant/Responses/MedicalExamination/UpsertTodayOdrResponse.cs
@@ -1,4 +1,5 @@
-﻿using EmrCloudApi.Tenant.Responses.OrdInf;
+﻿using EmrCloudApi.Tenant.Responses.KarteInf;
+using EmrCloudApi.Tenant.Responses.OrdInf;
 using UseCase.MedicalExamination.UpsertTodayOrd;
 using static Helper.Constants.RaiinInfConst;
 using static Helper.Constants.TodayKarteConst;
@@ -7,7 +8,7 @@ namespace EmrCloudApi.Tenant.Responses.MedicalExamination
 {
     public class UpsertTodayOdrResponse
     {
-        public UpsertTodayOdrResponse(UpsertTodayOrdStatus status, RaiinInfValidationStatus validationRaiinInf, List<ValidationOrdInfListItemResponse> validationOdrs, Dictionary<int, TodayKarteValidationStatus> validationKartes)
+        public UpsertTodayOdrResponse(UpsertTodayOrdStatus status, RaiinInfValidationStatus validationRaiinInf, List<ValidationOrdInfListItemResponse> validationOdrs, List<ValidationKarteInfListItemResponse> validationKartes)
         {
             Status = status;
             ValidationRaiinInf = validationRaiinInf;
@@ -19,6 +20,6 @@ namespace EmrCloudApi.Tenant.Responses.MedicalExamination
 
         public RaiinInfValidationStatus ValidationRaiinInf { get; private set; }
         public List<ValidationOrdInfListItemResponse> ValidationOdrs { get; private set; }
-        public Dictionary<int, TodayKarteValidationStatus> ValidationKartes { get; private set; }
+        public List<ValidationKarteInfListItemResponse> ValidationKartes { get; private set; }
     }
 }
