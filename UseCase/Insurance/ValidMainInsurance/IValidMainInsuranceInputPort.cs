79d06fd90c04925664fff7f03b6da8dbb1ac19bf
hungdao2 <manhhungsky1996@gmail.com>
2022-09-22 10:13:10 +0700

    update

--- UseCase/Insurance/ValidMainInsurance/IValidMainInsuranceInputPort.cs
+++ UseCase/Insurance/ValidMainInsurance/IValidMainInsuranceInputPort.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace UseCase.Insurance.ValidMainInsurance
 {
-    internal class ValidMainInsuranceInputPort
+    internal class IValidMainInsuranceInputPort
     {
     }
 }
