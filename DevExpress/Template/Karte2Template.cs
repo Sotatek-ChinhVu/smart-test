66d0abb72958cbd3b96867dbece7c6fab38c3959
Phat.Phan2-Sotatek <Phat.Phan2@sotatek.com>
2022-09-27 14:56:48 +0700

    tmp

---
+++ DevExpress/Template/Karte2Template.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace DevExpress.Template
+{
+    public partial class Karte2Template : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Karte2Template()
+        {
+            InitializeComponent();
+        }
+    }
+}
