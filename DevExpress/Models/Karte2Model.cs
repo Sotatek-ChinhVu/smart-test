ce0d62a9c5314dcf9b2050f85064ab72a1ded0cd
Phat.Phan2-Sotatek <Phat.Phan2@sotatek.com>
2022-09-27 15:50:56 +0700

    tmp

---
+++ DevExpress/Models/Karte2Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase.MedicalExamination.GetHistory;
+
+namespace DevExpress.Models
+{
+    public class Karte2Model
+    {
+        public string Id { get; set; }
+        public List<TempObj> tempObjs;
+    }
+    public class TempObj
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+}
