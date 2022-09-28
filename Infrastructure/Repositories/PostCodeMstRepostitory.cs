c45db0869995337b8eb665fbd66e4e50aa7f20a5
Sotatek-QuanTo <quan.to@sotatek.com>
2022-09-28 08:34:42 +0700

    SMAR-669

--- Infrastructure/Repositories/PostCodeMstRepostitory.cs
+++ Infrastructure/Repositories/PostCodeMstRepostitory.cs
@@ -1,12 +1,27 @@
 ﻿using Domain.Models.PostCodeMst;
+using Infrastructure.Interfaces;
+using PostgreDataContext;
 
 namespace Infrastructure.Repositories
 {
     public class PostCodeMstRepostitory : IPostCodeMstRepository
     {
-        public List<PostCodeMstModel> PostCodeMstModels(string postCode1, string postCode2, string address)
+        private readonly TenantDataContext _tenantDataContextTracking;
+        private readonly TenantDataContext _tenantDataContextNoTracking;
+
+        public PostCodeMstRepostitory(ITenantProvider tenantProvider)
         {
-            throw new NotImplementedException();
+            _tenantDataContextTracking = tenantProvider.GetTrackingTenantDataContext();
+            _tenantDataContextNoTracking = tenantProvider.GetNoTrackingDataContext();
         }
+
+        //public List<PostCodeMstModel> PostCodeMstModels(string postCode1, string postCode2, string address)
+        //{
+        //    var entities = _tenantDataContextNoTracking.PostCodeMsts;
+        //    if (!string.IsNullOrEmpty(postCode1))
+        //    {
+
+        //    }
+        //}
     }
 }
