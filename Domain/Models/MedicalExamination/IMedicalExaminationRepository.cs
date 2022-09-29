d94fa94e9b679f63a19c88360611a88e11405351
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-09-29 11:15:49 +0700

    temperaror

---
+++ Domain/Models/MedicalExamination/IMedicalExaminationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models.MstItem;
+using Domain.Models.OrdInf;
+
+namespace Domain.Models.OrdInfs
+{
+    public interface IMedicalExaminationRepository
+    {
+        OrdInfModel CheckValidationCommonToSave(int ordId);
+    }
+}
