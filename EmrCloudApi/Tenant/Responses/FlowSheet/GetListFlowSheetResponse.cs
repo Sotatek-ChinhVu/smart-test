db991daa5f894a7990a5b8835ea1425e0e352cba
vantt <van.tran@sotatek.com>
2022-10-07 10:37:10 +0700

    pv set

--- EmrCloudApi/Tenant/Responses/FlowSheet/GetListFlowSheetResponse.cs
+++ EmrCloudApi/Tenant/Responses/FlowSheet/GetListFlowSheetResponse.cs
@@ -5,9 +5,16 @@ namespace EmrCloudApi.Tenant.Responses.FlowSheet
 {
     public class GetListFlowSheetResponse
     {
-        public List<FlowSheetModel> ListFlowSheet { get; set; } = new List<FlowSheetModel>();
+        public GetListFlowSheetResponse(List<FlowSheetModel> listFlowSheet, List<RaiinListMstModel> listRaiinListMstModels, long totalFlowSheet)
+        {
+            ListFlowSheet = listFlowSheet;
+            ListRaiinListMstModels = listRaiinListMstModels;
+            TotalFlowSheet = totalFlowSheet;
+        }
 
-        public List<RaiinListMstModel> ListRaiinListMstModels { get; set; } = new List<RaiinListMstModel>();
-        public long TotalFlowSheet { get; set; } = 0;
+        public List<FlowSheetModel> ListFlowSheet { get; private set; } = new List<FlowSheetModel>();
+
+        public List<RaiinListMstModel> ListRaiinListMstModels { get; private set; } = new List<RaiinListMstModel>();
+        public long TotalFlowSheet { get; private set; } = 0;
     }
 }
