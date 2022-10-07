961330042a9fd80569c281036a8843d9e0a6b666
vantt <van.tran@sotatek.com>
2022-10-07 10:33:17 +0700

    temp

--- EmrCloudApi/Tenant/Requests/PatientInfor/SavePatientInfoRequest.cs
+++ EmrCloudApi/Tenant/Requests/PatientInfor/SavePatientInfoRequest.cs
@@ -4,11 +4,8 @@ namespace EmrCloudApi.Tenant.Requests.PatientInfor
 {
     public class SavePatientInfoRequest
     {
-        public SavePatientInfoRequest(SavePatientInfoModel ptInformation)
-        {
-            PtInformation = ptInformation;
-        }
-
+        #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public SavePatientInfoModel PtInformation { get; set; }
+        #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     }
 }
