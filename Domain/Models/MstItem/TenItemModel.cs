0a7f7eed80813f088e668e28bb21568085801c61
hungdao2 <manhhungsky1996@gmail.com>
2022-10-17 15:12:28 +0700

    Update koui name with api search ten mst

--- Domain/Models/MstItem/TenItemModel.cs
+++ Domain/Models/MstItem/TenItemModel.cs
@@ -196,16 +196,21 @@ namespace Domain.Models.MstItem
             }
         }
 
-        public string KouiName { get => BuildKouiName(ItemCd, DrugKbn, MasterSbt, BuiKbn); }
+        public string KouiName { get => BuildKouiName(ItemCd, DrugKbn, MasterSbt, BuiKbn, SinKouiKbn); }
 
-        private static string BuildKouiName(string itemCd, int drugKbn, string masterSbt, int buiKbn)
+        private static string BuildKouiName(string itemCd, int drugKbn, string masterSbt, int buiKbn, int sinKouiKbn)
         {
-            string rs = "検体";
+            string rs = "";
+            var sinKouiCollection = new SinkouiCollection();
+            var itemKoui = sinKouiCollection.FirstOrDefault(x => x.SinKouiCd == sinKouiKbn);
+            if (itemKoui != null)
+            {
+                rs = itemKoui.SinkouiName;
+            }
             if (itemCd == ItemCdConst.GazoDensibaitaiHozon)
             {
                 rs = "フィルム";
             }
-
             if (drugKbn > 0)
             {
                 switch (drugKbn)
