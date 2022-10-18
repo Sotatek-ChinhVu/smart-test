4015fa7b5ffccf9ebeac10c1f5d3e64fa0174741
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2022-10-18 14:52:01 +0700

    add karte 2 model

---
+++ DevExpress/Models/Karte2/Karte2ExportModel.cs
@@ -0,0 +1,47 @@
+﻿namespace DevExpress.Models.Karte2;
+
+public class Karte2ExportModel
+{
+    public Karte2ExportModel(int hpId, long ptId, long raiinNo, int sinDate, string kanaName, string name, string sex, string birthday, string currentTime, string startDate, string endDate, string fileName, List<RichTextKarteOrder> richTextKarte2Models)
+    {
+        HpId = hpId;
+        PtId = ptId;
+        RaiinNo = raiinNo;
+        SinDate = sinDate;
+        KanaName = kanaName;
+        Name = name;
+        Sex = sex;
+        Birthday = birthday;
+        CurrentTime = currentTime;
+        StartDate = startDate;
+        EndDate = endDate;
+        FileName = fileName;
+        RichTextKarte2Models = richTextKarte2Models;
+    }
+
+    public int HpId { get; private set; }
+
+    public long PtId { get; private set; }
+
+    public long RaiinNo { get; private set; }
+
+    public int SinDate { get; private set; }
+
+    public string KanaName { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string Sex { get; private set; } 
+
+    public string Birthday { get; private set; }
+
+    public string CurrentTime { get; private set; } 
+
+    public string StartDate { get; private set; } 
+
+    public string EndDate { get; private set; } 
+
+    public string FileName { get; private set; } 
+
+    public List<RichTextKarteOrder> RichTextKarte2Models { get; private set; }
+}
---
+++ DevExpress/Models/Karte2/RichTextKarteOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace DevExpress.Models.Karte2;
+
+public class RichTextKarteOrder
+{
+    public RichTextKarteOrder(string sindateDisplay, string richTextKarte, string richTextOrder)
+    {
+        SindateDisplay = sindateDisplay;
+        RichTextKarte = richTextKarte;
+        RichTextOrder = richTextOrder;
+    }
+
+    public string SindateDisplay { get; private set; }
+    public string RichTextKarte { get; private set; }
+    public string RichTextOrder { get; private set; }
+}
