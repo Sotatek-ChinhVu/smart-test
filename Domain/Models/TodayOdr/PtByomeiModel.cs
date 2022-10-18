e12d0df99ee442dc11f87c1061d4102053157d1c
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-10-18 10:40:16 +0700

    temperaror

---
+++ Domain/Models/TodayOdr/PtByomeiModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.TodayOdr
+{
+    public class PtByomeiModel
+    {
+
+        public int SikkanCd { get; private set; }
+
+        public int NanByoCd { get; private set; }
+
+        public string Byomei { get; private set; }
+
+        
+    }
+}
