9bcd35f86ed8b2c0809815ba7853c4e63447e055
vantt <van.tran@sotatek.com>
2022-10-19 09:17:09 +0700

    lack code

--- Domain/Models/PatientInfor/SavePatientInfoModel.cs
+++ Domain/Models/PatientInfor/SavePatientInfoModel.cs
@@ -1,11 +1,12 @@
-﻿using Domain.Models.GroupInf;
+﻿using Domain.Models.CalculationInf;
+using Domain.Models.GroupInf;
 using Domain.Models.InsuranceInfor;
 
 namespace Domain.Models.PatientInfor
 {
     public class SavePatientInfoModel
     {
-        public SavePatientInfoModel(PatientInforSaveModel patient, List<PtKyuseiModel> ptKyuseis, List<PtInfSanteiConfModel> ptSanteis, List<InsuranceModel> insurances, List<GroupInfModel> ptGrpInfs)
+        public SavePatientInfoModel(PatientInforSaveModel patient, List<PtKyuseiModel> ptKyuseis, List<CalculationInfModel> ptSanteis, List<InsuranceModel> insurances, List<GroupInfModel> ptGrpInfs)
         {
             Patient = patient;
             PtKyuseis = ptKyuseis;
@@ -16,7 +17,7 @@ namespace Domain.Models.PatientInfor
 
         public PatientInforSaveModel Patient { get; private set; }
         public List<PtKyuseiModel> PtKyuseis { get; private set; } = new List<PtKyuseiModel>();
-        public List<PtInfSanteiConfModel> PtSanteis { get; private set; } = new List<PtInfSanteiConfModel>();
+        public List<CalculationInfModel> PtSanteis { get; private set; } = new List<CalculationInfModel>();
         public List<InsuranceModel> Insurances { get; private set; } = new List<InsuranceModel>();
         public List<GroupInfModel> PtGrpInfs { get; private set; } = new List<GroupInfModel>();
         
--- UseCase/PatientInfor/Save/SavePatientInfoInputData.cs
+++ UseCase/PatientInfor/Save/SavePatientInfoInputData.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Models.GroupInf;
+﻿using Domain.Models.CalculationInf;
+using Domain.Models.GroupInf;
 using Domain.Models.InsuranceInfor;
 using Domain.Models.PatientInfor;
 using UseCase.Core.Sync.Core;
@@ -7,7 +8,7 @@ namespace UseCase.PatientInfor.Save
 {
     public class SavePatientInfoInputData : IInputData<SavePatientInfoOutputData>
     {
-        public SavePatientInfoInputData(PatientInforSaveModel patient, List<PtKyuseiModel> ptKyuseis, List<PtInfSanteiConfModel> ptSanteis, List<InsuranceModel> insurances, List<GroupInfModel> ptGrps)
+        public SavePatientInfoInputData(PatientInforSaveModel patient, List<PtKyuseiModel> ptKyuseis, List<CalculationInfModel> ptSanteis, List<InsuranceModel> insurances, List<GroupInfModel> ptGrps)
         {
             Patient = patient;
             PtKyuseis = ptKyuseis;
@@ -17,7 +18,7 @@ namespace UseCase.PatientInfor.Save
         }
         public PatientInforSaveModel Patient { get; private set; }
         public List<PtKyuseiModel> PtKyuseis { get; private set; } = new List<PtKyuseiModel>();
-        public List<PtInfSanteiConfModel> PtSanteis { get; private set; } = new List<PtInfSanteiConfModel>();
+        public List<CalculationInfModel> PtSanteis { get; private set; } = new List<CalculationInfModel>();
         public List<InsuranceModel> Insurances { get; private set; }
         public List<GroupInfModel> PtGrps { get; private set; }
     }
