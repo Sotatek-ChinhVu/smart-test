6a23a9b7d3bcdee5f6518ce6d30d073554d5650c
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-10-27 16:18:13 +0700

    Complete

---
+++ UseCase/UserConf/GetUserConfList/IGetUserConfListOutputPort.cs
@@ -0,0 +1,8 @@
+﻿using UseCase.Core.Sync.Core;
+
+namespace UseCase.User.GetUserConfList
+{
+    public interface IGetUserConfListOutputPort : IOutputPort<GetUserConfListOutputData>
+    {
+    }
+}
