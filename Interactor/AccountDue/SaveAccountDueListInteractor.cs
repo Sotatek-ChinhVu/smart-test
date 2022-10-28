c517c786f861e9181d756e8c74cc94e767cada54
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2022-10-28 14:33:28 +0700

    update temp

---
+++ Interactor/AccountDue/SaveAccountDueListInteractor.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models.AccountDue;
+using UseCase.AccountDue.SaveAccountDueList;
+
+namespace Interactor.AccountDue;
+
+public class SaveAccountDueListInteractor : ISaveAccountDueListInputPort
+{
+    private readonly IAccountDueRepository _accountDueRepository;
+
+    public SaveAccountDueListInteractor(IAccountDueRepository accountDueRepository)
+    {
+        _accountDueRepository = accountDueRepository;
+    }
+
+    public SaveAccountDueListOutputData Handle(SaveAccountDueListInputData inputData)
+    {
+        throw new NotImplementedException();
+    }
+}
--- UseCase/AccountDue/SaveAccountDueList/SyunoNyukinInputItem.cs
+++ UseCase/AccountDue/SaveAccountDueList/SyunoNyukinInputItem.cs
@@ -2,5 +2,28 @@
 
 public class SyunoNyukinInputItem
 {
+    //nyukin.RaiinNo = model.RaiinNo;
+    //nyukin.SortNo = model.SortNo;
+    //nyukin.AdjustFutan = model.AdjustFutan;
+    //nyukin.NyukinGaku = model.NyukinGaku;
+    //nyukin.PaymentMethodCd = model.PaymentMethodCd;
+    //nyukin.NyukinDate = model.NyukinDate;
+    //nyukin.UketukeSbt = model.UketukeSbt;
+    //nyukin.NyukinCmt = model.NyukinCmt;
+    //nyukin.NyukinjiSeikyu = model.SeikyuGaku;
+    //nyukin.NyukinjiTensu = model.SeikyuTensu;
+    //nyukin.NyukinjiDetail = model.SeikyuDetail;
+
+    public long RaiinNo { get; private set; }
+
+    public int SortNo { get; private set; }
+
+    public int AdjustFutan { get; private set; }
+
+    public int NyukinGaku { get; private set; }
+
+    public int PaymentMethodCd { get; private set; }
+
+    public int NyukinDate { get; private set; }
 
 }
