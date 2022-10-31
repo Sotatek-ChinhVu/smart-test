9e2ef8ea572a41050d2b52dc8a11bf5ca892d432
Sotatek-QuanTo <quan.to@sotatek.com>
2022-10-31 11:29:33 +0700

    Update respone totalcount

--- UseCase/MstItem/SearchPostCode/SearchPostCodeOutputData.cs
+++ UseCase/MstItem/SearchPostCode/SearchPostCodeOutputData.cs
@@ -5,12 +5,14 @@ namespace UseCase.MstItem.SearchPostCode
 {
     public class SearchPostCodeOutputData : IOutputData
     {
-        public SearchPostCodeOutputData(List<PostCodeMstModel> postCodeMstModels, SearchPostCodeStatus status)
+        public SearchPostCodeOutputData(int totalCount, List<PostCodeMstModel> postCodeMstModels, SearchPostCodeStatus status)
         {
+            TotalCount = totalCount;
             PostCodeMstModels = postCodeMstModels;
             Status = status;
         }
 
+        public int TotalCount { get; private set; }
         public List<PostCodeMstModel> PostCodeMstModels { get; private set; }
 
         public SearchPostCodeStatus Status { get; private set; }
