6e5aa1c93e3a191c969b25b3f9c4ef37dc91221d
DuongLe <duong.le@sotatek.com>
2022-11-15 15:45:12 +0700

    Catch exception

--- DevExpress/Export/Karte1Export.cs
+++ DevExpress/Export/Karte1Export.cs
@@ -10,33 +10,33 @@ public class Karte1Export: IKarte1Export
 {
     public (MemoryStream, string) ExportToPdf(Karte1ExportModel data)
     {
-        var report = new Karte1TemplatePage1();
-        var dataSource = new ObjectDataSource();
-        dataSource.DataSource = data;
-
-        report.DataSource = dataSource;
-        report.DataMember = "ListByomeiModelsPage1";
-
-        if (data.ListByomeiModelsPage2.Count > 0)
-        {
-            report.CreateDocument();
-            report.ModifyDocument(report =>
-            {
-                var page2 = new Karte1TemplatePage2();
-                page2.DataSource = dataSource;
-                page2.DataMember = "ListByomeiModelsPage2";
-                page2.CreateDocument();
-                report.AddPages(page2.Pages);
-            });
-        }
-
-        PdfExportOptions pdfExportOptions = new PdfExportOptions()
-        {
-            PdfACompatibility = PdfACompatibility.PdfA1b
-        };
-
         try
         {
+            var report = new Karte1TemplatePage1();
+            var dataSource = new ObjectDataSource();
+            dataSource.DataSource = data;
+
+            report.DataSource = dataSource;
+            report.DataMember = "ListByomeiModelsPage1";
+
+            if (data.ListByomeiModelsPage2.Count > 0)
+            {
+                report.CreateDocument();
+                report.ModifyDocument(report =>
+                {
+                    var page2 = new Karte1TemplatePage2();
+                    page2.DataSource = dataSource;
+                    page2.DataMember = "ListByomeiModelsPage2";
+                    page2.CreateDocument();
+                    report.AddPages(page2.Pages);
+                });
+            }
+
+            PdfExportOptions pdfExportOptions = new PdfExportOptions()
+            {
+                PdfACompatibility = PdfACompatibility.PdfA1b
+            };
+
             // Export the report.
             MemoryStream stream = new();
             report.ExportToPdf(stream, pdfExportOptions);
