b00f31ec69f69329df24695a03e289794b71b643
vantt <van.tran@sotatek.com>
2022-11-24 17:52:37 +0700

    lost

--- UseCase/Schema/SaveInsuranceScan/SaveInsuranceScanInputData.cs
+++ UseCase/Schema/SaveInsuranceScan/SaveInsuranceScanInputData.cs
@@ -4,13 +4,12 @@ namespace Schema.Insurance.SaveInsuranceScan
 {
     public class SaveInsuranceScanInputData : IInputData<SaveInsuranceScanOutputData>
     {
-        public SaveInsuranceScanInputData(int hpId, long ptId , int hokenGrp, int hokenId, long seqNo , string oldImage, int userId, Stream streamImage)
+        public SaveInsuranceScanInputData(int hpId, long ptId , int hokenGrp, int hokenId, string oldImage, int userId, Stream streamImage)
         {
             HpId = hpId;
             PtId = ptId;
             HokenGrp = hokenGrp;
             HokenId = hokenId;
-            SeqNo = seqNo;
             OldImage = oldImage;
             UserId = userId;
             StreamImage = streamImage;
@@ -23,8 +22,6 @@ namespace Schema.Insurance.SaveInsuranceScan
         public int HokenGrp { get; private set; }
 
         public int HokenId { get; private set; }
-
-        public long SeqNo { get; private set; }
 
         public string OldImage { get; private set; }
 
