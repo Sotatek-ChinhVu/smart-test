2d45c5f7872da4450ce8c493a714bc1f80a77d4f
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-12-05 17:26:06 +0700

    Complete

---
+++ Domain/Models/MstItem/RecedenCmtSelectModel.cs
@@ -0,0 +1,46 @@
+﻿namespace Domain.Models.MstItem
+{
+    public class RecedenCmtSelectModel
+    {
+        private List<int> cmtSbts = new List<int> { 20, 21, 22 };
+
+        public RecedenCmtSelectModel(int cmtSbt, string itemCd, string cmtCd, string commentName, int itemNo, int edaNo, string content, int sortNo, int condKbn)
+        {
+            CmtSbt = cmtSbt;
+            ItemCd = itemCd;
+            CmtCd = cmtCd;
+            CommentName = commentName;
+            ItemNo = itemNo;
+            EdaNo = edaNo;
+            Content = content;
+            SortNo = sortNo;
+            CondKbn = condKbn;
+        }
+
+        public bool IsSatsueiBui
+        {
+            get
+            {
+                return cmtSbts.Contains(CmtSbt);
+            }
+        }
+
+        public int CmtSbt { get; private set; }
+
+        public string ItemCd { get; private set; }
+
+        public string CmtCd { get; private set; }
+
+        public string CommentName { get; private set; }
+
+        public int ItemNo { get; private set; }
+
+        public int EdaNo { get; private set; }
+
+        public string Content { get; private set; }
+
+        public int SortNo { get; private set; }
+
+        public int CondKbn { get; private set; }
+    }
+}
---
+++ UseCase/MstItem/GetSelectiveComment/GetSelectiveCommentInputData.cs
@@ -0,0 +1,20 @@
+﻿using UseCase.Core.Sync.Core;
+
+namespace UseCase.MstItem.GetSelectiveComment
+{
+    public class GetSelectiveCommentInputData : IInputData<GetSelectiveCommentOutputData>
+    {
+        public GetSelectiveCommentInputData(int hpId, List<string> listItemCd, int sinDate)
+        {
+            HpId = hpId;
+            ListItemCd = listItemCd;
+            SinDate = sinDate;
+        }
+
+        public int HpId { get; private set; }
+
+        public List<string> ListItemCd { get; private set; }
+
+        public int SinDate { get; private set; }
+    }
+}
