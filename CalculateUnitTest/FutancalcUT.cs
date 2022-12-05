65c0e34f7c6585d61c9d7238bdd4d3e41cae03e3
DuongLe <duong.le@sotatek.com>
2022-12-05 17:07:28 +0700

    Create UI project for Calculate

---
+++ CalculateUnitTest/FutancalcUT.cs
@@ -0,0 +1,16 @@
+﻿namespace CalculateUnitTest
+{
+    public class FutancalcUT
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
