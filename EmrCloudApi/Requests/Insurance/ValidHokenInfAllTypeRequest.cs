a438ca13ef40d4b8325abda7fc597d22fcf1d4b8
vantt <van.tran@sotatek.com>
2022-12-07 09:04:41 +0700

    lack code

--- EmrCloudApi/Requests/Insurance/ValidHokenInfAllTypeRequest.cs
+++ EmrCloudApi/Requests/Insurance/ValidHokenInfAllTypeRequest.cs
@@ -4,82 +4,82 @@ namespace EmrCloudApi.Requests.Insurance
 {
     public class ValidHokenInfAllTypeRequest
     {
-        public int HpId { get; private set; }
+        public int HpId { get; set; }
 
-        public int HokenKbn { get; private set; }
+        public int HokenKbn { get; set; }
 
-        public int SinDate { get; private set; }
+        public int SinDate { get;  set; }
 
-        public bool IsSelectedHokenInf { get; private set; }
+        public bool IsSelectedHokenInf { get;  set; }
 
-        public string SelectedHokenInfRodoBango { get; private set; } = string.Empty;
+        public string SelectedHokenInfRodoBango { get;  set; } = string.Empty;
 
-        public List<RousaiTenkiModel> ListRousaiTenki { get; private set; } = new List<RousaiTenkiModel>();
+        public List<RousaiTenkiModel> ListRousaiTenki { get;  set; } = new List<RousaiTenkiModel>();
 
-        public int SelectedHokenInfRousaiSaigaiKbn { get; private set; }
+        public int SelectedHokenInfRousaiSaigaiKbn { get;  set; }
 
-        public int SelectedHokenInfRousaiSyobyoDate { get; private set; }
+        public int SelectedHokenInfRousaiSyobyoDate { get;  set; }
 
-        public string SelectedHokenInfRousaiSyobyoCd { get; private set; } = string.Empty;
+        public string SelectedHokenInfRousaiSyobyoCd { get;  set; } = string.Empty;
 
-        public int SelectedHokenInfRyoyoStartDate { get; private set; }
+        public int SelectedHokenInfRyoyoStartDate { get;  set; }
 
-        public int SelectedHokenInfRyoyoEndDate { get; private set; }
+        public int SelectedHokenInfRyoyoEndDate { get;  set; }
 
-        public int SelectedHokenInfStartDate { get; private set; }
+        public int SelectedHokenInfStartDate { get;  set; }
 
-        public int SelectedHokenInfEndDate { get; private set; }
+        public int SelectedHokenInfEndDate { get;  set; }
 
-        public bool SelectedHokenInfIsAddNew { get; private set; }
+        public bool SelectedHokenInfIsAddNew { get;  set; }
 
-        public string SelectedHokenInfNenkinBango { get; private set; } = string.Empty;
+        public string SelectedHokenInfNenkinBango { get;  set; } = string.Empty;
 
-        public string SelectedHokenInfKenkoKanriBango { get; private set; } = string.Empty;
+        public string SelectedHokenInfKenkoKanriBango { get;  set; } = string.Empty;
 
-        public int SelectedHokenInfConfirmDate { get; private set; }
+        public int SelectedHokenInfConfirmDate { get;  set; }
 
-        public bool SelectedHokenInfHokenMasterModelIsNull { get; private set; }
+        public bool SelectedHokenInfHokenMasterModelIsNull { get;  set; }
 
-        public bool SelectedHokenInf { get; private set; }
+        public bool SelectedHokenInf { get;  set; }
 
-        public string SelectedHokenInfTokki1 { get; private set; } = string.Empty;
+        public string SelectedHokenInfTokki1 { get;  set; } = string.Empty;
 
-        public string SelectedHokenInfTokki2 { get; private set; } = string.Empty;
+        public string SelectedHokenInfTokki2 { get;  set; } = string.Empty;
 
-        public string SelectedHokenInfTokki3 { get; private set; } = string.Empty;
+        public string SelectedHokenInfTokki3 { get;  set; } = string.Empty;
 
-        public string SelectedHokenInfTokki4 { get; private set; } = string.Empty;
+        public string SelectedHokenInfTokki4 { get;  set; } = string.Empty;
 
-        public string SelectedHokenInfTokki5 { get; private set; } = string.Empty;
+        public string SelectedHokenInfTokki5 { get;  set; } = string.Empty;
 
-        public string SelectedHokenInfHoubetu { get; private set; } = string.Empty;
+        public string SelectedHokenInfHoubetu { get;  set; } = string.Empty;
 
-        public bool SelectedHokenInfIsJihi { get; private set; }
+        public bool SelectedHokenInfIsJihi { get;  set; }
 
-        public string HokenSyaNo { get; private set; } = string.Empty;
+        public string HokenSyaNo { get;  set; } = string.Empty;
 
-        public string SelectedHokenInfKigo { get; private set; } = string.Empty;
+        public string SelectedHokenInfKigo { get;  set; } = string.Empty;
 
-        public string SelectedHokenInfBango { get; private set; } = string.Empty;
+        public string SelectedHokenInfBango { get;  set; } = string.Empty;
 
-        public int SelectedHokenInfTokureiYm1 { get; private set; }
+        public int SelectedHokenInfTokureiYm1 { get;  set; }
 
-        public int SelectedHokenInfTokureiYm2 { get; private set; }
+        public int SelectedHokenInfTokureiYm2 { get;  set; }
 
-        public bool SelectedHokenInfisShahoOrKokuho { get; private set; }
+        public bool SelectedHokenInfisShahoOrKokuho { get;  set; }
 
-        public bool SelectedHokenInfisExpirated { get; private set; }
+        public bool SelectedHokenInfisExpirated { get;  set; }
 
-        public int SelectedHokenInfconfirmDate { get; private set; }
+        public int SelectedHokenInfconfirmDate { get;  set; }
 
-        public int SelectedHokenInfHokenNo { get; private set; }
+        public int SelectedHokenInfHokenNo { get;  set; }
 
-        public int SelectedHokenInfHokenEdraNo { get; private set; }
+        public int SelectedHokenInfHokenEdraNo { get;  set; }
 
-        public bool IsSelectedHokenMst { get; private set; }
+        public bool IsSelectedHokenMst { get;  set; }
 
-        public int SelectedHokenInfHonkeKbn { get; private set; }
+        public int SelectedHokenInfHonkeKbn { get;  set; }
 
-        public int PtBirthday { get; private set; }
+        public int PtBirthday { get;  set; }
     }
 }
