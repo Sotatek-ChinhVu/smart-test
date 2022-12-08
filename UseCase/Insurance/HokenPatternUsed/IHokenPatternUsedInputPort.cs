a60d6f3eafbff36a0430adeaf0e92921b5a0c93b
vantt <van.tran@sotatek.com>
2022-12-08 14:52:55 +0700

    1234 task

---
+++ UseCase/Insurance/HokenPatternUsed/IHokenPatternUsedInputPort.cs
@@ -0,0 +1,8 @@
+﻿using UseCase.Core.Sync.Core;
+
+namespace UseCase.Insurance.HokenPatternUsed
+{
+    public interface IHokenPatternUsedInputPort : IInputPort<HokenPatternUsedInputData, HokenPatternUsedOutputData>
+    {
+    }
+}
---
+++ UseCase/Insurance/HokenPatternUsed/IHokenPatternUsedOutputPortcs.cs
@@ -0,0 +1,8 @@
+﻿using UseCase.Core.Sync.Core;
+
+namespace UseCase.Insurance.HokenPatternUsed
+{
+    public interface IHokenPatternUsedOutputPortcs : IOutputPort<HokenPatternUsedOutputData>
+    {
+    }
+}
