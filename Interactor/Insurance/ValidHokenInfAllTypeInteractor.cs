ab0bad9d487960062622dc49ee4bb5da86a55ba9
vantt <van.tran@sotatek.com>
2022-12-09 14:40:08 +0700

    update status

--- Interactor/Insurance/ValidHokenInfAllTypeInteractor.cs
+++ Interactor/Insurance/ValidHokenInfAllTypeInteractor.cs
@@ -419,7 +419,7 @@ namespace Interactor.Insurance
             {
                 var paramsMessage = new string[] { "労災保険", "無視する", "戻る" };
                 message = String.Format(ErrorMessage.MessageType_mInp00041, paramsMessage);
-                validateDetails.Add(new ResultValidateInsurance<ValidHokenInfAllTypeStatus>(ValidHokenInfAllTypeStatus.InvalidCheckDateExpirated, message, TypeMessage.TypeMessageError));
+                validateDetails.Add(new ResultValidateInsurance<ValidHokenInfAllTypeStatus>(ValidHokenInfAllTypeStatus.InvalidCheckDateExpirated, message, TypeMessage.TypeMessageWarning));
             }
         }
 
