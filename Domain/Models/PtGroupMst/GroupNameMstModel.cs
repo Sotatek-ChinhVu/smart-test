e8c90baa96aaa52ccaade56625667259603d86c5
vantt <van.tran@sotatek.com>
2022-12-13 17:07:37 +0700

    api temp

---
+++ Domain/Models/PtGroupMst/GroupNameMstModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Models.PtGroupMst
+{
+    public class GroupNameMstModel
+    {
+        public GroupNameMstModel(int grpId, int sortNo, string grpName,int isDeleted, List<GroupItemModel> groupItems)
+        {
+            GrpId = grpId;
+            SortNo = sortNo;
+            GrpName = grpName;
+            IsDeleted = isDeleted;
+            GroupItems = groupItems;
+        }
+
+        public int GrpId { get; private set; }
+
+        public int SortNo { get; private set; }
+
+        public string GrpName { get; private set; }
+
+        public int IsDeleted { get; private set; }
+
+        public List<GroupItemModel> GroupItems { get; private set; } = new List<GroupItemModel>();
+    }
+}
