0cd132091bfcc9bd709ce3a28a620755335560b5
DuongLe <duong.le@sotatek.com>
2022-12-14 20:40:40 +0700

    Uncomment code

--- Infrastructure/Repositories/JsonSettingRepository.cs
+++ Infrastructure/Repositories/JsonSettingRepository.cs
@@ -17,28 +17,27 @@ public class JsonSettingRepository : IJsonSettingRepository
 
     public JsonSettingModel? Get(int userId, string key)
     {
-        return null;
-        //var entity = _tenantDataContext.JsonSettings.FirstOrDefault(e => e.UserId == userId && e.Key == key);
-        //return entity is null ? null : ToModel(entity);
+        var entity = _tenantDataContext.JsonSettings.FirstOrDefault(e => e.UserId == userId && e.Key == key);
+        return entity is null ? null : ToModel(entity);
     }
 
     public void Upsert(JsonSettingModel model)
     {
-        //var existingEntity = _tenantDataContext.JsonSettings.AsTracking()
-        //    .FirstOrDefault(e => e.UserId == model.UserId && e.Key == model.Key);
-        //if (existingEntity is null)
-        //{
-        //    _tenantDataContext.JsonSettings.Add(new JsonSetting
-        //    {
-        //        UserId = model.UserId,
-        //        Key = model.Key,
-        //        Value = model.Value
-        //    });
-        //}
-        //else
-        //{
-        //    existingEntity.Value = model.Value;
-        //}
+        var existingEntity = _tenantDataContext.JsonSettings.AsTracking()
+            .FirstOrDefault(e => e.UserId == model.UserId && e.Key == model.Key);
+        if (existingEntity is null)
+        {
+            _tenantDataContext.JsonSettings.Add(new JsonSetting
+            {
+                UserId = model.UserId,
+                Key = model.Key,
+                Value = model.Value
+            });
+        }
+        else
+        {
+            existingEntity.Value = model.Value;
+        }
 
         _tenantDataContext.SaveChanges();
     }
