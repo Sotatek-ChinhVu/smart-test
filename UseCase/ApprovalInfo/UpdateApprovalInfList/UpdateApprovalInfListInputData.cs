27e759b6c9ae020c72112906ff81419c87307917
Sotatek-MinhLe <minh.le@sotatek.com>
2022-12-15 08:30:43 +0700

    Revert "Fix Comment Update Approval Info"
    This reverts commit e061780065b109f02da3ea6418dec41faeefdc25.

--- UseCase/ApprovalInfo/UpdateApprovalInfList/UpdateApprovalInfListInputData.cs
+++ UseCase/ApprovalInfo/UpdateApprovalInfList/UpdateApprovalInfListInputData.cs
@@ -5,15 +5,18 @@ namespace UseCase.ApprovalInfo.UpdateApprovalInfList;
 
 public class UpdateApprovalInfListInputData : IInputData<UpdateApprovalInfListOutputData>
 {
-    public UpdateApprovalInfListInputData(List<ApprovalInfModel> approvalInfs, int userId)
+    public UpdateApprovalInfListInputData(List<ApprovalInfModel> approvalInfs, int userId, DateTime dateTime)
     {
         ApprovalInfs = approvalInfs;
         UserId = userId;
+        Time = dateTime;
     }
 
     public List<ApprovalInfModel> ApprovalInfs { get; private set; }
 
     public int UserId { get; private set; }
+
+    public DateTime Time { get; private set; }
 
     public List<ApprovalInfModel> ToList()
     {
