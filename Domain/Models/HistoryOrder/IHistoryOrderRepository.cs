510f3824f012e531c3e461d37ba8c0fb5fef8768
DuongLe <duong.le@sotatek.com>
2022-12-19 10:39:07 +0700

    Implement search

--- Domain/Models/HistoryOrder/IHistoryOrderRepository.cs
+++ Domain/Models/HistoryOrder/IHistoryOrderRepository.cs
@@ -1,10 +1,13 @@
 ﻿using Domain.Models.KarteFilterMst;
+using Domain.Models.Reception;
 
 namespace Domain.Models.HistoryOrder
 {
     public interface IHistoryOrderRepository
     {
         (int, List<HistoryOrderModel>) GetList(int hpId, int userId, long ptId, int sinDate, int offset, int limit, int filterId, int isDeleted);
+
+        public (int, ReceptionModel) Search(int hpId, int userId, long ptId, int sinDate, int currentIndex, int filterId, int isDeleted, string keyWord, int searchType, bool isNext);
 
         KarteFilterMstModel GetFilter(int hpId, int userId, int filterId);
 
