858d944b19082799c8077b906be0fdb2c0b94940
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2022-12-20 09:40:39 +0700

    temp

---
+++ UnitTests/Interactor/MedicalExamination/GetCheckedSpecialItemInteractorTest.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Models.User;
+using Helper.Constants;
+using Interactor.User;
+using Moq;
+using UseCase.User.GetByLoginId;
+
+namespace UnitTests.Interactor.MedicalExamination;
+
+public class GetCheckedSpecialItemInteractorTest
+{
+    [Fact]
+    public void ReturnsSuccess_ForValidLoginId()
+    {
+        // Arrange
+        var testLoginId = "test login id";
+        var mockUserRepo = new Mock<IUserRepository>();
+        mockUserRepo.Setup(repo => repo.GetByLoginId(testLoginId))
+            .Returns(new UserMstModel(1, 1, 1, 1, 1, 1, string.Empty, string.Empty,
+                string.Empty, string.Empty, testLoginId, string.Empty,
+                string.Empty, 1, 1, 1, string.Empty, DeleteTypes.None));
+        var interactor = new GetUserByLoginIdInteractor(mockUserRepo.Object);
+        var input = new GetUserByLoginIdInputData(testLoginId);
+
+        // Act
+        var output = interactor.Handle(input);
+
+        // Assert
+        Assert.Equal(GetUserByLoginIdStatus.Success, output.Status);
+        Assert.NotNull(output.User);
+    }
+}
