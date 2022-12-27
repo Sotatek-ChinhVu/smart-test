26b5966fd8b8279044f60ca448f16854cef67ef3
DuongLe <duong.le@sotatek.com>
2022-12-28 01:41:00 +0700

    Create RunTraialCalculate request object

---
+++ EmrCalculateApi/Requests/OrderDetailInfo.cs
@@ -0,0 +1,67 @@
+﻿using Domain.Models.OrdInf;
+using Entity.Tenant;
+using Helper.Common;
+using Helper.Constants;
+using Helper.Extension;
+
+namespace EmrCalculateApi.Requests
+{
+    public class OrderDetailInfo
+    {
+        public long RpNo { get; set; }
+
+        public long RpEdaNo { get; set; }
+
+        public int RowNo { get; set; }
+
+        public int SinKouiKbn { get; set; }
+
+        public string ItemCd { get; set; } = string.Empty;
+
+        public double Suryo { get; set; }
+
+        public string UnitName { get; set; } = string.Empty;
+
+        public int UnitSBT { get; set; }
+
+        public double TermVal { get; set; }
+
+        public int KohatuKbn { get; set; }
+
+        public int SyohoKbn { get; set; }
+
+        public int SyohoLimitKbn { get; set; }
+
+        public int DrugKbn { get; set; }
+
+        public int YohoKbn { get; set; }
+
+        public string Kokuji1 { get; set; } = string.Empty;
+
+        public string Kokuji2 { get; set; } = string.Empty;
+
+        public int IsNodspRece { get; set; }
+
+        public string IpnCd { get; set; } = string.Empty;
+
+        public string IpnName { get; set; } = string.Empty;
+
+        public int JissiKbn { get; set; }
+
+        public DateTime JissiDate { get; set; }
+
+        public int JissiId { get; set; }
+
+        public string JissiMachine { get; set; } = string.Empty;
+
+        public string ReqCd { get; set; } = string.Empty;
+
+        public string Bunkatu { get; set; } = string.Empty;
+
+        public string CmtName { get; set; } = string.Empty;
+
+        public string CmtOpt { get; set; } = string.Empty;
+
+        public int CommentNewline { get; set; }
+    }
+}
---
+++ EmrCalculateApi/Requests/OrderInfo.cs
@@ -0,0 +1,35 @@
+﻿using Entity.Tenant;
+
+namespace EmrCalculateApi.Requests
+{
+    public class OrderInfo
+    {
+        public List<OrderDetailInfo> DetailInfoList { get; set; } = new List<OrderDetailInfo>();
+
+        public long RpNo { get; set; }
+
+        public long RpEdaNo { get; set; }
+
+        public int HokenPid { get; set; }
+
+        public int OdrKouiKbn { get; set; }
+
+        public int GroupOdrKouiKbn { get; set; }
+
+        public int InoutKbn { get; set; }
+
+        public int SikyuKbn { get; set; }
+
+        public int SyohoSbt { get; set; }
+
+        public int SanteiKbn { get; set; }
+
+        public int TosekiKbn { get; set; }
+
+        public int DaysCnt { get; set; }
+
+        public int SortNo { get; set; }
+
+        public int IsDeleted { get; set; }
+    }
+}
