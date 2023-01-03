91d8d6c72cd7486e5c573d061112767f8083b4b6
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2023-01-03 14:57:14 +0700

    create structure reporting

---
+++ EmrCloudApi/Controller/ExportReportController.cs
@@ -0,0 +1,18 @@
+﻿using EmrCloudApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Reporting;
+using UseCase.Core.Sync;
+
+namespace EmrCloudApi.Controller;
+
+[Route("api/[controller]")]
+public class ExportReportController : AuthorizeControllerBase
+{
+    private readonly UseCaseBus _bus;
+    private readonly IReporting _reporting;
+    public ExportReportController(UseCaseBus bus, IUserService userService, IReporting reporting) : base(userService)
+    {
+        _bus = bus;
+        _reporting = reporting;
+    }
+}
---
+++ Reporting/IReporting.cs
@@ -0,0 +1,5 @@
+﻿namespace Reporting;
+
+public interface IReporting
+{
+}
