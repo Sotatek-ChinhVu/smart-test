b081259b44c63aed970bed137d7b298a3f1e67a2
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2023-01-12 11:22:00 +0700

    update read data from excel for unit test

--- CloudTest/SampleData/ReadDataSanteiInf.cs
+++ CloudTest/SampleData/ReadDataSanteiInf.cs
@@ -137,6 +137,16 @@ public static class ReadDataSanteiInf
                             case 6:
                                 santeiInfDetail.Byomei = text;
                                 break;
+                            case 6:
+                                santeiInfDetail.HosokuComment = text;
+                                break;
+                            case 6:
+                                santeiInfDetail.Comment = text;
+                                break;
+                            case 6:
+                                int.TryParse(text, out int isDelete);
+                                santeiInfDetail.IsDeleted = isDelete;
+                                break;
                             case 7:
                                 long.TryParse(text, out long id);
                                 santeiInfDetail.Id = id;
@@ -153,9 +163,164 @@ public static class ReadDataSanteiInf
         return santeiInfDetails;
     }
 
+    public static List<OdrInf> ReadOrderInf()
+    {
+        var rootPath = Environment.CurrentDirectory;
+        rootPath = rootPath.Remove(rootPath.IndexOf("bin"));
+
+        string fileName = Path.Combine(rootPath, "SampleData", "DataSample.xlsx");
+        var odrInfs = new List<OdrInf>();
+        using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+        {
+            var sheetData = GetworksheetBySheetName(spreadsheetDocument, "SANTEI_INF").WorksheetPart?.Worksheet.Elements<SheetData>().First();
+            string text;
+            if (sheetData != null)
+            {
+                foreach (var r in sheetData.Elements<Row>().Skip(1))
+                {
+                    var odrInf = new OdrInf();
+                    odrInf.CreateId = 1;
+                    odrInf.CreateDate = DateTime.UtcNow;
+                    odrInf.UpdateId = 1;
+                    odrInf.UpdateDate = DateTime.UtcNow;
+                    var numberCell = 1;
+                    foreach (var c in r.Elements<Cell>())
+                    {
+                        if (numberCell > 7)
+                        {
+                            break;
+                        }
+                        text = c.CellValue?.Text ?? string.Empty;
+
+                        switch (numberCell)
+                        {
+                            case 1:
+                                int.TryParse(text, out int hpId);
+                                odrInf.HpId = hpId;
+                                break;
+                            case 3:
+                                long.TryParse(text, out long raiinNo);
+                                odrInf.RaiinNo = raiinNo;
+                                break;
+                            case 4:
+                                long.TryParse(text, out long rpNo);
+                                odrInf.RpNo = rpNo;
+                                break;
+                            case 5:
+                                long.TryParse(text, out long rpEdaNo);
+                                odrInf.RpEdaNo = rpEdaNo;
+                                break;
+                            case 2:
+                                long.TryParse(text, out long ptId);
+                                odrInf.PtId = ptId;
+                                break;
+                            case 6:
+                                int.TryParse(text, out int sindate);
+                                odrInf.SinDate = sindate;
+                                break;
+                            case 6:
+                                int.TryParse(text, out int hokenPid);
+                                odrInf.HokenPid = hokenPid;
+                                break;
+                            case 6:
+                                int.TryParse(text, out int isDelete);
+                                odrInf.IsDeleted = isDelete;
+                                break;
+                            case 7:
+                                long.TryParse(text, out long id);
+                                odrInf.Id = id;
+                                break;
+                            default:
+                                break;
+                        }
+                        numberCell++;
+                    }
+                    odrInfs.Add(odrInf);
+                }
+            }
+        }
+        return odrInfs;
+    }
+
+    public static List<OdrInfDetail> ReadOrderInfDetail()
+    {
+        var rootPath = Environment.CurrentDirectory;
+        rootPath = rootPath.Remove(rootPath.IndexOf("bin"));
+
+        string fileName = Path.Combine(rootPath, "SampleData", "DataSample.xlsx");
+        var odrInfDetails = new List<OdrInfDetail>();
+        using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+        {
+            var sheetData = GetworksheetBySheetName(spreadsheetDocument, "SANTEI_INF").WorksheetPart?.Worksheet.Elements<SheetData>().First();
+            string text;
+            if (sheetData != null)
+            {
+                foreach (var r in sheetData.Elements<Row>().Skip(1))
+                {
+                    var odrInfDetail = new OdrInfDetail();
+                    var numberCell = 1;
+                    foreach (var c in r.Elements<Cell>())
+                    {
+                        if (numberCell > 7)
+                        {
+                            break;
+                        }
+                        text = c.CellValue?.Text ?? string.Empty;
+
+                        switch (numberCell)
+                        {
+                            case 1:
+                                int.TryParse(text, out int hpId);
+                                odrInfDetail.HpId = hpId;
+                                break;
+                            case 3:
+                                long.TryParse(text, out long raiinNo);
+                                odrInfDetail.RaiinNo = raiinNo;
+                                break;
+                            case 4:
+                                long.TryParse(text, out long rpNo);
+                                odrInfDetail.RpNo = rpNo;
+                                break;
+                            case 5:
+                                long.TryParse(text, out long rpEdaNo);
+                                odrInfDetail.RpEdaNo = rpEdaNo;
+                                break;
+                            case 5:
+                                int.TryParse(text, out int rowNo);
+                                odrInfDetail.RowNo = rowNo;
+                                break;
+                            case 2:
+                                long.TryParse(text, out long ptId);
+                                odrInfDetail.PtId = ptId;
+                                break;
+                            case 6:
+                                int.TryParse(text, out int sindate);
+                                odrInfDetail.SinDate = sindate;
+                                break;
+                            case 6:
+                                odrInfDetail.ItemCd = text;
+                                break;
+                            case 6:
+                                odrInfDetail.ItemName = text;
+                                break;
+                            case 6:
+                                int.TryParse(text, out int suryo);
+                                odrInfDetail.Suryo = suryo;
+                                break;
+                            default:
+                                break;
+                        }
+                        numberCell++;
+                    }
+                    odrInfDetails.Add(odrInfDetail);
+                }
+            }
+        }
+        return odrInfDetails;
+    }
+
     private static Worksheet GetworksheetBySheetName(SpreadsheetDocument spreadsheetDocument, string sheetName)
     {
-
         var workbookPart = spreadsheetDocument.WorkbookPart;
         string relationshipId = workbookPart.Workbook.Descendants<Sheet>().FirstOrDefault(s => s.Name.Equals(sheetName))?.Id;
 
