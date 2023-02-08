bb707c8fa70e925470e0e80c687cb301520bcaf0
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2023-02-08 14:46:34 +0700

    refactor code, save list file nextorder

---
+++ Domain/Models/NextOrder/FileItemModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Models.NextOrder;
+
+public class FileItemModel
+{
+    public FileItemModel(bool isUpdateFile, List<string> listFileItems)
+    {
+        IsUpdateFile = isUpdateFile;
+        ListFileItems = listFileItems;
+    }
+
+    public FileItemModel()
+    {
+        IsUpdateFile = false;
+        ListFileItems = new();
+    }
+
+    public bool IsUpdateFile { get; private set; }
+
+    public List<string> ListFileItems { get; private set; }
+}
