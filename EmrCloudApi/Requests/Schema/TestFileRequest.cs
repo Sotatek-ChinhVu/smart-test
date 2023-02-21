de1ec322c3865a9e6152356816fc777b67fba4e4
vantt <van.tran@sotatek.com>
2023-02-21 10:03:10 +0700

    ok

--- EmrCloudApi/Requests/Schema/TestFileRequest.cs
+++ EmrCloudApi/Requests/Schema/TestFileRequest.cs
@@ -2,10 +2,14 @@
 {
     public class TestFileRequest
     {
-        public string Id { get; set; }
+        public long PtId { get; set; }
+
+        public int HokenGrp { get; set; }
+
+        public int HokenId { get; set; }
+
+        public string UrlOldImage { get; set; } = string.Empty;
 
         public IFormFile File { get; set; }
-
-        public string Folder { get; set; }
     }
 }
