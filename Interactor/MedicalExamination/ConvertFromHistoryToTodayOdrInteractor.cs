c62141e78a559d2368ea14d2da5fd12cff10071b
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-03-02 17:21:08 +0700

    temp

---
+++ Interactor/MedicalExamination/ConvertFromHistoryToTodayOdrInteractor.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Models.OrdInfDetails;
+using Domain.Models.OrdInfs;
+using Domain.Models.TodayOdr;
+using UseCase.MedicalExamination.CheckedItemName;
+using UseCase.MedicalExamination.ConvertFromHistoryTodayOrder;
+
+namespace Interactor.MedicalExamination
+{
+    public class ConvertFromHistoryToTodayOdrInteractor : ICheckedItemNameInputPort
+    {
+        private readonly ITodayOdrRepository _todayOdrRepository;
+
+        public ConvertFromHistoryToTodayOdrInteractor(ITodayOdrRepository todayOdrRepository)
+        {
+            _todayOdrRepository = todayOdrRepository;
+        }
+
+        public ConvertFromHistoryTodayOrderOutputData Handle(CheckedItemNameInputData inputData)
+        {
+            try
+            {
+                if (inputData.)
+                {
+
+                }
+
+                return new CheckedItemNameOutputData(CheckedItemNameStatus.Successed, checkedName);
+            }
+            catch
+            {
+                return new CheckedItemNameOutputData(CheckedItemNameStatus.Failed, new());
+            }
+            finally
+            {
+                _todayOdrRepository.ReleaseResource();
+            }
+        }
+    }
+}
