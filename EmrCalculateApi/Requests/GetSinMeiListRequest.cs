19130b8e35b274d86926172d50bf99f36c37aba3
DuongLe <duong.le@sotatek.com>
2023-03-07 09:17:17 +0700

    Get SinMei list

---
+++ EmrCalculateApi/Requests/GetSinMeiListRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace EmrCalculateApi.Requests
+{
+    public class GetSinMeiListRequest
+    {
+        public List<long> RaiinNoList { get; set; } = new List<long>();
+
+        public int SinDate { get; set; }
+
+        public long PtId { get; set; }
+
+        public int HpId { get; set; }
+    }
+}
