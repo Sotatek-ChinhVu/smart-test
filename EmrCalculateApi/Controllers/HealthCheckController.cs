48515f0cbd7071058c68f1046bac358b57cf6cd7
DuongLe <duong.le@sotatek.com>
2023-03-13 10:18:50 +0700

    Add healthCheck api for Calculation Project

---
+++ EmrCalculateApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmrCalculateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
