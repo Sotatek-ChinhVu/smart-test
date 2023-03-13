ac0f92136e79230961a7785fe93f1e2154fe2b3c
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-03-13 16:58:21 +0700

    Revert

--- EmrCalculateApi/Ika/ViewModels/IkaCalculateCommonOdrDataViewModel.cs
+++ EmrCalculateApi/Ika/ViewModels/IkaCalculateCommonOdrDataViewModel.cs
@@ -161,10 +161,10 @@ namespace EmrCalculateApi.Ika.ViewModels
 
         public IkaCalculateCommonOdrDataViewModel(List<OrderInfo> todayOdrInfs, RaiinInfModel raiinInfModel, List<PtHokenPatternModel> ptHokenPatternModels, OdrInfFinder odrInfFinder, MasterFinder masterFinder, IkaCalculateFinder ikaCalculateFinder, IkaCalculateCommonMasterViewModel tenMstCommon, int hpId, long ptId, int sinDate, IEmrLogger emrLogger, ISystemConfigProvider systemConfigProvider)
         {
+            const string conFncName = nameof(IkaCalculateCommonOdrDataViewModel);
+
             _emrLogger = emrLogger;
             _systemConfigProvider = systemConfigProvider;
-
-            const string conFncName = nameof(IkaCalculateCommonOdrDataViewModel);
 
             _emrLogger.WriteLogStart(this, conFncName, "");
 
