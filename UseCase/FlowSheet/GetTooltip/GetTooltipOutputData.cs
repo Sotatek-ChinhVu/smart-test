144a18119bb3e7681b53cd0249cf7800491f3281
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-04-10 09:11:10 +0700

    temp

---
+++ UseCase/FlowSheet/GetTooltip/GetTooltipOutputData.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models.FlowSheet;
+using Domain.Models.RaiinListMst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase.Core.Sync.Core;
+
+namespace UseCase.FlowSheet.GetTooltip
+{
+    public class GetTooltipOutputData :IOutputData
+    {
+        public GetTooltipOutputData(List<(int, string)> values)
+        {
+            Values = values;
+        }
+
+        public List<(int, string)> Values { get; private set; }
+    }
+}
---
+++ UseCase/FlowSheet/GetTooltip/GetTooltipStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase.FlowSheet.GetTooltip
+{
+    public class GetTooltipStatus
+    {
+
+    }
+}
