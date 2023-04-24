25707ed8bbd1b0f24c841d235192d8e8cf0224e1
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-04-24 17:04:21 +0700

    temp

--- EmrCloudApi/Requests/MedicalExamination/OrderRealtimeCheckerRequest.cs
+++ EmrCloudApi/Requests/MedicalExamination/OrderRealtimeCheckerRequest.cs
@@ -1,16 +1,23 @@
 ﻿using CommonChecker.Models.OrdInf;
+using Domain.Models.Diseases;
+using UseCase.Family;
+using UseCase.MedicalExamination.GetCheckedOrder;
+using UseCase.MedicalExamination.SaveMedical;
 
 namespace EmrCloudApi.Requests.MedicalExamination
 {
     public class OrderRealtimeCheckerRequest
     {
-        public OrderRealtimeCheckerRequest(long ptId, int hpId, int sinDay, List<OrdInfoModel> currentListOdr, List<OrdInfoModel> listCheckingOrder)
+        public OrderRealtimeCheckerRequest(long ptId, int hpId, int sinDay, List<OrdInfoModel> currentListOdr, List<OrdInfoModel> listCheckingOrder, SpecialNoteItem specialNoteItem, PtDiseaseModel ptDiseaseModel)
         {
             PtId = ptId;
             HpId = hpId;
             SinDay = sinDay;
             CurrentListOdr = currentListOdr;
             ListCheckingOrder = listCheckingOrder;
+            SpecialNoteItem = specialNoteItem;
+            PtDiseaseModel = ptDiseaseModel;
+
         }
 
         public long PtId { get; set; }
@@ -21,5 +28,8 @@ namespace EmrCloudApi.Requests.MedicalExamination
 
         public List<OrdInfoModel> CurrentListOdr { get; set; }
         public List<OrdInfoModel> ListCheckingOrder { get; set; }
+        public SpecialNoteItem SpecialNoteItem { get; set; }
+        public PtDiseaseModel PtDiseaseModel { get; set; }
+        public List<FamilyItem> FamilyList { get; private set; }
     }
 }
