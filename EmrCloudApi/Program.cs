3161c3f3103a10296e6c580286e4aac55e48f362
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-05-09 09:06:05 +0700

    Done

--- EmrCloudApi/Program.cs
+++ EmrCloudApi/Program.cs
@@ -14,42 +14,45 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEmrOptions(builder.Configuration);
-//builder.Services.AddSignalR();
 builder.Services.AddMemoryCache();
 
+#if DEBUG
+builder.Services.AddSignalR();
+#else
 // Setup signalR
-//builder.Services.AddSignalR()
-//        .AddStackExchangeRedis(o =>
-//        {
-//            o.ConnectionFactory = async writer =>
-//            {
-//                var config = new ConfigurationOptions
-//                {
-//                    AbortOnConnectFail = false
-//                };
+builder.Services.AddSignalR()
+        .AddStackExchangeRedis(o =>
+        {
+            o.ConnectionFactory = async writer =>
+            {
+                var config = new ConfigurationOptions
+                {
+                    AbortOnConnectFail = false
+                };
 
-//                string redisHost = builder.Configuration["RedisHost"];
-//                string redisPort = builder.Configuration["RedisPort"];
-//                config.EndPoints.Add(redisHost, int.Parse(redisPort));
+                string redisHost = builder.Configuration["RedisHost"];
+                string redisPort = builder.Configuration["RedisPort"];
+                config.EndPoints.Add(redisHost, int.Parse(redisPort));
 
-//                var connection = await ConnectionMultiplexer.ConnectAsync(config, writer);
-//                connection.ConnectionFailed += (_, e) =>
-//                {
-//                    Console.WriteLine("Connection to Redis failed.");
-//                };
+                var connection = await ConnectionMultiplexer.ConnectAsync(config, writer);
+                connection.ConnectionFailed += (_, e) =>
+                {
+                    Console.WriteLine("Connection to Redis failed.");
+                };
 
-//                if (!connection.IsConnected)
-//                {
-//                    Console.WriteLine("Did not connect to Redis.");
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Connected to Redis.");
-//                }
+                if (!connection.IsConnected)
+                {
+                    Console.WriteLine("Did not connect to Redis.");
+                }
+                else
+                {
+                    Console.WriteLine("Connected to Redis.");
+                }
 
-//                return connection;
-//            };
-//        });
+                return connection;
+            };
+        });
+#endif
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
