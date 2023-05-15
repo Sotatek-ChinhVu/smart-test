412f592ad5c20519cb337df18fd824f7c2c23867
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2023-05-15 09:09:57 +0700

    add hoken name to api get insurance rece inf

--- UseCase/Receipt/GetInsuranceReceInfList/GetInsuranceReceInfListOutputData.cs
+++ UseCase/Receipt/GetInsuranceReceInfList/GetInsuranceReceInfListOutputData.cs
@@ -5,11 +5,16 @@ namespace UseCase.Receipt.GetInsuranceReceInfList;
 
 public class GetInsuranceReceInfListOutputData : IOutputData
 {
-    public GetInsuranceReceInfListOutputData(InsuranceReceInfModel model, string insuranceName, GetInsuranceReceInfListStatus status)
+    public GetInsuranceReceInfListOutputData(string hokenName, int sinYm, InsuranceReceInfModel model, string insuranceName, GetInsuranceReceInfListStatus status)
     {
+        HokenName = hokenName;
         InsuranceReceInf = new InsuranceReceInfItem(model, insuranceName);
         Status = status;
+        SinYm = sinYm;
     }
+    public string HokenName { get; private set; }
+
+    public int SinYm { get; private set; }
 
     public InsuranceReceInfItem InsuranceReceInf { get; private set; }
 
