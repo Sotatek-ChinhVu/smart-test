4c2287ce6453928a3c416fca0aa9047fb70f94f4
vantt <van.tran@sotatek.com>
2023-05-15 11:16:55 +0700

    update code

--- EmrCloudApi/Requests/MstItem/CheckIsTenMstUsedRequest.cs
+++ EmrCloudApi/Requests/MstItem/CheckIsTenMstUsedRequest.cs
@@ -2,17 +2,10 @@
 {
     public class CheckIsTenMstUsedRequest
     {
-        public CheckIsTenMstUsedRequest(string itemCd, int startDate, int endDate)
-        {
-            ItemCd = itemCd;
-            StartDate = startDate;
-            EndDate = endDate;
-        }
+        public string ItemCd { get; set; } = string.Empty;
 
-        public string ItemCd { get; private set; }
+        public int StartDate { get; set; }
 
-        public int StartDate { get; private set; }
-
-        public int EndDate { get; private set; }
+        public int EndDate { get; set; }
     }
 }
