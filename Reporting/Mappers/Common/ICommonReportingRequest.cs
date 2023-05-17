39e4f6f6ad597fbc2c3f3df96894930151a6ba49
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2023-05-17 14:34:27 +0700

    add listText data to common API

--- Reporting/Mappers/Common/ICommonReportingRequest.cs
+++ Reporting/Mappers/Common/ICommonReportingRequest.cs
@@ -19,5 +19,9 @@ namespace Reporting.Mappers
         int GetReportType();
 
         Dictionary<string, string> GetFileNamePageMap();
+
+        string GetJobName();
+
+        List<ListTextObject> GetListTextData();
     }
 }
