d448de24c677aa9f8650af6de91b8e4fd7aff412
vantt <van.tran@sotatek.com>
2023-05-17 09:57:04 +0700

    update type msg

--- EmrCloudApi/Responses/Receipt/CreateUKEFileResponse.cs
+++ EmrCloudApi/Responses/Receipt/CreateUKEFileResponse.cs
@@ -4,14 +4,17 @@ namespace EmrCloudApi.Responses.Receipt
 {
     public class CreateUKEFileResponse
     {
-        public CreateUKEFileResponse(CreateUKEFileStatus status, string message)
+        public CreateUKEFileResponse(CreateUKEFileStatus status, string message, int typeMessage)
         {
             Status = status;
             Message = message;
+            TypeMessage = typeMessage;
         }
 
         public CreateUKEFileStatus Status { get; private set; }
 
         public string Message { get; private set; }
+
+        public int TypeMessage { get; private set; }
     }
 }
