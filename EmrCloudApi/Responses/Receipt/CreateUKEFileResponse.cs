9b5be075ee0bbae7d0c58a52975180c651754352
vantt <van.tran@sotatek.com>
2023-05-17 10:09:36 +0700

    back code

--- EmrCloudApi/Responses/Receipt/CreateUKEFileResponse.cs
+++ EmrCloudApi/Responses/Receipt/CreateUKEFileResponse.cs
@@ -4,17 +4,14 @@ namespace EmrCloudApi.Responses.Receipt
 {
     public class CreateUKEFileResponse
     {
-        public CreateUKEFileResponse(CreateUKEFileStatus status, string message, int typeMessage)
+        public CreateUKEFileResponse(CreateUKEFileStatus status, string message)
         {
             Status = status;
             Message = message;
-            TypeMessage = typeMessage;
         }
 
         public CreateUKEFileStatus Status { get; private set; }
 
         public string Message { get; private set; }
-
-        public int TypeMessage { get; private set; }
     }
 }
