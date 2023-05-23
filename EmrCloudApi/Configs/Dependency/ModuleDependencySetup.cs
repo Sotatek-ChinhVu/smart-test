a2401c6e458c49d502a96c6e18a07fb3e8f48fe5
Sotatek-MinhLe <minh.le@sotatek.com>
2023-05-23 23:09:54 +0700

    Report: P30KoukiSeikyuCoReportService

--- EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
+++ EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
@@ -698,6 +698,7 @@ namespace EmrCloudApi.Configs.Dependency
             services.AddTransient<ICoKoukiSeikyuFinder, CoKoukiSeikyuFinder>();
             services.AddTransient<IP28KoukiSeikyuCoReportService, P28KoukiSeikyuCoReportService>();
             services.AddTransient<IP29KoukiSeikyuCoReportService, P29KoukiSeikyuCoReportService>();
+            services.AddTransient<IP30KoukiSeikyuCoReportService, P30KoukiSeikyuCoReportService>();
 
             //call Calculate API
             services.AddTransient<ICalculateService, CalculateService>();
--- Reporting/ReceiptPrint/Service/ReceiptPrintService.cs
+++ Reporting/ReceiptPrint/Service/ReceiptPrintService.cs
@@ -20,11 +20,13 @@ namespace Reporting.ReceiptPrint.Service
         private readonly IP11KokhoSokatuCoReportService _p11KokhoSokatuCoReportService;
         private readonly IP28KoukiSeikyuCoReportService _p28KoukiSeikyuCoReportService;
         private readonly IP29KoukiSeikyuCoReportService _p29KoukiSeikyuCoReportService;
+        private readonly IP30KoukiSeikyuCoReportService _p30KoukiSeikyuCoReportService;
 
         public ReceiptPrintService(IP28KokhoSokatuCoReportService p28KokhoSokatuCoReportService
             , IP11KokhoSokatuCoReportService p11KokhoSokatuCoReportService
             , IP28KoukiSeikyuCoReportService p28KoukiSeikyuCoReportService
             , IP29KoukiSeikyuCoReportService p29KoukiSeikyuCoReportService
+            , IP30KoukiSeikyuCoReportService p30KoukiSeikyuCoReportService
 
                                   )
         {
@@ -32,6 +34,7 @@ namespace Reporting.ReceiptPrint.Service
             _p11KokhoSokatuCoReportService = p11KokhoSokatuCoReportService;
             _p28KoukiSeikyuCoReportService = p28KoukiSeikyuCoReportService;
             _p29KoukiSeikyuCoReportService = p29KoukiSeikyuCoReportService;
+            _p30KoukiSeikyuCoReportService = p30KoukiSeikyuCoReportService;
         }
 
         public CommonReportingRequestModel GetReceiptPrint(int hpId, int prefNo, int reportId, int reportEdaNo, int dataKbn, int ptId, int seikyuYm, int sinYm, int hokenId)
@@ -51,6 +54,9 @@ namespace Reporting.ReceiptPrint.Service
             }else if (prefNo == 29 && reportId == 104 && reportEdaNo == 0)
             {
                 return _p29KoukiSeikyuCoReportService.GetP29KoukiSeikyuReportingData(hpId, seikyuYm, seikyuType);
+            }else if (prefNo == 30 && reportId == 104 && reportEdaNo == 0)
+            {
+                return _p30KoukiSeikyuCoReportService.GetP30KoukiSeikyuReportingData(hpId, seikyuYm, seikyuType);
             }
 
             return new();
