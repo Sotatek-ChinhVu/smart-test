f9abd6721008262e062c37c120248317acc3bde2
Sotatek-QuanTo <quan.to@sotatek.com>
2023-05-24 17:37:20 +0700

    Karte3

---
+++ Reporting/Karte3/Enum/CoKarte3Column.cs
@@ -0,0 +1,20 @@
+﻿namespace Reporting.Karte3.Enum
+{
+    class CoKarte3Column
+    {
+        public enum Karte3Column
+        {
+            Sinsatu = 0,
+            Zaitaku = 1,
+            IgakuKanri = 2,
+            Toyaku = 3,
+            Chusya = 4,
+            Shoti = 5,
+            Ope = 6,
+            Kensa = 7,
+            Gazo = 8,
+            Sonota = 9,
+            HokenGai = 10
+        }
+    }
+}
