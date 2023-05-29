3498e8c87006940b6ceb3c2d4985eb7355b84b92
vantt <van.tran@sotatek.com>
2023-05-29 16:41:33 +0700

    api GetHokenMasterReadOnly

---
+++ UseCase/InsuranceMst/GetHokenMasterReadOnly/GetHokenMasterReadOnlyOutputData.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models.InsuranceMst;
+using UseCase.Core.Sync.Core;
+
+namespace UseCase.InsuranceMst.GetHokenMasterReadOnly
+{
+    public class GetHokenMasterReadOnlyOutputData : IOutputData
+    {
+        public GetHokenMasterReadOnlyOutputData(GetHokenMasterReadOnlyStatus status, HokenMstModel hokenMaster)
+        {
+            Status = status;
+            HokenMaster = hokenMaster;
+        }
+
+        public GetHokenMasterReadOnlyStatus Status { get; private set; }
+
+        public HokenMstModel HokenMaster { get; private set; }
+    }
+}
