db923d9e26a216076c3a0893cc81cb13b09d9b6e
Sotatek-MinhLe <minh.le@sotatek.com>
2023-05-29 16:07:39 +0700

    Update file

--- EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
+++ EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
@@ -710,6 +710,7 @@ namespace EmrCloudApi.Configs.Dependency
             services.AddTransient<ICoSijisenFinder, CoSijisenFinder>();
             services.AddTransient<ICoAccountingCardFinder, CoAccountingCardFinder>();
             services.AddTransient<IAccountingCardCoReportService, AccountingCardCoReportService>();
+            services.AddTransient<IP45KoukiSeikyuCoReportService, P45KoukiSeikyuCoReportService>();
 
             //call Calculate API
             services.AddTransient<ICalculateService, CalculateService>();
