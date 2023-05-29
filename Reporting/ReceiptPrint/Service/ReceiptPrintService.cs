2c7a8ef515ac9d04fe5da37858985f798dad516c
Sotatek-MinhLe <minh.le@sotatek.com>
2023-05-29 21:47:03 +0700

    P12KoukiSeikyuCoReportService

--- Reporting/ReceiptPrint/Service/ReceiptPrintService.cs
+++ Reporting/ReceiptPrint/Service/ReceiptPrintService.cs
@@ -20,11 +20,12 @@ namespace Reporting.ReceiptPrint.Service
         private readonly IP11KokhoSokatuCoReportService _p11KokhoSokatuCoReportService;
         private readonly IP28KoukiSeikyuCoReportService _p28KoukiSeikyuCoReportService;
         private readonly IP29KoukiSeikyuCoReportService _p29KoukiSeikyuCoReportService;
-
+        private readonly IP12KoukiSeikyuCoReportService _p12KoukiSeikyuCoReportService;
         public ReceiptPrintService(IP28KokhoSokatuCoReportService p28KokhoSokatuCoReportService
             , IP11KokhoSokatuCoReportService p11KokhoSokatuCoReportService
             , IP28KoukiSeikyuCoReportService p28KoukiSeikyuCoReportService
             , IP29KoukiSeikyuCoReportService p29KoukiSeikyuCoReportService
+            , IP12KoukiSeikyuCoReportService p12KoukiSeikyuCoReportService
 
                                   )
         {
@@ -32,6 +33,7 @@ namespace Reporting.ReceiptPrint.Service
             _p11KokhoSokatuCoReportService = p11KokhoSokatuCoReportService;
             _p28KoukiSeikyuCoReportService = p28KoukiSeikyuCoReportService;
             _p29KoukiSeikyuCoReportService = p29KoukiSeikyuCoReportService;
+            _p12KoukiSeikyuCoReportService = p12KoukiSeikyuCoReportService;
         }
 
         public CommonReportingRequestModel GetReceiptPrint(int hpId, int prefNo, int reportId, int reportEdaNo, int dataKbn, int ptId, int seikyuYm, int sinYm, int hokenId)
@@ -51,6 +53,9 @@ namespace Reporting.ReceiptPrint.Service
             }else if (prefNo == 29 && reportId == 104 && reportEdaNo == 0)
             {
                 return _p29KoukiSeikyuCoReportService.GetP29KoukiSeikyuReportingData(hpId, seikyuYm, seikyuType);
+            }else if (prefNo == 12 && reportId == 104 && reportEdaNo == 0)
+            {
+                return _p12KoukiSeikyuCoReportService.GetP12KoukiSeikyuReportingData(hpId, seikyuYm, seikyuType);
             }
 
             return new();
