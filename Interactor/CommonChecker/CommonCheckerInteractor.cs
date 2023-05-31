1c2b969e6ea12863dc57c7e6cc48aa58fdeba769
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-05-31 11:50:39 +0700

    refactor realtime checker

--- Interactor/CommonChecker/CommonCheckerInteractor.cs
+++ Interactor/CommonChecker/CommonCheckerInteractor.cs
@@ -1,4 +1,5 @@
 ﻿using Interactor.CommonChecker.CommonMedicalCheck;
+using System.Diagnostics;
 using UseCase.CommonChecker;
 
 namespace Interactor.CommonChecker
@@ -14,10 +15,28 @@ namespace Interactor.CommonChecker
 
         public GetOrderCheckerOutputData Handle(GetOrderCheckerInputData inputData)
         {
+            var timer = new Stopwatch();
+            timer.Start();
+
             var checkedResult = _commonMedicalCheck.CheckListOrder(inputData.HpId, inputData.PtId, inputData.SinDay, inputData.CurrentListOdr, inputData.ListCheckingOrder, inputData.SpecialNoteItem, inputData.PtDiseaseModels, inputData.FamilyItems, inputData.IsDataOfDb, inputData.RealTimeCheckerCondition);
 
+            //timer.Stop();
+
+            TimeSpan timeTaken = timer.Elapsed;
+            string foo = "Time Check: " + timeTaken.ToString(@"m\:ss\.fff");
+
+            Console.WriteLine(foo);
+
+            //timer = new Stopwatch();
+            //timer.Start();
             var result = _commonMedicalCheck.GetErrorDetails(inputData.HpId, inputData.PtId, inputData.SinDay, checkedResult);
 
+            timer.Stop();
+
+            timeTaken = timer.Elapsed;
+            foo = "Time Get: " + timeTaken.ToString(@"m\:ss\.fff");
+
+            Console.WriteLine(foo);
             if (checkedResult == null || checkedResult.Count == 0)
             {
                 return new GetOrderCheckerOutputData(new(), GetOrderCheckerStatus.Successed);
