257ed30c54b941c914b3e3ac8b1823c14605608a
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-06-01 17:54:27 +0700

    temp

---
+++ UseCase/NextOrder/CheckNextOrdHaveOdr/CheckNextOrdHaveOdrOutputData.cs
@@ -0,0 +1,21 @@
+﻿using UseCase.Core.Sync.Core;
+using static Helper.Constants.KarteConst;
+using static Helper.Constants.NextOrderConst;
+using static Helper.Constants.OrderInfConst;
+using static Helper.Constants.RsvkrtByomeiConst;
+
+namespace UseCase.NextOrder.CheckNextOrdHaveOdr
+{
+    public class CheckNextOrdHaveOdrOutputData : IOutputData
+    {
+        public CheckNextOrdHaveOdrOutputData(CheckNextOrdHaveOdrStatus status, bool result)
+        {
+            Status = status;
+            Result = result;
+        }
+
+        public CheckNextOrdHaveOdrStatus Status { get; private set; }
+
+        public bool Result { get; private set; }
+    }
+}
---
+++ UseCase/NextOrder/CheckNextOrdHaveOdr/CheckNextOrdHaveOdrStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace UseCase.NextOrder.CheckNextOrdHaveOdr
+{
+    public enum CheckNextOrdHaveOdrStatus : byte
+    {
+        Successed = 1,
+        InvalidHpId = 2,
+        InvalidPtId = 3,
+        InvalidUserId = 4,
+        InvalidRsvkrtNo = 5,
+        InvalidRsvkrtDate = 6,
+        Failed = 7
+    }
+}
