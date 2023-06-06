9087f44939696fedcb128190ef2bc7916131f635
Sotatek-QuanTo <quan.to@sotatek.com>
2023-06-06 16:38:28 +0700

    unit test DiseaseChecker

---
+++ CloudTest/CommonChecker/Services/DiseaseCheckerTest.cs
@@ -0,0 +1,72 @@
+﻿using CommonChecker.Models.OrdInf;
+using CommonChecker.Models.OrdInfDetailModel;
+using CommonCheckers.OrderRealtimeChecker.Enums;
+using CommonCheckers.OrderRealtimeChecker.Models;
+using CommonCheckers.OrderRealtimeChecker.Services;
+using Entity.Tenant;
+
+namespace CloudUnitTest.CommonChecker.Services
+{
+    public class DiseaseCheckerTest : BaseUT
+    {
+        /// <summary>
+        /// Test DiseaseCheck With Setting Value is 5
+        /// </summary>
+        [Test]
+        public void CheckDiseaseChecker_001_ReturnsEmptyList_WhenFollowSettingValue()
+        {
+            //Setup
+            var ordInfDetails = new List<OrdInfoDetailModel>()
+            {
+                new OrdInfoDetailModel("id1", 20, "611170008", "・ｼ・・ｽ・・ｽ・・・ｻ・・ｫ・・ｷ・・ｳ・・", 1, "・・", 0, 2, 0, 1, 0, "1124017F4", "", "Y", 0),
+                new OrdInfoDetailModel("id2", 21, "Y101", "・・・・ｼ・・・ｵｷ・ｺ・・・・", 2, "・・･・・・", 0, 0, 0, 0, 1, "", "", "", 1),
+            };
+
+            var odrInfoModel = new List<OrdInfoModel>()
+            {
+                new OrdInfoModel(21, 0, ordInfDetails)
+            };
+
+            var unitCheckerForOrderListResult = new UnitCheckerForOrderListResult<OrdInfoModel, OrdInfoDetailModel>(
+                                                                    RealtimeCheckerType.Disease, odrInfoModel, 20230101, 111, new(new(), new(), new()), new(), new(), true);
+
+            var tenantTracking = TenantProvider.GetTrackingTenantDataContext();
+            var diseaseChecker = new DiseaseChecker<OrdInfoModel, OrdInfoDetailModel>();
+            diseaseChecker.HpID = 1;
+            diseaseChecker.PtID = 111;
+            diseaseChecker.Sinday = 20230101;
+            diseaseChecker.DataContext = TenantProvider.GetNoTrackingDataContext();
+
+            var systemConf = tenantTracking.SystemConfs.FirstOrDefault(p => p.HpId == 1 && p.GrpCd == 2027 && p.GrpEdaNo == 2);
+            var temp = systemConf?.Val ?? 0;
+            if (systemConf != null)
+            {
+                systemConf.Val = 5;
+            }
+            else
+            {
+                systemConf = new SystemConf
+                {
+                    HpId = 1,
+                    GrpCd = 2027,
+                    GrpEdaNo = 2,
+                    CreateDate = DateTime.UtcNow,
+                    UpdateDate = DateTime.UtcNow,
+                    CreateId = 2,
+                    UpdateId = 2,
+                    Val = 5
+                };
+                tenantTracking.SystemConfs.Add(systemConf);
+            }
+            tenantTracking.SaveChanges();
+
+            //// Act
+            var result = diseaseChecker.HandleCheckOrderList(unitCheckerForOrderListResult);
+            //// Assert
+            Assert.True(result.ErrorOrderList.Count == 0);
+
+            if (systemConf != null) systemConf.Val = temp;
+            tenantTracking.SaveChanges();
+        }
+    }
+}
---
+++ CloudTest/SampleData/CommonCheckerData.cs
@@ -0,0 +1,321 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Entity.Tenant;
+
+namespace CloudUnitTest.SampleData
+{
+    public static class CommonCheckerData
+    {
+        public static List<PtByomei> ReadPtByomei()
+        {
+            var rootPath = Environment.CurrentDirectory;
+            rootPath = rootPath.Remove(rootPath.IndexOf("bin"));
+
+            string fileName = Path.Combine(rootPath, "SampleData", "CommonCheckerTest.xlsx");
+            var ptByomeis = new List<PtByomei>();
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+
+                var workbookPart = spreadsheetDocument.WorkbookPart;
+                var sheetData = GetworksheetBySheetName(spreadsheetDocument, "PT_BYOMEI").WorksheetPart?.Worksheet.Elements<SheetData>().First();
+                string text;
+                if (sheetData != null)
+                {
+                    foreach (var r in sheetData.Elements<Row>().Skip(1))
+                    {
+                        var ptByomei = new PtByomei();
+                        foreach (var c in r.Elements<Cell>())
+                        {
+                            text = c.CellValue?.Text ?? string.Empty;
+                            if (c.DataType != null && c.DataType == CellValues.SharedString)
+                            {
+                                var stringId = Convert.ToInt32(c.InnerText);
+                                text = workbookPart?.SharedStringTablePart?.SharedStringTable.Elements<SharedStringItem>().ElementAt(stringId).InnerText ?? string.Empty;
+                            }
+                            var columnName = GetColumnName(c.CellReference?.ToString() ?? string.Empty);
+
+                            switch (columnName)
+                            {
+                                case "A":
+                                    int.TryParse(text, out int hpId);
+                                    ptByomei.HpId = hpId;
+                                    break;
+                                case "B":
+                                    int.TryParse(text, out int ptId);
+                                    ptByomei.PtId = ptId;
+                                    break;
+                                case "C":
+                                    int.TryParse(text, out int seqNo);
+                                    ptByomei.SeqNo = seqNo;
+                                    break;
+                                case "D":
+                                    ptByomei.ByomeiCd = text;
+                                    break;
+                                case "E":
+                                    int.TryParse(text, out int sortNo);
+                                    ptByomei.SortNo = sortNo;
+                                    break;
+                                case "F":
+                                    ptByomei.SyusyokuCd1 = text;
+                                    break;
+                                case "G":
+                                    ptByomei.SyusyokuCd2 = text;
+                                    break;
+                                case "H":
+                                    ptByomei.SyusyokuCd3 = text;
+                                    break;
+                                case "I":
+                                    ptByomei.SyusyokuCd4 = text;
+                                    break;
+                                case "J":
+                                    ptByomei.SyusyokuCd5 = text;
+                                    break;
+                                case "K":
+                                    ptByomei.SyusyokuCd6 = text;
+                                    break;
+                                case "L":
+                                    ptByomei.SyusyokuCd7 = text;
+                                    break;
+                                case "M":
+                                    ptByomei.SyusyokuCd8 = text;
+                                    break;
+                                case "N":
+                                    ptByomei.SyusyokuCd8 = text;
+                                    break;
+                                case "O":
+                                    ptByomei.SyusyokuCd10 = text;
+                                    break;
+                                case "P":
+                                    ptByomei.SyusyokuCd11 = text;
+                                    break;
+                                case "Q":
+                                    ptByomei.SyusyokuCd12 = text;
+                                    break;
+                                case "R":
+                                    ptByomei.SyusyokuCd13 = text;
+                                    break;
+                                case "S":
+                                    ptByomei.SyusyokuCd14 = text;
+                                    break;
+                                case "T":
+                                    ptByomei.SyusyokuCd15 = text;
+                                    break;
+                                case "U":
+                                    ptByomei.SyusyokuCd16 = text;
+                                    break;
+                                case "V":
+                                    ptByomei.SyusyokuCd17 = text;
+                                    break;
+                                case "W":
+                                    ptByomei.SyusyokuCd18 = text;
+                                    break;
+                                case "X":
+                                    ptByomei.SyusyokuCd19 = text;
+                                    break;
+                                case "Y":
+                                    ptByomei.SyusyokuCd20 = text;
+                                    break;
+                                case "Z":
+                                    ptByomei.SyusyokuCd21 = text;
+                                    break;
+                                case "AA":
+                                    ptByomei.Byomei = text;
+                                    break;
+                                case "AB":
+                                    int.TryParse(text, out int startDate);
+                                    ptByomei.StartDate = startDate;
+                                    break;
+                                case "AC":
+                                    int.TryParse(text, out int tenkiKbn);
+                                    ptByomei.TenkiKbn = tenkiKbn;
+                                    break;
+                                case "AD":
+                                    int.TryParse(text, out int tenkiDate);
+                                    ptByomei.TenkiDate = tenkiDate;
+                                    break;
+                                case "AE":
+                                    int.TryParse(text, out int syubyoKbn);
+                                    ptByomei.SyubyoKbn = syubyoKbn;
+                                    break;
+                                case "AF":
+                                    int.TryParse(text, out int sikkanKbn);
+                                    ptByomei.SikkanKbn = sikkanKbn;
+                                    break;
+                                case "AG":
+                                    int.TryParse(text, out int nanByoCd);
+                                    ptByomei.NanByoCd = nanByoCd;
+                                    break;
+                                case "AH":
+                                    ptByomei.HosokuCmt = text;
+                                    break;
+                                case "AI":
+                                    int.TryParse(text, out int hokenPid);
+                                    ptByomei.HokenPid = hokenPid;
+                                    break;
+                                case "AJ":
+                                    int.TryParse(text, out int togetuByomei);
+                                    ptByomei.TogetuByomei = togetuByomei;
+                                    break;
+                                case "AK":
+                                    int.TryParse(text, out int isNodspRece);
+                                    ptByomei.IsNodspRece = isNodspRece;
+                                    break;
+                                case "AL":
+                                    int.TryParse(text, out int isNodspKarte);
+                                    ptByomei.IsNodspKarte = isNodspKarte;
+                                    break;
+                                case "AM":
+                                    int.TryParse(text, out int isDeleted);
+                                    ptByomei.IsDeleted = isDeleted;
+                                    break;
+                                case "AN":
+
+                                    ptByomei.CreateDate = DateTime.Parse(text);
+                                    break;
+                                case "AO":
+                                    int.TryParse(text, out int createId);
+                                    ptByomei.CreateId = createId;
+                                    break;
+                                case "AP":
+                                    ptByomei.CreateMachine = text;
+                                    break;
+                                case "AQ":
+                                    ptByomei.UpdateDate = DateTime.Parse(text);
+                                    break;
+                                case "AR":
+                                    int.TryParse(text, out int updateId);
+                                    ptByomei.UpdateId = updateId;
+                                    break;
+                                case "AS":
+                                    ptByomei.UpdateMachine = text;
+                                    break;
+                                case "AT":
+                                    int.TryParse(text, out int id);
+                                    ptByomei.Id = id;
+                                    break;
+                                case "AU":
+                                    int.TryParse(text, out int isImportant);
+                                    ptByomei.IsImportant = isImportant;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        ptByomeis.Add(ptByomei);
+                    }
+                }
+            }
+
+            return ptByomeis;
+        }
+
+        public static List<SystemConf> ReadSystemConf()
+        {
+            var rootPath = Environment.CurrentDirectory;
+            rootPath = rootPath.Remove(rootPath.IndexOf("bin"));
+
+            string fileName = Path.Combine(rootPath, "SampleData", "CommonCheckerTest.xlsx");
+            var systemConfs = new List<SystemConf>();
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+
+                var workbookPart = spreadsheetDocument.WorkbookPart;
+                var sheetData = GetworksheetBySheetName(spreadsheetDocument, "SYSTEM_CONF").WorksheetPart?.Worksheet.Elements<SheetData>().First();
+                string text;
+                if (sheetData != null)
+                {
+                    foreach (var r in sheetData.Elements<Row>().Skip(1))
+                    {
+                        var systemConf = new SystemConf();
+                        foreach (var c in r.Elements<Cell>())
+                        {
+                            text = c.CellValue?.Text ?? string.Empty;
+                            if (c.DataType != null && c.DataType == CellValues.SharedString)
+                            {
+                                var stringId = Convert.ToInt32(c.InnerText);
+                                text = workbookPart?.SharedStringTablePart?.SharedStringTable.Elements<SharedStringItem>().ElementAt(stringId).InnerText ?? string.Empty;
+                            }
+                            var columnName = GetColumnName(c.CellReference?.ToString() ?? string.Empty);
+
+                            switch (columnName)
+                            {
+                                case "A":
+                                    int.TryParse(text, out int hpId);
+                                    systemConf.HpId = hpId;
+                                    break;
+                                case "B":
+                                    int.TryParse(text, out int ptId);
+                                    systemConf.GrpCd = ptId;
+                                    break;
+                                case "C":
+                                    int.TryParse(text, out int seqNo);
+                                    systemConf.GrpEdaNo = seqNo;
+                                    break;
+                                case "D":
+                                    int.TryParse(text, out int val);
+                                    systemConf.Val = val;
+                                    break;
+                                case "E":
+                                    systemConf.Param = text;
+                                    break;
+                                case "F":
+                                    systemConf.Biko = text;
+                                    break;
+                                case "G":
+                                    systemConf.CreateDate = DateTime.Now;
+                                    break;
+                                case "H":
+                                    int.TryParse(text, out int createId);
+                                    systemConf.CreateId = createId;
+                                    break;
+                                case "I":
+                                    systemConf.CreateMachine = text;
+                                    break;
+                                case "J":
+                                    systemConf.UpdateDate = DateTime.Now;
+                                    break;
+                                case "K":
+                                    int.TryParse(text, out int updateId);
+                                    systemConf.UpdateId = updateId;
+                                    break;
+                                case "L":
+                                    systemConf.UpdateMachine = text;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        systemConfs.Add(systemConf);
+                    }
+                }
+            }
+
+            return systemConfs;
+        }
+
+        private static Worksheet GetworksheetBySheetName(SpreadsheetDocument spreadsheetDocument, string sheetName)
+        {
+
+            var workbookPart = spreadsheetDocument.WorkbookPart;
+            StringValue relationshipId = workbookPart?.Workbook.Descendants<Sheet>().FirstOrDefault(s => s.Name != null && s.Name.Equals(sheetName))?.Id ?? string.Empty;
+
+            var worksheet = workbookPart != null ? ((WorksheetPart)workbookPart.GetPartById(relationshipId.Value ?? string.Empty)).Worksheet : new();
+
+            return worksheet;
+        }
+
+        private static string GetColumnName(string text)
+        {
+            var check = int.TryParse(text.Skip(1).FirstOrDefault().ToString(), out int number);
+            if (check)
+            {
+                return text.FirstOrDefault().ToString();
+            }
+            else
+            {
+                return text.Substring(0, 2);
+            }
+        }
+    }
+}
