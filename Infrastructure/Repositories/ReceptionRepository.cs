08c7dec7bc8000ecb144cad3910f1492afac03da
Sotatek-QuanTo <quan.to@sotatek.com>
2023-06-12 17:17:51 +0700

    Save Status Reception when yokaku = 1

--- Infrastructure/Repositories/ReceptionRepository.cs
+++ Infrastructure/Repositories/ReceptionRepository.cs
@@ -241,6 +241,15 @@ namespace Infrastructure.Repositories
                     entity.UpdateDate = CIUtil.GetJapanDateTimeNow();
                     entity.UpdateId = userId;
                 }
+
+                if (entity.IsYoyaku == 1)
+                {
+                    if (string.IsNullOrEmpty(entity.UketukeTime))
+                    {
+                        entity.UketukeTime = model.UketukeTime;
+                    }
+                    entity.Status = model.Status;
+                }
             }
 
             void ResetOyaRaiinNo(int hpId, int userId, long ptId, long raiinNo)
