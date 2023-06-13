74c919c9d1217bd871e21a1532dbaee8c95fd596
Sotatek-MinhLe <minh.le@sotatek.com>
2023-06-13 16:17:31 +0700

    P13KokhoSeikyuCoReportService

---
+++ Reporting/Sokatu/KokhoSeikyu/Service/IP13KokhoSeikyuCoReportService.cs
@@ -0,0 +1,9 @@
+﻿using Reporting.Mappers.Common;
+using Reporting.Structs;
+
+namespace Reporting.Sokatu.KokhoSeikyu.Service;
+
+public interface IP13KokhoSeikyuCoReportService
+{
+    CommonReportingRequestModel GetP13KokhoSeikyuReportingData(int hpId, int seikyuYm, SeikyuType seikyuType);
+}
