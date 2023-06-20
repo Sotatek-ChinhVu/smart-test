436b6ec04a4acb8e897fb01dbd70b55145066ef5
Sotatek-MinhLe <minh.le@sotatek.com>
2023-06-20 10:19:28 +0700

    Remove file

--- Reporting/Sokatu/KokhoSeikyu/Service/IP25KokhoSeikyuCoReportService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Reporting.Mappers.Common;
-using Reporting.Structs;
-
-namespace Reporting.Sokatu.KokhoSeikyu.Service;
-
-public interface IP25KokhoSeikyuCoReportService
-{
-    CommonReportingRequestModel GetP25KokhoSeikyuReportingData(int hpId, int seikyuYm, SeikyuType seikyuType);
-}
