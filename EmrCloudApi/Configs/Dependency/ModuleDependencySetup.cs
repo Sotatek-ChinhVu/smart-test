04777fd6522831eedf0a265724c7658fb988c357
Sotatek-MinhLe <minh.le@sotatek.com>
2023-06-21 17:31:13 +0700

    P22WelfareSeikyuCoReportService

--- EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
+++ EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
@@ -780,6 +780,7 @@ namespace EmrCloudApi.Configs.Dependency
             services.AddTransient<ICoHpInfFinder, CoHpInfFinder>();
             services.AddTransient<IReceiptPrintService, ReceiptPrintService>();
             services.AddTransient<IP13WelfareSeikyuCoReportService, P13WelfareSeikyuCoReportService>();
+            services.AddTransient<IP22WelfareSeikyuCoReportService, P22WelfareSeikyuCoReportService>();
 
             //call Calculate API
             services.AddTransient<ICalculateService, CalculateService>();
