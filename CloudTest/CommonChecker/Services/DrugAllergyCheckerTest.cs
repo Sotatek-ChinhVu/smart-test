825445a571314012f132cb054fadb62cd0f6780b
Sotatek-QuanTo <quan.to@sotatek.com>
2023-06-22 21:37:53 +0700

    common checker unit  test

--- CloudTest/CommonChecker/Services/DrugAllergyCheckerTest.cs
+++ CloudTest/CommonChecker/Services/DrugAllergyCheckerTest.cs
@@ -176,7 +176,7 @@ namespace CloudUnitTest.CommonChecker.Services
         }
 
         [Test]
-        public void CheckDrugAllergyChecker_002_CheckingError_IsDuplicatedItemCode()
+        public void CheckDrugAllergyChecker_004_CheckingError_IsDuplicatedItemCode()
         {
             var ordInfDetails = new List<OrdInfoDetailModel>()
             {
@@ -257,7 +257,9 @@ namespace CloudUnitTest.CommonChecker.Services
 
             var tenantTracking = TenantProvider.GetTrackingTenantDataContext();
             var ptInfs = CommonCheckerData.ReadPtInf();
+            var ptAlrgyDrugs = CommonCheckerData.ReadPtAlrgyDrug();
             tenantTracking.PtInfs.AddRange(ptInfs);
+            tenantTracking.PtAlrgyDrugs.AddRange(ptAlrgyDrugs);
             tenantTracking.SaveChanges();
 
             var drugAllergy = new DrugAllergyChecker<OrdInfoModel, OrdInfoDetailModel>();
@@ -271,9 +273,115 @@ namespace CloudUnitTest.CommonChecker.Services
             var result = drugAllergy.HandleCheckOrderList(unitCheckerForOrderListResult);
 
             tenantTracking.PtInfs.RemoveRange(ptInfs);
+            tenantTracking.PtAlrgyDrugs.RemoveRange(ptAlrgyDrugs);
             tenantTracking.SaveChanges();
             //// Assert
-            Assert.True(result.ErrorOrderList.Count == 0);
+            Assert.True(result.ErrorOrderList.Count > 0);
+        }
+
+        [Test]
+        public void CheckDrugAllergyChecker_005_CheckingError_IsDuplicatedItemCode()
+        {
+            var ordInfDetails = new List<OrdInfoDetailModel>()
+            {
+                new OrdInfoDetailModel( id: "id1",
+                                        sinKouiKbn: 20,
+                                        itemCd: "613110017",
+                                        itemName: "・・ｭ・・ｫ・・ｫ・・・・・ｭ・・ｼ・・ｫ・・ｫ・・・・・ｻ・・ｫ・ｼ・・ｼ・・ｼ・・ｼ・・・ｼ・・ｼ・・ｼ・・ｼ・ﾎｼ・ｽ・",
+                                        suryo: 1,
+                                        unitName: "g",
+                                        termVal: 0,
+                                        syohoKbn: 2,
+                                        syohoLimitKbn: 1,
+                                        drugKbn: 1,
+                                        yohoKbn: 0,
+                                        ipnCd: "3112004M1",
+                                        bunkatu: "",
+                                        masterSbt: "Y",
+                                        bunkatuKoui: 0),
+
+                new OrdInfoDetailModel( id: "id2",
+                                        sinKouiKbn: 21,
+                                        itemCd: "Y101",
+                                        itemName: "・・・・ｼ・・・ｵｷ・ｺ・・・・",
+                                        suryo: 1,
+                                        unitName: "・・･・・・",
+                                        termVal: 0,
+                                        syohoKbn: 0,
+                                        syohoLimitKbn: 0,
+                                        drugKbn: 0,
+                                        yohoKbn: 1,
+                                        ipnCd: "",
+                                        bunkatu: "",
+                                        masterSbt: "",
+                                        bunkatuKoui: 0),
+
+                // Duplicated
+                new OrdInfoDetailModel( id: "id1",
+                                        sinKouiKbn: 20,
+                                        itemCd: "613110017",
+                                        itemName: "・・ｭ・・ｫ・・ｫ・・・・・ｭ・・ｼ・・ｫ・・ｫ・・・・・ｻ・・ｫ・ｼ・・ｼ・・ｼ・・ｼ・・・ｼ・・ｼ・・ｼ・・ｼ・ﾎｼ・ｽ・",
+                                        suryo: 1,
+                                        unitName: "g",
+                                        termVal: 0,
+                                        syohoKbn: 2,
+                                        syohoLimitKbn: 1,
+                                        drugKbn: 1,
+                                        yohoKbn: 0,
+                                        ipnCd: "3112004M1",
+                                        bunkatu: "",
+                                        masterSbt: "Y",
+                                        bunkatuKoui: 0),
+
+                new OrdInfoDetailModel( id: "id2",
+                                        sinKouiKbn: 21,
+                                        itemCd: "Y101",
+                                        itemName: "・・・・ｼ・・・ｵｷ・ｺ・・・・",
+                                        suryo: 1,
+                                        unitName: "・・･・・・",
+                                        termVal: 0,
+                                        syohoKbn: 0,
+                                        syohoLimitKbn: 0,
+                                        drugKbn: 0,
+                                        yohoKbn: 1,
+                                        ipnCd: "",
+                                        bunkatu: "",
+                                        masterSbt: "",
+                                        bunkatuKoui: 0),
+            };
+
+            var odrInfoModel = new List<OrdInfoModel>()
+            {
+                new OrdInfoModel(odrKouiKbn: 21,santeiKbn: 0, ordInfDetails: ordInfDetails),
+                new OrdInfoModel(odrKouiKbn: 21,santeiKbn: 0, ordInfDetails: ordInfDetails)
+            };
+
+            var unitCheckerForOrderListResult = new UnitCheckerForOrderListResult<OrdInfoModel, OrdInfoDetailModel>(
+                                                                    RealtimeCheckerType.DrugAllergy, odrInfoModel, 20230101, 1231, new(new(), new(), new()), new(), new(), true);
+
+            var tenantTracking = TenantProvider.GetTrackingTenantDataContext();
+            var ptInfs = CommonCheckerData.ReadPtInf();
+            var ptAlrgyDrugs = CommonCheckerData.ReadPtAlrgyDrug();
+            tenantTracking.PtInfs.AddRange(ptInfs);
+            tenantTracking.PtAlrgyDrugs.AddRange(ptAlrgyDrugs);
+            tenantTracking.SaveChanges();
+
+            var drugAllergy = new DrugAllergyChecker<OrdInfoModel, OrdInfoDetailModel>();
+
+            drugAllergy.HpID = 999;
+            drugAllergy.PtID = 1231;
+            drugAllergy.Sinday = 20230101;
+            drugAllergy.DataContext = TenantProvider.GetNoTrackingDataContext();
+
+            //// Act
+            var result = drugAllergy.HandleCheckOrderList(unitCheckerForOrderListResult);
+
+            tenantTracking.PtInfs.RemoveRange(ptInfs);
+            tenantTracking.PtAlrgyDrugs.RemoveRange(ptAlrgyDrugs);
+            tenantTracking.SaveChanges();
+
+            //// Assert
+            Assert.True(result.ErrorOrderList.Count > 0);
         }
     }
 }
