353be14e45bc1a6cbc0b8be616ca698175c9467b
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-06-23 11:40:22 +0700

    Done

--- UseCase/NextOrder/GetList/GetNextOrderListStatus.cs
+++ UseCase/NextOrder/GetList/GetNextOrderListStatus.cs
@@ -5,8 +5,7 @@
         Successed = 1,
         InvalidHpId = 2,
         InvalidPtId = 3,
-        InvalidRsvkrtKbn = 4,
-        NoData = 5,
-        Failed = 6
+        NoData = 4,
+        Failed = 5
     }
 }
