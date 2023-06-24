084e4f0d251c8bb0d64d940b242824aca78716a7
Sotatek-QuanTo <quan.to@sotatek.com>
2023-06-24 17:14:54 +0700

    fix bug accounting SMAR-3694

--- Interactor/Accounting/SaveAccountingInteractor.cs
+++ Interactor/Accounting/SaveAccountingInteractor.cs
@@ -101,10 +101,6 @@ namespace Interactor.Accounting
             {
                 return SaveAccountingStatus.InvalidPtId;
             }
-            else if (inputData.SumAdjust < 0)
-            {
-                return SaveAccountingStatus.InvalidSumAdjust;
-            }
             else if (inputData.ThisWari < 0)
             {
                 return SaveAccountingStatus.InvalidThisWari;
