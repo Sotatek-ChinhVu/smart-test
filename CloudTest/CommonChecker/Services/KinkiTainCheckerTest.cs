dc2f573c909da7ef18540bf44240dbaa6cc950d6
Sotatek-QuanTo <quan.to@sotatek.com>
2023-06-27 09:29:26 +0700

    KinkiTainCheck Unit Test

---
+++ CloudTest/CommonChecker/Services/KinkiTainCheckerTest.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudUnitTest.CommonChecker.Services
+{
+    public class KinkiTainCheckerTest : BaseUT
+    {
+        [Test]
+        public void Test_001_KinkiTainChecker()
+        {
+
+        }
+    }
+}
