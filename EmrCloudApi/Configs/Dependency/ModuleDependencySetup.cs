4c749af0854cc8eaaa3f0010b56bea8d1b435fe1
Sotatek-MinhLe <minh.le@sotatek.com>
2023-07-04 09:44:57 +0700

    P26VaccineSokatuCoReportService

--- EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
+++ EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
@@ -786,6 +786,7 @@ namespace EmrCloudApi.Configs.Dependency
             services.AddTransient<IP13WelfareSeikyuCoReportService, P13WelfareSeikyuCoReportService>();
             services.AddTransient<IP08KokhoSeikyuCoReportService, P08KokhoSeikyuCoReportService>();
             services.AddTransient<IP22WelfareSeikyuCoReportService, P22WelfareSeikyuCoReportService>();
+            services.AddTransient<IP26VaccineSokatuCoReportService, P26VaccineSokatuCoReportService>();
 
             //call Calculate API
             services.AddTransient<ICalculateService, CalculateService>();
