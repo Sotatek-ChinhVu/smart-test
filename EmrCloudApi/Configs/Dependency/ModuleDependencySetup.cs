0787866a8b72821a2e16cadb3dccb1572a50a05c
Sotatek-MinhLe <minh.le@sotatek.com>
2023-07-06 14:12:50 +0700

    P43KikuchiSeikyu41CoReportService

--- EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
+++ EmrCloudApi/Configs/Dependency/ModuleDependencySetup.cs
@@ -790,6 +790,7 @@ namespace EmrCloudApi.Configs.Dependency
             services.AddTransient<IP22WelfareSeikyuCoReportService, P22WelfareSeikyuCoReportService>();
             services.AddTransient<IKarte3CoReportService, Karte3CoReportService>();
             services.AddTransient<ICoKarte3Finder, CoKarte3Finder>();
+            services.AddTransient<IP43KikuchiSeikyu41CoReportService, P43KikuchiSeikyu41CoReportService>();
 
             //call Calculate API
             services.AddTransient<ICalculateService, CalculateService>();
