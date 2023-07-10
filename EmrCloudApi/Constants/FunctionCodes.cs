061774cabef8c24e42aa4c6c751e8b896ca937f1
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-07-10 17:41:34 +0700

    remove unusing code

--- EmrCloudApi/Constants/FunctionCodes.cs
+++ EmrCloudApi/Constants/FunctionCodes.cs
@@ -4,18 +4,9 @@
 public static class FunctionCodes
 {
     public const string ReceptionChanged = "ReceptionChanged";
-    public const string RaiinInfChanged = "RaiinInfChanged";
     public const string PatientInfChanged = "PatientInfChanged";
-    public const string UserMstChanged = "UserMstChanged";
-    public const string RaiinKubunChanged = "RaiinKubunChanged";
-    public const string LockInfChanged = "LockInfChanged";
-    public const string RaiinCmtChanged = "RaiinCmtChanged";
-    public const string PatientCmtChanged = "PatientCmtChanged";
-    public const string ReservationChanged = "ReservationChanged";
     public const string MedicalChanged = "MedicalChanged";
-    public const string AddLockChanged = "AddLockChanged";
     public const string LockChanged = "LockChanged";
-    public const string RemoveLockChanged = "RemoveLockChanged";
     public const string SupserSetSaveChanged = "SupserSetSaveChanged";
     public const string SupserSetReorderChanged = "SupserSetReorderChanged";
     public const string SuperCopyPasteChanged = "SuperCopyPasteChanged";
--- EmrCloudApi/Messages/CommonMessage.cs
+++ EmrCloudApi/Messages/CommonMessage.cs
@@ -12,15 +12,6 @@ public class CommonMessage
     public long PtId { get; set; } = CommonConstants.InvalidId;
 }
 
-public class LockMessage
-{
-    public int SinDate { get; set; }
-    public long RaiinNo { get; set; } = CommonConstants.InvalidId;
-    public long PtId { get; set; } = CommonConstants.InvalidId;
-    public byte Type { get; set; }
-    public string FunctionCod { get; set; } = string.Empty;
-}
-
 public class SuperSetMessage
 {
     public List<GetSetMstListOutputItem> ReorderSetMstModels { get; set; } = new();
