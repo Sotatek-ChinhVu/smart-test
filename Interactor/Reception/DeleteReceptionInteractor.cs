beda793d3f20b2d59babf8a4d1b8d40f84464e86
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-07-14 16:44:12 +0700

    remove delete patient

--- Interactor/Reception/DeleteReceptionInteractor.cs
+++ Interactor/Reception/DeleteReceptionInteractor.cs
@@ -41,35 +41,16 @@ public class DeleteReceptionInteractor : IDeleteReceptionInputPort
             {
                 return new DeleteReceptionOutputData(DeleteReceptionStatus.InvalidRaiinNo, new(), receptionInfos, sameVisitList);
             }
-            var watch = System.Diagnostics.Stopwatch.StartNew();
             var result = _raiinInfRepository.Delete(inputData.Flag, inputData.HpId, inputData.PtId, inputData.UserId, inputData.SinDate, raiinNos.Select(r => new Tuple<long, long, int>(r.RaiinNo, r.OyaRaiinNo, r.Status)).ToList());
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-            Console.WriteLine("Delete: " + elapsedMs);
-            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
             if (result.Any())
             {
-                watch = System.Diagnostics.Stopwatch.StartNew();
                 Task.Run(() =>
                 {
                     _calculateService.RunCalculate(new RecaculationInputDto(inputData.HpId, inputData.PtId, inputData.SinDate, 0, "DR_"));
                 });
-                watch.Stop();
-                elapsedMs = watch.ElapsedMilliseconds;
-                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-                Console.WriteLine("Run Calculate: " + elapsedMs);
-                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
-                watch = System.Diagnostics.Stopwatch.StartNew();
                 //Item1: SinDate, Item2: RaiinNo, Item3: PtId
                 receptionInfos.Add(new ReceptionRowModel(result.First().Item2, result.First().Item3, result.First().Item1, 1));
                 sameVisitList = _receptionRepository.GetListSameVisit(inputData.HpId, inputData.PtId, inputData.SinDate);
-                watch.Stop();
-                elapsedMs = watch.ElapsedMilliseconds;
-                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-                Console.WriteLine("GetVisit: " + elapsedMs);
-                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                 return new DeleteReceptionOutputData(DeleteReceptionStatus.Successed, result.Select(r => new DeleteReceptionItem(r.Item1, r.Item2, r.Item3)).ToList(), receptionInfos, sameVisitList);
             }
 
