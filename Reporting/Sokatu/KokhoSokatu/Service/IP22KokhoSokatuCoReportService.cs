d8e2ba537480980d66095ce4b9692edf2d992cfd
Sotatek-MinhLe <minh.le@sotatek.com>
2023-07-19 20:36:00 +0700

    revert file

--- Reporting/Sokatu/KokhoSokatu/Service/IP22KokhoSokatuCoReportService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Reporting.Mappers.Common;
-using Reporting.Structs;
-
-namespace Reporting.Sokatu.KokhoSokatu.Service;
-
-public interface IP22KokhoSokatuCoReportService
-{
-    CommonReportingRequestModel GetP22KokhoSokatuReportingData(int hpId, int seikyuYm, SeikyuType seikyuType);
-}
