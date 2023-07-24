6ee45600ed4a338c22d4959939c36a83024e535a
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-07-24 16:56:59 +0700

    create api GetTeikyoByomei

---
+++ UseCase/MstItem/GetTeikyoByomei/GetTeikyoByomeiOutputData.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models.MstItem;
+using UseCase.Core.Sync.Core;
+
+namespace UseCase.MstItem.GetTeikyoByomei;
+
+public class GetTeikyoByomeiOutputData : IOutputData
+{
+    public GetTeikyoByomeiOutputData(List<TeikyoByomeiModel> teikyoByomeis, GetTeikyoByomeiStatus status)
+    {
+        TeikyoByomeis = teikyoByomeis;
+        Status = status;
+    }
+
+    public List<TeikyoByomeiModel> TeikyoByomeis { get; private set; }
+
+    public GetTeikyoByomeiStatus Status { get; private set; }
+}
