3ae6993dfdff9c583ab77298eba7072deb191596
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-07-28 16:50:59 +0700

    Fix Request Controller

--- EmrCloudApi/Requests/Receipt/CheckExisReceInfEditRequest.cs
+++ EmrCloudApi/Requests/Receipt/CheckExisReceInfEditRequest.cs
@@ -2,21 +2,12 @@
 {
     public class CheckExisReceInfEditRequest
     {
-        public CheckExisReceInfEditRequest(int seikyuYm, long ptId, int sinYm, int hokenId)
-        {
-            SeikyuYm = seikyuYm;
-            PtId = ptId;
-            SinYm = sinYm;
-            HokenId = hokenId;
-        }
+        public int SeikyuYm { get; set; }
 
-        public int SeikyuYm { get; private set; }
+        public long PtId { get; set; }
 
-        public long PtId { get; private set; }
+        public int SinYm { get; set; }
 
-        public int SinYm { get; private set; }
-
-        public int HokenId { get; private set; }
-
+        public int HokenId { get; set; }
     }
 }
