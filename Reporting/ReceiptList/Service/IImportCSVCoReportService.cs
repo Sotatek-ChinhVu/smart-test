69b5d31d46d8c9b21a1735d9ae1306997fd00a07
Sotatek-MinhLe <minh.le@sotatek.com>
2023-07-31 08:43:17 +0700

    ImportCSVCoReportService

---
+++ Reporting/ReceiptList/Service/IImportCSVCoReportService.cs
@@ -0,0 +1,11 @@
+﻿using Reporting.CommonMasters.Enums;
+using Reporting.Mappers.Common;
+using Reporting.ReceiptList.Model;
+
+namespace Reporting.ReceiptList.Service
+{
+    public interface IImportCSVCoReportService
+    {
+        CommonExcelReportingModel GetImportCSVCoReportServiceReportingData(int hpId, int seikyuYm, List<ReceiptListModel> receiptListModels, CoFileType fileType, bool outputTitle = false);
+    }
+}
