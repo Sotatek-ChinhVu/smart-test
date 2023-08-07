c41ed42578b2b7ca85d441083e29e5c163e29311
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-08-07 14:16:51 +0700

    Create api GetOutDrugOrderList

---
+++ UseCase/Reception/GetOutDrugOrderList/GetOutDrugOrderListInputData.cs
@@ -0,0 +1,19 @@
+﻿using UseCase.Core.Sync.Core;
+
+namespace UseCase.Reception.GetOutDrugOrderList;
+
+public class GetOutDrugOrderListInputData : IInputData<GetOutDrugOrderListOutputData>
+{
+    public GetOutDrugOrderListInputData(int hpId, int fromDate, int toDate)
+    {
+        HpId = hpId;
+        FromDate = fromDate;
+        ToDate = toDate;
+    }
+
+    public int HpId { get; private set; }
+
+    public int FromDate { get; private set; }
+
+    public int ToDate { get; private set; }
+}
