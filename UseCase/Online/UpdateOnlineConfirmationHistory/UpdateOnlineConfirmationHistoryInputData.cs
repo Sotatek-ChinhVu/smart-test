2913b61533b7b3b74ad9887ff48a01bd02ce8e65
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-08-21 09:36:15 +0700

    create UpdateOnlineConfirmationHistory repository

---
+++ UseCase/Online/UpdateOnlineConfirmationHistory/UpdateOnlineConfirmationHistoryInputData.cs
@@ -0,0 +1,22 @@
+﻿using UseCase.Core.Sync.Core;
+
+namespace UseCase.Online.UpdateOnlineConfirmationHistory;
+
+public class UpdateOnlineConfirmationHistoryInputData : IInputData<UpdateOnlineConfirmationHistoryOutputData>
+{
+    public UpdateOnlineConfirmationHistoryInputData(int id, int userId, bool isDeleted, bool isUpdated)
+    {
+        Id = id;
+        UserId = userId;
+        IsDeleted = isDeleted;
+        IsUpdated = isUpdated;
+    }
+
+    public int Id { get; private set; }
+
+    public int UserId { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+
+    public bool IsUpdated { get; private set; }
+}
---
+++ UseCase/Online/UpdateOnlineConfirmationHistory/UpdateOnlineConfirmationHistoryStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace UseCase.Online.UpdateOnlineConfirmationHistory;
+
+public enum UpdateOnlineConfirmationHistoryStatus : byte
+{
+    Successed = 1,
+    Failed = 2,
+}
