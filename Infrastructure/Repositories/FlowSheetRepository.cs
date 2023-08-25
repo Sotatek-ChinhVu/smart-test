e854eaaae70deae1183c54b079ffc153ca00f1b1
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-08-25 14:08:25 +0700

    Fix condition to get all

--- Infrastructure/Repositories/FlowSheetRepository.cs
+++ Infrastructure/Repositories/FlowSheetRepository.cs
@@ -506,7 +506,7 @@ namespace Infrastructure.Repositories
             }
 
             List<(int, string)> result = new();
-            var raiinInfs = NoTrackingDataContext.RaiinInfs
+            var raiinInfs = isAll ? new() : NoTrackingDataContext.RaiinInfs
                 .Where(r => r.HpId == hpId && (isAll || r.PtId == ptId) && r.IsDeleted == DeleteTypes.None && r.SinDate >= startDate && r.SinDate <= endDate && r.Status >= RaiinState.TempSave)
                 .Select(r => new { r.SinDate, r.SyosaisinKbn, r.Status }).ToList();
             var holidays = NoTrackingDataContext.HolidayMsts.Where(r => r.HpId == hpId && r.IsDeleted == DeleteTypes.None && r.SinDate >= startDate && r.SinDate <= endDate).Select(r => new { r.SinDate, r.HolidayName }).ToList();
