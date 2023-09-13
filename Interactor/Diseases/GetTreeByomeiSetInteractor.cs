d81b0167bb4b1685a2caa30d866ea435f423525c
thaitc <thaitc@gitlab.com>
2023-09-13 16:57:54 +0700

    fix check childrens

--- Interactor/Diseases/GetTreeByomeiSetInteractor.cs
+++ Interactor/Diseases/GetTreeByomeiSetInteractor.cs
@@ -26,21 +26,21 @@ namespace Interactor.Diseases
                     return new GetTreeByomeiSetOutputData(new List<ByomeiSetMstItem>(), GetTreeByomeiSetStatus.NoData);
                 else
                 {
-                    var rootNodes = datas.Select(item => new ByomeiSetMstItem(item)).ToList();
+                    var dataItems = datas.Select(item => new ByomeiSetMstItem(item)).ToList();
                     List<ByomeiSetMstItem> result = new List<ByomeiSetMstItem>();                   
-                     rootNodes = rootNodes.FindAll(p => p.Level == 1).OrderBy(p => p.Level1).ToList();
+                     var rootNodes = dataItems.FindAll(p => p.Level == 1).OrderBy(p => p.Level1).ToList();
                     foreach (var levelNode1 in rootNodes)
                     {
-                        levelNode1.Childrens = rootNodes.FindAll(p => p.Level == 2 && p.Level1 == levelNode1.Level1).OrderBy(p => p.Level2).ToList();
+                        levelNode1.Childrens = dataItems.FindAll(p => p.Level == 2 && p.Level1 == levelNode1.Level1).OrderBy(p => p.Level2).ToList();
                         foreach (var levelNode2 in levelNode1.Childrens)
                         {
-                            levelNode2.Childrens = rootNodes.FindAll(p => p.Level == 3 && p.Level1 == levelNode1.Level1 && p.Level2 == levelNode2.Level2).OrderBy(p => p.Level3).ToList();
+                            levelNode2.Childrens = dataItems.FindAll(p => p.Level == 3 && p.Level1 == levelNode1.Level1 && p.Level2 == levelNode2.Level2).OrderBy(p => p.Level3).ToList();
                             foreach (var levelNode3 in levelNode2.Childrens)
                             {
-                                levelNode3.Childrens = rootNodes.FindAll(p => p.Level == 4 && p.Level1 == levelNode1.Level1 && p.Level2 == levelNode2.Level2 && p.Level3 == levelNode3.Level3).OrderBy(p => p.Level4).ToList();
+                                levelNode3.Childrens = dataItems.FindAll(p => p.Level == 4 && p.Level1 == levelNode1.Level1 && p.Level2 == levelNode2.Level2 && p.Level3 == levelNode3.Level3).OrderBy(p => p.Level4).ToList();
                                 foreach (var levelNode4 in levelNode3.Childrens)
                                 {
-                                    levelNode4.Childrens = rootNodes.FindAll(p => p.Level == 5 && p.Level1 == levelNode1.Level1 && p.Level2 == levelNode2.Level2 && p.Level3 == levelNode3.Level3 && p.Level4 == levelNode4.Level4).OrderBy(p => p.Level5).ToList();
+                                    levelNode4.Childrens = dataItems.FindAll(p => p.Level == 5 && p.Level1 == levelNode1.Level1 && p.Level2 == levelNode2.Level2 && p.Level3 == levelNode3.Level3 && p.Level4 == levelNode4.Level4).OrderBy(p => p.Level5).ToList();
                                 }
                             }
                         }
