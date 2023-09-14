1610bda9ce23653891a24d9c96aec649540ee859
thaitc <thaitc@gitlab.com>
2023-09-14 15:16:46 +0700

    add model, usecase ByomeiSetGenerationMst

---
+++ UseCase/ByomeiSetGenerationMst/GetListByomeiSetGenerationMst/GetListByomeiSetGenerationMstStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace UseCase.ByomeiSetGenerationMst.GetListByomeiSetGenerationMst
+{
+    public enum GetListByomeiSetGenerationMstStatus
+    {
+        Successed = 1,
+        InvalidHpId = 2,
+        NoData = 3
+    }
+}
---
+++ UseCase/ByomeiSetGenerationMst/GetListByomeiSetGenerationMst/IGetListByomeiSetGenerationMstOutputPort.cs
@@ -0,0 +1,8 @@
+﻿using UseCase.Core.Sync.Core;
+
+namespace UseCase.ByomeiSetGenerationMst.GetListByomeiSetGenerationMst
+{
+    public interface IGetListByomeiSetGenerationMstOutputPort : IOutputPort<GetListByomeiSetGenerationMstOutputData>
+    {
+    }
+}
