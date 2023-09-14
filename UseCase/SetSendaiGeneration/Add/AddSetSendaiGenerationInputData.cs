6f0054c08ddeae42fdce3d664b062d102b63d1bf
hungdao2 <hung.dao2@sotatek.com>
2023-09-14 09:01:31 +0700

    update add usecase of delete sensaiGeneration

---
+++ UseCase/SetSendaiGeneration/Add/AddSetSendaiGenerationInputData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase.Core.Sync.Core;
+
+namespace UseCase.SetSendaiGeneration.Add
+{
+    public class AddSetSendaiGenerationInputData: IInputData<AddSetSendaiGenerationOutputData>
+    {
+        public AddSetSendaiGenerationInputData(int startDate, int hpId, int userId)
+        {
+            StartDate = startDate;
+            HpId = hpId;
+            UserId = userId;
+        }
+
+        public int StartDate { get; set; }
+        public int HpId { get; set; }
+        public int UserId { get; set; }
+    }
+}
