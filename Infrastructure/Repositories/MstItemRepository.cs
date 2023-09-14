ef52076ad980cffdc8b0cf423ac5ce994ccb733b
minh.le <minh.le@sotatek.com>
2023-09-14 15:40:55 +0700

    Revert file

--- Infrastructure/Repositories/MstItemRepository.cs
+++ Infrastructure/Repositories/MstItemRepository.cs
@@ -6307,37 +6307,6 @@ namespace Infrastructure.Repositories
             return true;
         }
 
-        public bool UpdateByomeiMst(int userId, int hpId, List<UpdateByomeiMstModel> listData)
-        {
-            // Update IsAdopted Item TenMst
-            foreach (var item in listData)
-            {
-                var itemUpdate = TrackingDataContext.ByomeiMsts.FirstOrDefault(t => t.HpId == hpId && t.ByomeiCd == item.ByomeiCd);
-                if (itemUpdate != null)
-                {
-                    itemUpdate.KanaName2 = item.KanaName2;
-                    itemUpdate.SikkanCd = item.SikkanCd;
-                    itemUpdate.NanbyoCd = item.NanbyoCd;
-                    itemUpdate.IsAdopted = item.IsAdopted ? 1 : 0;
-                    itemUpdate.UpdateDate = CIUtil.GetJapanDateTimeNow();
-                    itemUpdate.UpdateId = userId;
-                    TrackingDataContext.SaveChanges();
-                }
-            }  
-            return true;
-        }
-
-        public bool IsUsingKensa(int hpId, string kensaItemCd, List<string> itemCds)
-        {
-            bool result = NoTrackingDataContext.KensaInfDetails.Where(p => p.HpId == hpId && p.KensaItemCd == kensaItemCd).Any();
-
-            if (itemCds?.Count > 0)
-            {
-                result = result || NoTrackingDataContext.OdrInfDetails.Where(p => p.HpId == hpId && itemCds.Contains(p.ItemCd ?? string.Empty)).Any();
-            }
-            return result;
-        }
-
         public List<KensaStdMstModel> GetKensaStdMstModels(int hpId, string kensaItemCd)
         {
             var kensaStdMsts = NoTrackingDataContext.KensaStdMsts.Where(p => p.HpId == hpId && p.KensaItemCd == kensaItemCd);
