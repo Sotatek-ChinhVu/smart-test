d1f10c39376b4217cde67f531f19957897785352
Sotatek-QuanTo <quan.to@sotatek.com>
2023-09-18 17:01:49 +0700

    set default null SearchPtInfs

--- EmrCloudApi/Requests/PatientManagement/SearchPtInfsRequest.cs
+++ EmrCloudApi/Requests/PatientManagement/SearchPtInfsRequest.cs
@@ -10,18 +10,18 @@ namespace EmrCloudApi.Requests.PatientManagement
 
         public int PageCount { get; set; }
 
-        public CoSta9000PtConf CoSta9000PtConf { get; set; }
+        public CoSta9000PtConf? CoSta9000PtConf { get; set; } = null;
 
-        public CoSta9000HokenConf CoSta9000HokenConf { get; set; }
+        public CoSta9000HokenConf? CoSta9000HokenConf { get; set; } = null;
 
-        public CoSta9000ByomeiConf CoSta9000ByomeiConf { get; set; }
+        public CoSta9000ByomeiConf? CoSta9000ByomeiConf { get; set; } = null;
 
-        public CoSta9000RaiinConf CoSta9000RaiinConf { get; set; }
+        public CoSta9000RaiinConf? CoSta9000RaiinConf { get; set; } = null;
 
-        public CoSta9000SinConf CoSta9000SinConf { get; set; }
+        public CoSta9000SinConf? CoSta9000SinConf { get; set; } = null;
 
-        public CoSta9000KarteConf CoSta9000KarteConf { get; set; }
+        public CoSta9000KarteConf? CoSta9000KarteConf { get; set; } = null;
 
-        public CoSta9000KensaConf CoSta9000KensaConf { get; set; }
+        public CoSta9000KensaConf? CoSta9000KensaConf { get; set; } = null;
     }
 }
