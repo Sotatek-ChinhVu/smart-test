bb725bf8250718dec255203113f135cdbf65d4b4
minh.le <minh.le@sotatek.com>
2023-09-21 17:06:51 +0700

    return model RaiinListMstModel

--- Domain/Models/RaiinListMst/RaiinListMstModel.cs
+++ Domain/Models/RaiinListMst/RaiinListMstModel.cs
@@ -11,19 +11,6 @@
             IsDeleted = isDeleted;
         }
 
-        public RaiinListMstModel(int grpId, string grpName, int sortNo, int isDeleted, List<RaiinListDetailModel> raiinListDetailsList, int grpIdMax, int sortNoMax, int sortNoDetailMax, int kbnCdMax)
-        {
-            GrpId = grpId;
-            GrpName = grpName;
-            SortNo = sortNo;
-            RaiinListDetailsList = raiinListDetailsList;
-            IsDeleted = isDeleted;
-            GrpIdMax = grpIdMax;
-            SortNoMax = sortNoMax;
-            SortNoDetailMax = sortNoDetailMax;
-            KbnCdMax = kbnCdMax;
-        }
-
         public int GrpId { get; private set; }
 
         public string GrpName { get; private set; }
@@ -33,13 +20,5 @@
         public int IsDeleted { get; private set; }
 
         public List<RaiinListDetailModel> RaiinListDetailsList { get; private set; }
-
-        public int GrpIdMax { get; private set; }
-
-        public int SortNoMax { get; private set; }
-
-        public int KbnCdMax { get; private set; }
-
-        public int SortNoDetailMax { get; private set; }
     }
 }
