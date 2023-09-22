277e9648dba6007a8ebbf922f42f1b305dd3f39c
thaitc <thaitc@gitlab.com>
2023-09-22 16:00:58 +0700

    change private set SetCheckBoxStatusModel

--- Domain/Models/MstItem/SetCheckBoxStatusModel.cs
+++ Domain/Models/MstItem/SetCheckBoxStatusModel.cs
@@ -22,34 +22,34 @@
             YohoChecked = yohoChecked;
             DiffChecked = diffChecked;
         }
-        public bool SetKbnChecked1 { get; set; }
+        public bool SetKbnChecked1 { get; private set; }
 
-        public bool SetKbnChecked2 { get; set; }
+        public bool SetKbnChecked2 { get; private set; }
 
-        public bool SetKbnChecked3 { get; set; }
+        public bool SetKbnChecked3 { get; private set; }
 
-        public bool SetKbnChecked4 { get; set; }
+        public bool SetKbnChecked4 { get; private set; }
 
-        public bool SetKbnChecked5 { get; set; }
+        public bool SetKbnChecked5 { get; private set; }
 
-        public bool SetKbnChecked6 { get; set; }
+        public bool SetKbnChecked6 { get; private set; }
 
-        public bool SetKbnChecked7 { get; set; }
+        public bool SetKbnChecked7 { get; private set; }
 
-        public bool SetKbnChecked8 { get; set; }
+        public bool SetKbnChecked8 { get; private set; }
 
-        public bool SetKbnChecked9 { get; set; }
+        public bool SetKbnChecked9 { get; private set; }
 
-        public bool SetKbnChecked10 { get; set; }
+        public bool SetKbnChecked10 { get; private set; }
 
-        public bool JihiChecked { get; set; }
+        public bool JihiChecked { get; private set; }
 
-        public bool KihonChecked { get; set; }
+        public bool KihonChecked { get; private set; }
 
-        public bool TokuChecked { get; set; }
+        public bool TokuChecked { get; private set; }
 
-        public bool YohoChecked { get; set; }
+        public bool YohoChecked { get; private set; }
 
-        public bool DiffChecked { get; set; }
+        public bool DiffChecked { get; private set; }
     }
 }
