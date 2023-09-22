660ea13e4c970c77a885a360fbc6a8f9e7e4beed
Sotatek-QuanTo <quan.to@sotatek.com>
2023-09-22 13:45:53 +0700

    Get Disease Name

--- Interactor/PatientManagement/GetStaConfMenuInteractor.cs
+++ Interactor/PatientManagement/GetStaConfMenuInteractor.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Models.MainMenu;
+﻿using Domain.Models.Diseases;
+using Domain.Models.MainMenu;
 using Domain.Models.MstItem;
 using UseCase.PatientManagement.GetStaConf;
 
@@ -8,11 +9,13 @@ namespace Interactor.PatientManagement
     {
         private readonly IStatisticRepository _statisticRepository;
         private readonly IMstItemRepository _mstItemRepository;
+        private readonly IPtDiseaseRepository _ptDiseaseRepository;
 
-        public GetStaConfMenuInteractor(IStatisticRepository statisticRepository, IMstItemRepository mstItemRepository)
+        public GetStaConfMenuInteractor(IStatisticRepository statisticRepository, IMstItemRepository mstItemRepository, IPtDiseaseRepository ptDiseaseRepository)
         {
             _statisticRepository = statisticRepository;
             _mstItemRepository = mstItemRepository;
+            _ptDiseaseRepository = ptDiseaseRepository;
         }
 
         public GetStaConfMenuOutputData Handle(GetStaConfMenuInputData inputData)
@@ -22,11 +25,12 @@ namespace Interactor.PatientManagement
                 var staMenu = _statisticRepository.GetStatisticMenuModels(inputData.HpId);
 
                 var tenMstItems = new Dictionary<string, string>();
+                var byomeis = new Dictionary<string, string>();
 
                 foreach (var item in staMenu)
                 {
                     var itemDatas = GetItemName(inputData.HpId, item.PatientManagement.ItemCds, item.PatientManagement.ItemCmts);
-
+                    var byomeiDatas = GetByomeiName(inputData.HpId, item.PatientManagement.ByomeiCds);
                     foreach (var kvp in itemDatas)
                     {
                         if (!tenMstItems.ContainsKey(kvp.Key))
@@ -34,14 +38,23 @@ namespace Interactor.PatientManagement
                             tenMstItems.Add(kvp.Key, kvp.Value);
                         }
                     }
+
+                    foreach (var kvp in byomeiDatas)
+                    {
+                        if (!byomeis.ContainsKey(kvp.Key))
+                        {
+                            byomeis.Add(kvp.Key, kvp.Value);
+                        }
+                    }
                 }
 
-                return new GetStaConfMenuOutputData(staMenu, tenMstItems, staMenu.Any() ? GetStaConfMenuStatus.Successed : GetStaConfMenuStatus.NoData);
+                return new GetStaConfMenuOutputData(staMenu, tenMstItems, byomeis, staMenu.Any() ? GetStaConfMenuStatus.Successed : GetStaConfMenuStatus.NoData);
             }
             finally
             {
                 _statisticRepository.ReleaseResource();
                 _mstItemRepository.ReleaseResource();
+                _ptDiseaseRepository.ReleaseResource();
             }
         }
 
@@ -68,6 +81,23 @@ namespace Interactor.PatientManagement
                     {
                         result.Add(itemCds[i], _mstItemRepository.GetNameByItemCd(hpId, itemCds[i]));
                     }
+                }
+            }
+
+            return result;
+        }
+
+        private Dictionary<string, string> GetByomeiName(int hpId, List<string> byomeiCds)
+        {
+            var result = new Dictionary<string, string>();
+
+            var byomeis = _ptDiseaseRepository.GetByomeiMst(hpId, byomeiCds);
+
+            foreach (var item in byomeis)
+            {
+                if (!result.ContainsKey(item.Key))
+                {
+                    result.Add(item.Key, item.Value);
                 }
             }
 
