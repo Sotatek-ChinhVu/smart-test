ad64ea0f155c985e6d97d4a4125e7093095173fa
Sotatek-QuanTo <quan.to@sotatek.com>
2023-09-26 10:23:38 +0700

    update ArgumentModel

--- Domain/Models/AuditLog/ArgumentModel.cs
+++ Domain/Models/AuditLog/ArgumentModel.cs
@@ -2,7 +2,7 @@
 {
     public class ArgumentModel
     {
-        public ArgumentModel(string eventCd, long ptId, int sinDate, long raiinNo, int? misyu, int nyukinDate, int? nyukin, int nyukinSortNo, string hosoku)
+        public ArgumentModel(string eventCd, long ptId, int sinDate, long raiinNo, int misyu, int nyukinDate, int nyukin, int nyukinSortNo, string hosoku)
         {
             EventCd = eventCd;
             PtId = ptId;
@@ -19,9 +19,9 @@
         public long PtId { get; private set; }
         public int SinDate { get; private set; }
         public long RaiinNo { get; private set; }
-        public int? Misyu { get; private set; } = null;
+        public int Misyu { get; private set; }
         public int NyukinDate { get; private set; }
-        public int? Nyukin { get; private set; } = null;
+        public int Nyukin { get; private set; }
         public int NyukinSortNo { get; private set; }
         public string Hosoku { get; private set; }
     }
