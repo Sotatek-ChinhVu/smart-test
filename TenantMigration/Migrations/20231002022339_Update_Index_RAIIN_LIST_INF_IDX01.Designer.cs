1ded6ab6d4517c4f54d3ca127634bb51ac3472e8
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-10-02 09:25:03 +0700

    update index raiinListInf

--- TenantMigration/Migrations/20231002022339_Update_Index_RAIIN_LIST_INF_IDX01.Designer.cs
+++ TenantMigration/Migrations/20231002022339_Update_Index_RAIIN_LIST_INF_IDX01.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PostgreDataContext;
@@ -11,9 +12,11 @@ using PostgreDataContext;
 namespace TenantMigration.Migrations
 {
     [DbContext(typeof(TenantDataContext))]
-    partial class TenantDataContextModelSnapshot : ModelSnapshot
+    [Migration("20231002022339_Update_Index_RAIIN_LIST_INF_IDX01")]
+    partial class UpdateIndexRAIINLISTINFIDX01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
