26cbccc7863a5c33b6f8764e6d7605e1bcb613c7
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-10-04 16:27:40 +0700

    add AsEnumable to api get

--- Reporting/Statistics/Sta3020/DB/CoSta3020Finder.cs
+++ Reporting/Statistics/Sta3020/DB/CoSta3020Finder.cs
@@ -119,7 +119,7 @@ namespace Reporting.Statistics.Sta3020.DB
                         CenterItemCd2 = kensaMstJoin.CenterItemCd2 == null ? string.Empty : kensaMstJoin.CenterItemCd2,
                         MaxEndDate = maxTenMstJoin.MaxEndDate == null ? 0 : maxTenMstJoin.MaxEndDate
                     }
-                );
+                ).AsEnumerable();
 
                 //条件２-対象データ
                 switch (printConf.TgtData)
