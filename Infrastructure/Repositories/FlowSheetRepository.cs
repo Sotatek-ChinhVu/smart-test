a5f583617e0d44af4e4e6f12b9b4def17bcfc422
Sotatek-LuuNguyen2 <93107464+Sotatek-LuuNguyen2@users.noreply.github.com>
2023-10-09 09:06:27 +0700

    fix join query in raiinListInf

--- Infrastructure/Repositories/FlowSheetRepository.cs
+++ Infrastructure/Repositories/FlowSheetRepository.cs
@@ -543,11 +543,12 @@ namespace Infrastructure.Repositories
             var stopwatch = Stopwatch.StartNew();
             var raiinListInfs =
                      (
-                        from raiinListInf in NoTrackingDataContext.RaiinListInfs.Where(r => r.HpId == hpId && r.PtId == ptId && r.RaiinNo != 0)
-                        join raiinListMst in NoTrackingDataContext.RaiinListDetails.Where(d => d.HpId == hpId && d.IsDeleted == DeleteTypes.None)
+                        from raiinListInf in NoTrackingDataContext.RaiinListInfs
+                        join raiinListMst in NoTrackingDataContext.RaiinListDetails
                         on new { raiinListInf.GrpId, raiinListInf.KbnCd } equals new { raiinListMst.GrpId, raiinListMst.KbnCd }
+                        where raiinListInf.HpId == hpId && raiinListInf.PtId == ptId && raiinListInf.RaiinNo != 0 && raiinListMst.HpId == hpId && raiinListMst.IsDeleted == DeleteTypes.None
                         select new { raiinListInf.RaiinNo, raiinListInf.GrpId, raiinListInf.KbnCd, raiinListInf.RaiinListKbn, raiinListMst.KbnName, raiinListMst.ColorCd }
-                     );
+                     ).ToList();
 
             var result = raiinListInfs
                 .GroupBy(r => r.RaiinNo)
@@ -560,12 +561,13 @@ namespace Infrastructure.Repositories
         {
             var stopwatch = Stopwatch.StartNew();
             var raiinListInfs =
-                     (
-                        from raiinListInf in NoTrackingDataContext.RaiinListInfs.Where(r => r.HpId == hpId && r.PtId == ptId && r.RaiinNo == 0)
-                        join raiinListMst in NoTrackingDataContext.RaiinListDetails.Where(d => d.HpId == hpId && d.IsDeleted == DeleteTypes.None)
-                        on new { raiinListInf.GrpId, raiinListInf.KbnCd } equals new { raiinListMst.GrpId, raiinListMst.KbnCd }
-                        select new { raiinListInf.SinDate, raiinListInf.RaiinNo, raiinListInf.GrpId, raiinListInf.KbnCd, raiinListInf.RaiinListKbn, raiinListMst.KbnName, raiinListMst.ColorCd }
-                     );
+               (
+                  from raiinListInf in NoTrackingDataContext.RaiinListInfs
+                  join raiinListMst in NoTrackingDataContext.RaiinListDetails
+                  on new { raiinListInf.GrpId, raiinListInf.KbnCd } equals new { raiinListMst.GrpId, raiinListMst.KbnCd }
+                  where raiinListInf.HpId == hpId && raiinListInf.PtId == ptId && raiinListInf.RaiinNo == 0 && raiinListMst.HpId == hpId && raiinListMst.IsDeleted == DeleteTypes.None
+                  select new { raiinListInf.SinDate, raiinListInf.RaiinNo, raiinListInf.GrpId, raiinListInf.KbnCd, raiinListInf.RaiinListKbn, raiinListMst.KbnName, raiinListMst.ColorCd }
+               ).ToList();
 
             var result = raiinListInfs
                 .GroupBy(r => r.SinDate)
