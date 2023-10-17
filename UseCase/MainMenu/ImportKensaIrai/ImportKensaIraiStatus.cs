96d7606d934ab1e7a1a8a695378bf38b9378b802
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-10-17 08:11:51 +0700

    remove unusing code

--- UseCase/MainMenu/ImportKensaIrai/ImportKensaIraiStatus.cs
+++ UseCase/MainMenu/ImportKensaIrai/ImportKensaIraiStatus.cs
@@ -2,14 +2,7 @@
 
 public enum ImportKensaIraiStatus : byte
 {
-    ValidateSuccessed = 0,
     Successed = 1,
     Failed = 2,
-    InvalidCenterCd = 3,
-    InvalidIraiCd = 4,
-    InvalidKensaItemCd = 5,
-    InvalidResultType = 6,
-    InvalidAbnormalKbn = 7,
-    InvalidInputFile = 8,
-    InvalidSizeFile = 9,
+    InvalidInputFile = 3,
 }
