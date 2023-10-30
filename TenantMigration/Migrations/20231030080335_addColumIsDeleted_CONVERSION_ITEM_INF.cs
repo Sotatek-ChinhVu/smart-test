d4ae3a695ec75ee9e039c5f25c9f1e75c64e186b
thaitc <thaitc@gitlab.com>
2023-10-30 15:05:27 +0700

    add migration add colum IsDeleted table CONVERSION_ITEM_INF

---
+++ TenantMigration/Migrations/20231030080335_addColumIsDeleted_CONVERSION_ITEM_INF.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TenantMigration.Migrations
+{
+    /// <inheritdoc />
+    public partial class addColumIsDeletedCONVERSIONITEMINF : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IS_DELETED",
+                table: "CONVERSION_ITEM_INF",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IS_DELETED",
+                table: "CONVERSION_ITEM_INF");
+        }
+    }
+}
