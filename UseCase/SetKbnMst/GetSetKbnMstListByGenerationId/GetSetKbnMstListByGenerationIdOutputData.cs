80547ecadf24907aca15122ce2fa8332e70fbbb3
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-10-31 08:56:42 +0700

    create Api GetSetKbnMstListByGenerationId

---
+++ UseCase/SetKbnMst/GetSetKbnMstListByGenerationId/GetSetKbnMstListByGenerationIdOutputData.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models.SetKbnMst;
+using UseCase.Core.Sync.Core;
+
+namespace UseCase.SetKbnMst.GetSetKbnMstListByGenerationId;
+
+public class GetSetKbnMstListByGenerationIdOutputData : IOutputData
+{
+    public GetSetKbnMstListByGenerationIdOutputData(List<SetKbnMstModel> setKbnMstList, GetSetKbnMstListByGenerationIdStatus status)
+    {
+        SetKbnMstList = setKbnMstList;
+        Status = status;
+    }
+
+    public List<SetKbnMstModel> SetKbnMstList { get; private set; }
+
+    public GetSetKbnMstListByGenerationIdStatus Status { get; private set; }
+}
