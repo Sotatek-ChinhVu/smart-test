4a231bb21bc1cfa04b6a0bb6c3ce87389a8335c6
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-11-03 14:06:46 +0700

    add IsExclude to entity

---
+++ Domain/Models/DrugInfor/SinrekiFilterMstDetailModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Models.DrugInfor;
+
+public class SinrekiFilterMstDetailModel
+{
+    public SinrekiFilterMstDetailModel(int grpCd, string itemCd, int sortNo, bool isExclude)
+    {
+        GrpCd = grpCd;
+        ItemCd = itemCd;
+        SortNo = sortNo;
+        IsExclude = isExclude;
+    }
+
+    public int GrpCd { get; private set; }
+
+    public string ItemCd { get; private set; }
+
+    public int SortNo { get; private set; }
+
+    public bool IsExclude { get; private set; }
+}
---
+++ Entity/Tenant/SinrekiFilterMstKoui.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entity.Tenant;
+
+[Table(name: "SINREKI_FILTER_MST_KOUI")]
+public class SinrekiFilterMstKoui : EmrCloneable<SinrekiFilterMstKoui>
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [Column(name: "HP_ID", Order = 1)]
+    public long HpId { get; set; }
+
+    [Column(name: "GRP_CD", Order = 2)]
+    public int GrpCd { get; set; }
+
+    [Column(name: "ID", Order = 1)]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+}
