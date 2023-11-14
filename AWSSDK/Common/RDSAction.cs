551ec49e6ac8a5ce8cad3fed82a5cd5c1a7b4d27
thaitc <thaitc@gitlab.com>
2023-11-14 09:07:42 +0700

    add function rds monitoring

---
+++ AWSSDK/Common/RDSAction.cs
@@ -0,0 +1,150 @@
+﻿using Amazon.RDS;
+using Amazon.RDS.Model;
+
+namespace AWSSDK.Common
+{
+    public static class RDSAction
+    {
+        public static async Task<Dictionary<string, RDSInformation>> GetRDSInformation()
+        {
+            try
+            {
+                var rds = new AmazonRDSClient();
+                var response = await rds.DescribeDBInstancesAsync();
+
+                var rdsInformation = new Dictionary<string, RDSInformation>();
+
+                foreach (var dbInstance in response.DBInstances)
+                {
+                    string rdsIdentifier = dbInstance.DBInstanceIdentifier;
+                    string dbType = dbInstance.DBInstanceClass;
+                    int dbStorage = (int)dbInstance.AllocatedStorage;
+                    string dbEngine = dbInstance.Engine;
+
+                    var rdsInfo = new RDSInformation
+                    {
+                        RDSIdentifier = rdsIdentifier,
+                        InstanceType = dbType,
+                        SizeStorage = dbStorage,
+                        DBEngine = dbEngine
+                    };
+
+                    rdsInformation.Add(rdsIdentifier, rdsInfo);
+                }
+                return rdsInformation;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task CreateNewShardAsync(string dbIdentifier)
+        {
+            try
+            {
+                var rdsClient = new AmazonRDSClient();
+
+                var request = new CreateDBInstanceRequest
+                {
+                    DBInstanceIdentifier = dbIdentifier,
+                    AllocatedStorage = 20,
+                    DBName = "smartkarte",
+                    Engine = "postgres",
+                    EngineVersion = "14.4",
+                    StorageType = "gp2",
+                    StorageEncrypted = false,
+                    AutoMinorVersionUpgrade = true,
+                    MultiAZ = false,
+                    MasterUsername = "postgres",
+                    MasterUserPassword = "Emr!23456789",
+                    DBSubnetGroupName = "develop-smartkarte-rds-subnetgroup",
+                    VpcSecurityGroupIds = new List<string> { "sg-0cc9111542280b236" },
+                    DBInstanceClass = "db.t4g.micro"
+                };
+
+                await rdsClient.CreateDBInstanceAsync(request);
+
+                Console.WriteLine($"Starting RDS instance with ID: {dbIdentifier}");
+            }
+            catch (AmazonRDSException e)
+            {
+                if (e.ErrorCode == "DBInstanceAlreadyExists")
+                {
+                    Console.WriteLine($"Database Shard {dbIdentifier} exists already, continuing to poll ...");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public static async Task<string> CheckingRDSStatusAsync(string dbIdentifier)
+        {
+            try
+            {
+                string host = string.Empty;
+                bool running = true;
+
+                while (running)
+                {
+                    var rdsClient = new AmazonRDSClient();
+
+                    var response = await rdsClient.DescribeDBInstancesAsync(new DescribeDBInstancesRequest
+                    {
+                        DBInstanceIdentifier = dbIdentifier
+                    });
+
+                    var dbInstances = response.DBInstances;
+
+                    if (dbInstances.Count != 1)
+                    {
+                        throw new Exception("More than one Database Shard returned; this should never happen");
+                    }
+
+                    var dbInstance = dbInstances[0];
+                    var status = dbInstance.DBInstanceStatus;
+
+                    Console.WriteLine($"Last Database Shard status: {status}");
+
+                    Thread.Sleep(5000);
+
+                    if (status == "available")
+                    {
+                        var endpoint = dbInstance.Endpoint;
+                        host = endpoint.Address;
+                        running = false;
+                    }
+                }
+
+                return host;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return null;
+            }
+        }
+
+
+        public static void GenerateDumpfile(string tenantId)
+        {
+            try
+            {
+                string templatePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "template", "gdump.sql");
+                string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "template", $"{tenantId}_dump.sql");
+
+                string content = File.ReadAllText(templatePath);
+                content = content.Replace("dumpdb.", $"{tenantId}.");
+
+                File.WriteAllText(outputPath, content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ AWSSDK/Interfaces/IAwsSdkService.cs
@@ -0,0 +1,8 @@
+﻿namespace AWSSDK.Interfaces
+{
+    public interface IAwsSdkService
+    {
+        Task<Dictionary<string, Dictionary<string, string>>> SummaryCard();
+        Task<List<string>> GetAvailableIdentifiersAsync();
+    }
+}
