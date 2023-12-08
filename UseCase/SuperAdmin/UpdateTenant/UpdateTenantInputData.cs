2132d414dbf55242d5dcf67215bb626366da32f7
hoangnguyen11 <hoang.nguyen11@sotatek.com>
2023-12-08 15:16:31 +0700

    add api update tenant

--- UseCase/SuperAdmin/UpdateTenant/UpdateTenantInputData.cs
+++ UseCase/SuperAdmin/UpdateTenant/UpdateTenantInputData.cs
@@ -4,5 +4,17 @@ namespace UseCase.SuperAdmin.UpdateTenant
 {
     public class UpdateTenantInputData : IInputData<UpdateTenantOutputData>
     {
+        public UpdateTenantInputData(int tenantId, dynamic webSocketService, int type)
+        {
+            TenantId = tenantId;
+            WebSocketService = webSocketService;
+            File = type;
+        }
+
+        public int TenantId { get; private set; }
+
+        public dynamic WebSocketService { get; private set; }
+
+        public int File { get; private set; }
     }
 }
---
+++ UseCase/SuperAdmin/UpdateTenant/UpdateTenantStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase.SuperAdmin.UpdateTenant
+{
+    public enum UpdateTenantStatus : byte
+    {
+        Successed = 1,
+        InvalidTenantId = 2,
+        Failed = 3,
+        TenantDoesNotExist = 4,
+        TenantNotAvailable = 5,
+        TenantNotStoped = 6,
+    }
+}
