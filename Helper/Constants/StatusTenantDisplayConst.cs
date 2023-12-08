e1eedbec70f101b982eba4d51e34bb83424de1da
hoangnguyen11 <hoang.nguyen11@sotatek.com>
2023-12-08 16:09:46 +0700

    update map status tenant to status SRS

--- Helper/Constants/StatusTenantDisplayConst.cs
+++ Helper/Constants/StatusTenantDisplayConst.cs
@@ -7,12 +7,31 @@ public static class StatusTenantDisplayConst
     /// </summary>
     private static readonly Dictionary<byte, byte> statusTenantDisplayDictionnary = new()
     {
-        {2, 1}, {3, 1}, {5,1}, {6,1}, {8,1}, //pending
+            //{"available", 1},
+            //{"creating", 2},
+            //{"modifying", 3},
+            //{"deleting", 4},
+            //{"backing-up", 5},
+            //{"upgrading", 6},
+            //{"failed", 7},
+            //{"inaccessible-encryption-credentials",8},
+            //{"storage-full", 9},
+            //{"upgrade-failed", 10},
+            //{"terminating", 11},
+            //{"terminated", 12},
+            //{"terminate-failed", 13},
+            //{"stoped", 14},
+            //{"restoring", 15},
+            //{"restore-failed", 16},
+            //{"stopping", 17},
+            //{"starting", 18}
+
+        {2, 1}, {3, 1}, {5,1}, {8,1}, // pending
         {7, 2}, {10, 2}, {13,2}, {16,2}, //failded
         {1, 3}, {9,3}, //running
-        //stopping
-        {14, 5}, //stopped
-        {4, 6}, //sutting-down
+        {17 ,4}, {18,4}, {6,4}, {15, 4}, //stopping
+        {11, 5}, //stopped
+        {4, 6}, {11, 6}, //sutting-down
         {12, 7}, //teminated
     };
 
