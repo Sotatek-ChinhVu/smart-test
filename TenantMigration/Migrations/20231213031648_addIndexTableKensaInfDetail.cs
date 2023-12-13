37326371360b8d4861c8198138e9c0378ee726a3
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2023-12-13 10:23:50 +0700

    add index to table kensaInfDetail

---
+++ TenantMigration/Migrations/20231213031648_addIndexTableKensaInfDetail.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TenantMigration.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIndexTableKensaInfDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "KENSA_INF_DETAIL_PT_ID_IDX",
+                table: "KENSA_INF_DETAIL",
+                columns: new[] { "PT_ID", "IS_DELETED", "KENSA_ITEM_CD" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "KENSA_INF_DETAIL_PT_ID_IDX",
+                table: "KENSA_INF_DETAIL");
+        }
+    }
+}
