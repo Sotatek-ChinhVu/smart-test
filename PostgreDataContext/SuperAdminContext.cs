ecf33bbd9cc1129b94b2471aa8f627749f366364
DESKTOP-122RJ39\admin <minh.le@sotatek.com>
2023-12-26 15:07:43 +0700

    Migration entity ReleasenoteRead

--- PostgreDataContext/SuperAdminContext.cs
+++ PostgreDataContext/SuperAdminContext.cs
@@ -19,6 +19,7 @@ namespace PostgreDataContext
             modelBuilder.Entity<Tenant>().HasKey(a => new { a.TenantId });
             modelBuilder.Entity<UserToken>().HasKey(a => new { a.UserId, a.RefreshToken });
             modelBuilder.Entity<MigrationTenantHistory>().HasKey(a => new { a.Id });
+            modelBuilder.Entity<ReleasenoteRead>().HasKey(a => new { a.HpId, a.UserId, a.Version });
 
             modelBuilder.Entity<Admin>().HasIndex(a => new { a.LoginId }).HasFilter($"\"IS_DELETED\" = 0").IsUnique();
             modelBuilder.Entity<Tenant>().HasIndex(a => new { a.Hospital }).HasFilter($"\"IS_DELETED\" = 0").IsUnique();
@@ -39,5 +40,7 @@ namespace PostgreDataContext
         public DbSet<UserToken> UserTokens { get; set; } = default!;
 
         public DbSet<MigrationTenantHistory> MigrationTenantHistories { get; set; } = default!;
+
+        public DbSet<ReleasenoteRead> ReleasenoteReads { get; set; } = default!;
     }
 }
