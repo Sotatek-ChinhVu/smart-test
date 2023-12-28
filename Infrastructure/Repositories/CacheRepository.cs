7b13577f4a855473ee5e51bb00dc88751c402986
DESKTOP-122RJ39\admin <minh.le@sotatek.com>
2023-12-28 10:49:36 +0700

    clean code

--- Infrastructure/Repositories/CacheRepository.cs
+++ Infrastructure/Repositories/CacheRepository.cs
@@ -11,21 +11,9 @@ public class CacheRepository : RepositoryBase, IRemoveCacheRepository
 {
 
     private readonly IDatabase _cache;
-    private readonly IConfiguration _configuration;
     public CacheRepository(IConfiguration configuration, ITenantProvider tenantProvider) : base(tenantProvider)
     {
-        _configuration = configuration;
-        GetRedis();
         _cache = RedisConnectorHelper.Connection.GetDatabase();
-    }
-
-    public void GetRedis()
-    {
-        string connection = string.Concat(_configuration["Redis:RedisHost"], ":", _configuration["Redis:RedisPort"]);
-        if (RedisConnectorHelper.RedisHost != connection)
-        {
-            RedisConnectorHelper.RedisHost = connection;
-        }
     }
 
     public bool RemoveCache(string keyCache)
