2c52b1b898492b5c1e3835b45abf9fff3c94455d
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2024-01-03 11:23:22 +0700

    create api IsHokenInfInUsed

---
+++ EmrCloudApi/Requests/Diseases/IsHokenInfInUsedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EmrCloudApi.Requests.Diseases;
+
+public class IsHokenInfInUsedRequest
+{
+    public long PtId { get; set; }
+
+    public int HokenId { get; set; }
+}
---
+++ UseCase/Diseases/IsHokenInfInUsed/IsHokenInfInUsedInputData.cs
@@ -0,0 +1,19 @@
+﻿using UseCase.Core.Sync.Core;
+
+namespace UseCase.Diseases.IsHokenInfInUsed;
+
+public class IsHokenInfInUsedInputData : IInputData<IsHokenInfInUsedOutputData>
+{
+    public IsHokenInfInUsedInputData(int hpId, long ptId, int hokenId)
+    {
+        HpId = hpId;
+        PtId = ptId;
+        HokenId = hokenId;
+    }
+
+    public int HpId { get; private set; }
+
+    public long PtId { get; private set; }
+
+    public int HokenId { get; private set; }
+}
