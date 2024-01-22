533bf34f62b45c5cf2d12a12425a41d90af39f1e
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2024-01-22 15:19:42 +0700

    revert logic DeleteYousikiInf

--- Infrastructure/Repositories/YousikiRepository.cs
+++ Infrastructure/Repositories/YousikiRepository.cs
@@ -513,6 +513,32 @@ public class YousikiRepository : RepositoryBase, IYousikiRepository
                             .ToDictionary(kaMst => kaMst.YousikiKaCd.PadLeft(3, '0'), kaMst => kaMst.KaName);
     }
 
+    /// <summary>
+    /// Delete YousikiInf
+    /// </summary>
+    /// <param name="hpId"></param>
+    /// <param name="userId"></param>
+    /// <param name="sinYm"></param>
+    /// <param name="ptId"></param>
+    /// <param name="dataType"></param>
+    /// <returns></returns>
+    public bool DeleteYousikiInf(int hpId, int userId, int sinYm, long ptId, int dataType)
+    {
+        var yousikiInf = TrackingDataContext.Yousiki1Infs.FirstOrDefault(item => item.SinYm == sinYm
+                                                                                 && item.PtId == ptId
+                                                                                 && item.DataType == dataType
+                                                                                 && item.HpId == hpId
+                                                                                 && item.IsDeleted == 0);
+        if (yousikiInf != null)
+        {
+            yousikiInf.IsDeleted = 1;
+            yousikiInf.UpdateDate = CIUtil.GetJapanDateTimeNow();
+            yousikiInf.UpdateId = userId;
+        }
+        TrackingDataContext.SaveChanges();
+        return true;
+    }
+
     public void ReleaseResource()
     {
         DisposeDataContext();
