027c8fa9512b37aa0db4d125e4a9fe732ff21960
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2024-01-22 09:47:14 +0700

    update logic get yousiki detail tab

--- Domain/Models/Yousiki/IYousikiRepository.cs
+++ Domain/Models/Yousiki/IYousikiRepository.cs
@@ -8,6 +8,8 @@ public interface IYousikiRepository : IRepositoryBase
 
     List<Yousiki1InfDetailModel> GetYousiki1InfDetails(int hpId, int sinYm, long ptId, int dataType, int seqNo);
 
+    List<Yousiki1InfDetailModel> GetYousiki1InfDetails(int hpId, int sinYm, List<long> ptIdList);
+
     List<VisitingInfModel> GetVisitingInfs(int hpId, long ptId, int sinYm);
 
     List<Yousiki1InfModel> GetHistoryYousiki(int hpId, int sinYm, long ptId, int dataType);
