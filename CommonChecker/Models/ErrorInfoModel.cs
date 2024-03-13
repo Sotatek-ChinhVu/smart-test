a712019c7f964b7c1e945e8660d721562b0a5689
Sotatek-QuanTo <quan.to@sotatek.com>
2024-03-13 10:14:06 +0700

    clean code

--- CommonChecker/Models/ErrorInfoModel.cs
+++ CommonChecker/Models/ErrorInfoModel.cs
@@ -22,10 +22,6 @@
 
         public CommonCheckerType ErrorType { get; set; } = CommonCheckerType.None;
 
-        public double Weight { get; set; } = 0;
-
-        public double Height { get; set; } = 0;
-
         public List<LevelInfoModel> ListLevelInfo { get; set; } = new List<LevelInfoModel>();
 
     }
