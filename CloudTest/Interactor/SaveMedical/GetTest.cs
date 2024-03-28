a11b775bc47841f91e4348d4894cff38060d77ee
DESKTOP-122RJ39\admin <minh.le@sotatek.com>
2024-03-28 16:19:26 +0700

    [UT] Get(SaveMedicalInteractor)

---
+++ CloudTest/Interactor/SaveMedical/GetTest.cs
@@ -0,0 +1,109 @@
+﻿using Amazon.Runtime.Internal.Util;
+using Entity.Tenant;
+using Helper.Redis;
+using Infrastructure.Repositories.SpecialNote;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace CloudUnitTest.Interactor.SaveMedical
+{
+    public class GetTest : BaseUT
+    {
+
+        private readonly StackExchange.Redis.IDatabase _cache;
+
+        public GetTest()
+        {
+            string connection = string.Concat("10.2.15.78", ":", "6379");
+            if (RedisConnectorHelper.RedisHost != connection)
+            {
+                RedisConnectorHelper.RedisHost = connection;
+            }
+            _cache = RedisConnectorHelper.Connection.GetDatabase();
+        }
+
+        [Test]
+        public void TC_001_SaveMedicalInteractor_Test()
+        {
+            //Arrange
+            var mockIConfiguration = new Mock<IConfiguration>();
+            var summaryInfRepository = new SummaryInfRepository(TenantProvider, mockIConfiguration.Object);
+
+            var tenantTracking = TenantProvider.GetTrackingTenantDataContext();
+
+            long id = 9999999;
+            int hpid = 1;
+            long ptId = 28032001;
+            int seqNo = 99999999;
+            string text = "平成28年8月13日にて事故症状固定";
+
+            SummaryInf summaryInf = new SummaryInf()
+            {
+                Id = id,
+                HpId = hpid,
+                PtId = ptId,
+                SeqNo = seqNo,
+                Text = text
+            };
+
+            tenantTracking.SummaryInfs.Add(summaryInf);
+            tenantTracking.SaveChanges();
+
+            string finalKey = "SummaryInfGetList_1_28032001";
+            _cache.StringAppend(finalKey, string.Empty);
+
+            var result = summaryInfRepository.Get(hpid, ptId);
+            try
+            {
+                Assert.That(result.Id == 9999999);
+            }
+            finally
+            {
+                summaryInfRepository.ReleaseResource();
+                _cache.KeyDelete(finalKey);
+                tenantTracking.SummaryInfs.Remove(summaryInf);
+                tenantTracking.SaveChanges();
+            }
+        }
+
+        [Test]
+        public void TC_002_SaveMedicalInteractor_Test()
+        {
+            //Arrange
+            var mockIConfiguration = new Mock<IConfiguration>();
+            var summaryInfRepository = new SummaryInfRepository(TenantProvider, mockIConfiguration.Object);
+
+            var tenantTracking = TenantProvider.GetTrackingTenantDataContext();
+
+            long id = 9999999;
+            int hpid = 1;
+            long ptId = 28032001;
+            int seqNo = 99999999;
+            string text = "平成28年8月13日にて事故症状固定";
+
+            SummaryInf summaryInf = new SummaryInf()
+            {
+                Id = id,
+                HpId = hpid,
+                PtId = ptId,
+                SeqNo = seqNo,
+                Text = text
+            };
+
+            tenantTracking.SummaryInfs.Add(summaryInf);
+            tenantTracking.SaveChanges();
+
+            var result = summaryInfRepository.Get(hpid, ptId);
+            try
+            {
+                Assert.That(result.Id == 9999999);
+            }
+            finally
+            {
+                summaryInfRepository.ReleaseResource();
+                tenantTracking.SummaryInfs.Remove(summaryInf);
+                tenantTracking.SaveChanges();
+            }
+        }
+    }
+}
