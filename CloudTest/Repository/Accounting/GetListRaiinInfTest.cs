8f35aa89dccd1cf3ff83604bb502bab53f1ec017
thaitc <thaitc@gitlab.com>
2024-04-05 17:46:49 +0700

    unit test GetListRaiinInf AccountingRepo

---
+++ CloudTest/Repository/Accounting/GetListRaiinInfTest.cs
@@ -0,0 +1,111 @@
+﻿using CloudUnitTest.SampleData.AccountingRepository;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudUnitTest.Repository.Accounting
+{
+    public class GetListRaiinInfTest : BaseUT
+    {
+        [Test]
+        public void GetListRaiinInf_001()
+        {
+            SetupTestEnvironment(out AccountingRepository accountingRepository);
+            try
+            {
+                int hpId = 1; long ptId = 1; int sinDate = 20230303; long raiinNo = -1; bool isGetHeader = false; bool getAll = true;
+                var result = accountingRepository.GetListRaiinInf(hpId, ptId, sinDate, raiinNo, isGetHeader, getAll);
+                Assert.True(result.Count == 0);
+            }
+            finally
+            {
+                CleanupResources(accountingRepository);
+            }
+        }
+
+        [Test]
+        public void GetListRaiinInf_002()
+        {
+            SetupTestEnvironment(out AccountingRepository accountingRepository);
+            var tenant = TenantProvider.GetNoTrackingDataContext();
+            int hpId = 998; long ptId = 12345; int sinDate = 20180807; long raiinNo = 1234321; bool isGetHeader = false; bool getAll = true;
+            var raiinInfs = AccountingRepositoryData.ReadRaiinInf();
+            tenant.AddRange(raiinInfs);
+            try
+            {
+                tenant.SaveChanges();
+                var result = accountingRepository.GetListRaiinInf(hpId, ptId, sinDate, raiinNo, isGetHeader, getAll);
+                Assert.True(result.Count == 1);
+            }
+            finally
+            {
+                tenant.RemoveRange(raiinInfs);
+                tenant.SaveChanges();
+                CleanupResources(accountingRepository);
+            }
+        }
+
+        [Test]
+        public void GetListRaiinInf_003()
+        {
+            SetupTestEnvironment(out AccountingRepository accountingRepository);
+            var tenant = TenantProvider.GetNoTrackingDataContext();
+            int hpId = 998; long ptId = 12345; int sinDate = 20180807; long raiinNo = 1234321; bool isGetHeader = true; bool getAll = true;
+            var raiinInfs = AccountingRepositoryData.ReadRaiinInf();
+            tenant.AddRange(raiinInfs);
+            try
+            {
+                tenant.SaveChanges();
+                var result = accountingRepository.GetListRaiinInf(hpId, ptId, sinDate, raiinNo, isGetHeader, getAll);
+                Assert.True(result.Count == 1);
+            }
+            finally
+            {
+                tenant.RemoveRange(raiinInfs);
+                tenant.SaveChanges();
+                CleanupResources(accountingRepository);
+            }
+        }
+
+        [Test]
+        public void GetListRaiinInf_004()
+        {
+            SetupTestEnvironment(out AccountingRepository accountingRepository);
+            var tenant = TenantProvider.GetNoTrackingDataContext();
+            int hpId = 998; long ptId = 12345; int sinDate = 20180807; long raiinNo = 1234321; bool isGetHeader = true; bool getAll = true;
+            var raiinInfs = AccountingRepositoryData.ReadRaiinInf();
+            var raiinInfs2 = AccountingRepositoryData.ReadRaiinInf();
+            tenant.AddRange(raiinInfs);
+            try
+            {
+                tenant.SaveChanges();
+                var result = accountingRepository.GetListRaiinInf(hpId, ptId, sinDate, raiinNo, isGetHeader, getAll);
+                Assert.True(result.Count == 2 && result.Select(i=>i.PersonNumber).Contains(0));
+            }
+            finally
+            {
+                tenant.RemoveRange(raiinInfs);
+                tenant.SaveChanges();
+                CleanupResources(accountingRepository);
+            }
+        }
+
+        private void SetupTestEnvironment(out AccountingRepository accountingRepository)
+        {
+            var mockConfiguration = new Mock<IConfiguration>();
+            mockConfiguration.SetupGet(x => x[It.Is<string>(s => s == "Redis:RedisHost")]).Returns("10.2.15.78");
+            mockConfiguration.SetupGet(x => x[It.Is<string>(s => s == "Redis:RedisPort")]).Returns("6379");
+            accountingRepository = new AccountingRepository(TenantProvider, mockConfiguration.Object);
+        }
+        private void CleanupResources(AccountingRepository accountingRepository)
+        {
+            accountingRepository.ReleaseResource();
+        }
+    }
+}
---
+++ CloudTest/SampleData/AccountingRepository/AccountingRepositoryData.cs
@@ -0,0 +1,107 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using Entity.Tenant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudUnitTest.SampleData.AccountingRepository
+{
+    public class AccountingRepositoryData
+    {
+        public static List<RaiinInf> ReadRaiinInf()
+        {
+            var rootPath = Environment.CurrentDirectory;
+            rootPath = rootPath.Remove(rootPath.IndexOf("bin"));
+
+            string fileName = Path.Combine(rootPath, "SampleData", "AccountingRepository", "AccountingRepositorySample.xlsx");
+            var raiinInfs = new List<RaiinInf>();
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+                var workbookPart = spreadsheetDocument.WorkbookPart;
+                var sheetData = GetworksheetBySheetName(spreadsheetDocument, "RaiinInf").WorksheetPart?.Worksheet.Elements<SheetData>().First();
+                string text;
+                if (sheetData != null)
+                {
+                    foreach (var r in sheetData.Elements<Row>().Skip(1))
+                    {
+                        var raiinInf = new RaiinInf();
+                        raiinInf.CreateId = 1;
+                        raiinInf.CreateDate = DateTime.UtcNow;
+                        raiinInf.UpdateId = 1;
+                        raiinInf.UpdateDate = DateTime.UtcNow;
+                        foreach (var c in r.Elements<Cell>())
+                        {
+                            text = c.CellValue?.Text ?? string.Empty;
+                            if (c.DataType != null && c.DataType == CellValues.SharedString)
+                            {
+                                var stringId = Convert.ToInt32(c.InnerText);
+                                text = workbookPart?.SharedStringTablePart?.SharedStringTable.Elements<SharedStringItem>().ElementAt(stringId).InnerText ?? string.Empty;
+                            }
+                            var columnName = GetColumnName(c.CellReference?.ToString() ?? string.Empty);
+                            switch (columnName)
+                            {
+                                case "A":
+                                    int.TryParse(text, out int hpId);
+                                    raiinInf.HpId = hpId;
+                                    break;
+                                case "B":
+                                    int.TryParse(text, out int raiinNo);
+                                    raiinInf.RaiinNo = raiinNo;
+                                    break;
+                                case "C":
+                                    long.TryParse(text, out long ptId);
+                                    raiinInf.PtId = ptId;
+                                    break;
+                                case "D":
+                                    int.TryParse(text, out int sinDate);
+                                    raiinInf.SinDate = sinDate;
+                                    break;
+                                case "E":
+                                    long.TryParse(text, out long oyaRaiinNo);
+                                    raiinInf.OyaRaiinNo = oyaRaiinNo;
+                                    break;
+                                case "F":
+                                    int.TryParse(text, out int status);
+                                    raiinInf.Status = status;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        raiinInfs.Add(raiinInf);
+                    }
+                }
+            }
+
+            return raiinInfs;
+        }
+
+        private static Worksheet GetworksheetBySheetName(SpreadsheetDocument spreadsheetDocument, string sheetName)
+        {
+
+            var workbookPart = spreadsheetDocument.WorkbookPart;
+            StringValue relationshipId = workbookPart?.Workbook.Descendants<Sheet>().FirstOrDefault(s => s.Name != null && s.Name.Equals(sheetName))?.Id ?? string.Empty;
+
+            var worksheet = (workbookPart != null && !string.IsNullOrEmpty(relationshipId.ToString())) ? ((WorksheetPart)workbookPart.GetPartById(relationshipId?.Value ?? string.Empty)).Worksheet : new();
+
+            return worksheet;
+        }
+
+        private static string GetColumnName(string text)
+        {
+            var check = int.TryParse(text.Skip(1).FirstOrDefault().ToString(), out int number);
+            if (check)
+            {
+                return text.FirstOrDefault().ToString();
+            }
+            else
+            {
+                return text.Substring(0, 2);
+            }
+        }
+    }
+}
