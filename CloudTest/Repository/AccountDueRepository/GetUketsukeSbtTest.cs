d31abe48c4e89ab028760705ddda2445ca3e1ab7
DESKTOP-122RJ39\admin <minh.le@sotatek.com>
2024-04-08 13:49:09 +0700

    [UT] GetUketsukeSbt(AccountDueRepository)

---
+++ CloudTest/Repository/AccountDueRepository/GetUketsukeSbtTest.cs
@@ -0,0 +1,45 @@
+﻿using Entity.Tenant;
+using Infrastructure.Repositories;
+using System;
+
+namespace CloudUnitTest.Repository.AccountDueRepo
+{
+    public class GetUketsukeSbtTest : BaseUT
+    {
+        [Test]
+        public void TC_001_NextOrderRepository_GetUketsukeSbt()
+        {
+            //Arrange
+            AccountDueRepository accountDueRepository = new AccountDueRepository(TenantProvider);
+            var tenantTracking = TenantProvider.GetTrackingTenantDataContext();
+            Random random = new Random();
+            int hpId = random.Next(999, 99999);
+            int kbnId = random.Next(999, 99999);
+
+            UketukeSbtMst uketukeSbtMst = new UketukeSbtMst()
+            {
+                HpId = hpId,
+                KbnId = kbnId,
+                IsDeleted = 0
+            };
+
+            tenantTracking.Add(uketukeSbtMst);
+
+            try
+            {
+                // Act
+                tenantTracking.SaveChanges();
+                var result = accountDueRepository.GetUketsukeSbt(hpId);
+
+                // Assert
+                Assert.That(result.Count() > 0 && result.Any(x => x.Key == kbnId));
+            }
+            finally
+            {
+                accountDueRepository.ReleaseResource();
+                tenantTracking.Remove(uketukeSbtMst);
+                tenantTracking.SaveChanges();
+            }
+        }
+    }
+}
