dcc0f3c60c6784691458537364c2ac8c2f37841d
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2024-04-10 13:49:51 +0700

    create api SaveDetailDefault

---
+++ EmrCloudApi/Requests/Yousiki/SaveDetailDefaultRequest.cs
@@ -0,0 +1,21 @@
+﻿using EmrCloudApi.Requests.Yousiki.RequestItem;
+
+namespace EmrCloudApi.Requests.Yousiki;
+
+public class SaveDetailDefaultRequest
+{
+    /// <summary>
+    /// 1 - BarthelIndexList
+    /// 2 - FIMList
+    /// 3 - Yousiki1InfDetailList
+    /// </summary>
+    public int Mode { get; set; }
+
+    public int DataType { get; set; }
+
+    public List<PatientStatusRequest> BarthelIndexList { get; set; } = new();
+
+    public List<PatientStatusRequest> FIMList { get; set; } = new();
+
+    public List<Yousiki1InfDetailRequest> Yousiki1InfDetailList { get; set; } = new();
+}
