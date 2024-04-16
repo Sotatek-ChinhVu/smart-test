b5325c00c3f851f664cd42b4474f4f4fcfe61962
Sotatek-AnhVu3 <anh.vu3@sotatek.com>
2024-04-16 08:22:16 +0700

    update logic SaveReceCmtList

--- CloudTest/Repository/ReceCheck/ReceiptRepositoryTest.cs
+++ CloudTest/Repository/ReceCheck/ReceiptRepositoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.CalculationInf;
 using Domain.Models.MstItem;
+using Domain.Models.Receipt;
 using Entity.Tenant;
 using Infrastructure.Repositories;
 using Microsoft.Extensions.Configuration;
@@ -108,6 +109,360 @@ public class ReceiptRepositoryTest : BaseUT
         }
     }
     #endregion GetReceCmtList
+
+    #region SaveReceCmtList
+    [Test]
+    public void TC_002_SaveReceCmtList_TestSuccess()
+    {
+        // Arrange
+        SetupTestEnvironment(out ReceiptRepository receiptRepository, out TenantNoTrackingDataContext tenant);
+
+        Random random = new();
+        int hpId = random.Next(999, 999999);
+        int userId = random.Next(999, 999999);
+        long id = 0;
+        long ptId = random.Next(9999, 999999999);
+        int seqNo = random.Next(9999, 999999999);
+        int hokenId = random.Next(9999, 999999999);
+        int cmtKbn = random.Next(9999, 999999999);
+        int cmtSbt = random.Next(9999, 999999999);
+        int cmtCol1 = random.Next(9999, 999999999);
+        int cmtCol2 = random.Next(9999, 999999999);
+        int cmtCol3 = random.Next(9999, 999999999);
+        int cmtCol4 = random.Next(9999, 999999999);
+        int cmtColKeta1 = random.Next(9999, 999999999);
+        int cmtColKeta2 = random.Next(9999, 999999999);
+        int cmtColKeta3 = random.Next(9999, 999999999);
+        int cmtColKeta4 = random.Next(9999, 999999999);
+        int sinYm = 202202;
+        string cmt = "Cmt";
+        string cmtData = "CmtData";
+        string itemCd = "ItemCdUT";
+        ReceCmtModel receCmtModel = new ReceCmtModel(id, ptId, seqNo, sinYm, hokenId, cmtKbn, cmtSbt, cmt, cmtData, itemCd, cmtCol1, cmtCol2, cmtCol3, cmtCol4, cmtColKeta1, cmtColKeta2, cmtColKeta3, cmtColKeta4, false);
+        ReceCmt? receCmt = null;
+
+        try
+        {
+            // Act
+            var result = receiptRepository.SaveReceCmtList(hpId, userId, new List<ReceCmtModel>() { receCmtModel });
+
+            receCmt = tenant.ReceCmts.FirstOrDefault(item => item.PtId == ptId
+                                                             && item.HpId == hpId
+                                                             && item.SinYm == sinYm
+                                                             && item.HokenId == hokenId
+                                                             && item.CmtKbn == cmtKbn
+                                                             && item.CmtSbt == cmtSbt
+                                                             && item.Cmt == cmt
+                                                             && item.CmtData == cmtData
+                                                             && item.ItemCd == itemCd);
+            // Assert
+            Assert.IsTrue(result && receCmt != null);
+        }
+        finally
+        {
+            if (receCmt != null)
+            {
+                tenant.ReceCmts.Remove(receCmt);
+                tenant.SaveChanges();
+            }
+        }
+    }
+
+    [Test]
+    public void TC_003_SaveReceCmtList_TestReceCmtListEmpty()
+    {
+        // Arrange
+        SetupTestEnvironment(out ReceiptRepository receiptRepository, out TenantNoTrackingDataContext tenant);
+
+        Random random = new();
+        int hpId = random.Next(999, 999999);
+        int userId = random.Next(999, 999999);
+        long id = random.Next(9999, 999999999);
+        long ptId = random.Next(9999, 999999999);
+        int seqNo = random.Next(9999, 999999999);
+        int hokenId = random.Next(9999, 999999999);
+        int cmtKbn = random.Next(9999, 999999999);
+        int cmtSbt = random.Next(9999, 999999999);
+        int sinYm = 202202;
+        string cmt = "Cmt";
+        string cmtData = "CmtData";
+        string itemCd = "ItemCdUT";
+        ReceCmt? receCmt = null;
+
+        try
+        {
+            // Act
+            var result = receiptRepository.SaveReceCmtList(hpId, userId, new());
+
+            receCmt = tenant.ReceCmts.FirstOrDefault(item => item.PtId == ptId
+                                                             && item.HpId == hpId
+                                                             && item.SinYm == sinYm
+                                                             && item.HokenId == hokenId
+                                                             && item.CmtKbn == cmtKbn
+                                                             && item.CmtSbt == cmtSbt
+                                                             && item.Cmt == cmt
+                                                             && item.CmtData == cmtData
+                                                             && item.ItemCd == itemCd);
+            // Assert
+            Assert.IsTrue(result && receCmt == null);
+        }
+        finally
+        {
+            if (receCmt != null)
+            {
+                tenant.ReceCmts.Remove(receCmt);
+                tenant.SaveChanges();
+            }
+        }
+    }
+
+    [Test]
+    public void TC_004_SaveReceCmtListAction_TestAddNew()
+    {
+        // Arrange
+        SetupTestEnvironment(out ReceiptRepository receiptRepository, out TenantNoTrackingDataContext tenant);
+
+        Random random = new();
+        int hpId = random.Next(999, 999999);
+        int userId = random.Next(999, 999999);
+        long id = 0;
+        long ptId = random.Next(9999, 999999999);
+        int seqNo = random.Next(9999, 999999999);
+        int hokenId = random.Next(9999, 999999999);
+        int cmtKbn = random.Next(9999, 999999999);
+        int cmtSbt = random.Next(9999, 999999999);
+        int cmtCol1 = random.Next(9999, 999999999);
+        int cmtCol2 = random.Next(9999, 999999999);
+        int cmtCol3 = random.Next(9999, 999999999);
+        int cmtCol4 = random.Next(9999, 999999999);
+        int cmtColKeta1 = random.Next(9999, 999999999);
+        int cmtColKeta2 = random.Next(9999, 999999999);
+        int cmtColKeta3 = random.Next(9999, 999999999);
+        int cmtColKeta4 = random.Next(9999, 999999999);
+        int sinYm = 202202;
+        string cmt = "Cmt";
+        string cmtData = "CmtData";
+        string itemCd = "ItemCdUT";
+        ReceCmtModel receCmtModel = new ReceCmtModel(id, ptId, seqNo, sinYm, hokenId, cmtKbn, cmtSbt, cmt, cmtData, itemCd, cmtCol1, cmtCol2, cmtCol3, cmtCol4, cmtColKeta1, cmtColKeta2, cmtColKeta3, cmtColKeta4, false);
+        ReceCmt? receCmt = null;
+
+        try
+        {
+            // Act
+            var result = receiptRepository.SaveReceCmtList(hpId, userId, new List<ReceCmtModel>() { receCmtModel });
+
+            receCmt = tenant.ReceCmts.FirstOrDefault(item => item.PtId == ptId
+                                                             && item.HpId == hpId
+                                                             && item.SinYm == sinYm
+                                                             && item.HokenId == hokenId
+                                                             && item.CmtKbn == cmtKbn
+                                                             && item.CmtSbt == cmtSbt
+                                                             && item.Cmt == cmt
+                                                             && item.CmtData == cmtData
+                                                             && item.ItemCd == itemCd);
+            // Assert
+            Assert.IsTrue(result && receCmt != null);
+        }
+        finally
+        {
+            if (receCmt != null)
+            {
+                tenant.ReceCmts.Remove(receCmt);
+                tenant.SaveChanges();
+            }
+        }
+    }
+
+    [Test]
+    public void TC_005_SaveReceCmtListAction_TestContinue()
+    {
+        // Arrange
+        SetupTestEnvironment(out ReceiptRepository receiptRepository, out TenantNoTrackingDataContext tenant);
+
+        Random random = new();
+        int hpId = random.Next(999, 999999);
+        int userId = random.Next(999, 999999);
+        long id = random.Next(9999, 999999999);
+        long ptId = random.Next(9999, 999999999);
+        int seqNo = random.Next(9999, 999999999);
+        int hokenId = random.Next(9999, 999999999);
+        int cmtKbn = random.Next(9999, 999999999);
+        int cmtSbt = random.Next(9999, 999999999);
+        int cmtCol1 = random.Next(9999, 999999999);
+        int cmtCol2 = random.Next(9999, 999999999);
+        int cmtCol3 = random.Next(9999, 999999999);
+        int cmtCol4 = random.Next(9999, 999999999);
+        int cmtColKeta1 = random.Next(9999, 999999999);
+        int cmtColKeta2 = random.Next(9999, 999999999);
+        int cmtColKeta3 = random.Next(9999, 999999999);
+        int cmtColKeta4 = random.Next(9999, 999999999);
+        int sinYm = 202202;
+        string cmt = "Cmt";
+        string cmtData = "CmtData";
+        string itemCd = "ItemCdUT";
+        ReceCmtModel receCmtModel = new ReceCmtModel(id, ptId, seqNo, sinYm, hokenId, cmtKbn, cmtSbt, cmt, cmtData, itemCd, cmtCol1, cmtCol2, cmtCol3, cmtCol4, cmtColKeta1, cmtColKeta2, cmtColKeta3, cmtColKeta4, false);
+        ReceCmt? receCmt = null;
+
+        try
+        {
+            // Act
+            var result = receiptRepository.SaveReceCmtList(hpId, userId, new List<ReceCmtModel>() { receCmtModel });
+
+            receCmt = tenant.ReceCmts.FirstOrDefault(item => item.PtId == ptId
+                                                             && item.HpId == hpId
+                                                             && item.SinYm == sinYm
+                                                             && item.HokenId == hokenId
+                                                             && item.CmtKbn == cmtKbn
+                                                             && item.CmtSbt == cmtSbt
+                                                             && item.Cmt == cmt
+                                                             && item.CmtData == cmtData
+                                                             && item.ItemCd == itemCd);
+            // Assert
+            Assert.IsTrue(result && receCmt == null);
+        }
+        finally
+        {
+            if (receCmt != null)
+            {
+                tenant.ReceCmts.Remove(receCmt);
+                tenant.SaveChanges();
+            }
+        }
+    }
+
+    [Test]
+    public void TC_006_SaveReceCmtListAction_TestUpdate()
+    {
+        // Arrange
+        SetupTestEnvironment(out ReceiptRepository receiptRepository, out TenantNoTrackingDataContext tenant);
+
+        Random random = new();
+        int hpId = random.Next(999, 999999);
+        int userId = random.Next(999, 999999);
+        long id = random.Next(9999, 999999999);
+        long ptId = random.Next(9999, 999999999);
+        int seqNo = random.Next(9999, 999999999);
+        int hokenId = random.Next(9999, 999999999);
+        int cmtKbn = random.Next(9999, 999999999);
+        int cmtSbt = random.Next(9999, 999999999);
+        int cmtCol1 = random.Next(9999, 999999999);
+        int cmtCol2 = random.Next(9999, 999999999);
+        int cmtCol3 = random.Next(9999, 999999999);
+        int cmtCol4 = random.Next(9999, 999999999);
+        int cmtColKeta1 = random.Next(9999, 999999999);
+        int cmtColKeta2 = random.Next(9999, 999999999);
+        int cmtColKeta3 = random.Next(9999, 999999999);
+        int cmtColKeta4 = random.Next(9999, 999999999);
+        int sinYm = 202202;
+        string cmt = "Cmt";
+        string cmtData = "CmtData";
+        string itemCd = "ItemCdUT";
+        ReceCmtModel receCmtModel = new ReceCmtModel(id, ptId, seqNo, sinYm, hokenId, cmtKbn, cmtSbt, cmt, cmtData, itemCd, cmtCol1, cmtCol2, cmtCol3, cmtCol4, cmtColKeta1, cmtColKeta2, cmtColKeta3, cmtColKeta4, false);
+        ReceCmt? receCmt = new()
+        {
+            Id = id,
+            HpId = hpId,
+            PtId = ptId,
+            SinYm = sinYm,
+            HokenId = hokenId,
+            IsDeleted = 0,
+            SeqNo = seqNo,
+        };
+
+        tenant.ReceCmts.Add(receCmt);
+        try
+        {
+            tenant.SaveChanges();
+
+            // Act
+            var result = receiptRepository.SaveReceCmtList(hpId, userId, new List<ReceCmtModel>() { receCmtModel });
+
+            var receCmtAfter = tenant.ReceCmts.FirstOrDefault(item => item.PtId == ptId
+                                                               && item.HpId == hpId
+                                                               && item.SinYm == sinYm
+                                                               && item.HokenId == hokenId
+                                                               && item.Cmt == cmt
+                                                               && item.CmtData == cmtData
+                                                               && item.ItemCd == itemCd);
+            // Assert
+            Assert.IsTrue(result && receCmtAfter != null);
+        }
+        finally
+        {
+            if (receCmt != null)
+            {
+                tenant.ReceCmts.Remove(receCmt);
+                tenant.SaveChanges();
+            }
+        }
+    }
+
+    [Test]
+    public void TC_007_SaveReceCmtListAction_TestDeleted()
+    {
+        // Arrange
+        SetupTestEnvironment(out ReceiptRepository receiptRepository, out TenantNoTrackingDataContext tenant);
+
+        Random random = new();
+        int hpId = random.Next(999, 999999);
+        int userId = random.Next(999, 999999);
+        long id = random.Next(9999, 999999999);
+        long ptId = random.Next(9999, 999999999);
+        int seqNo = random.Next(9999, 999999999);
+        int hokenId = random.Next(9999, 999999999);
+        int cmtKbn = random.Next(9999, 999999999);
+        int cmtSbt = random.Next(9999, 999999999);
+        int cmtCol1 = random.Next(9999, 999999999);
+        int cmtCol2 = random.Next(9999, 999999999);
+        int cmtCol3 = random.Next(9999, 999999999);
+        int cmtCol4 = random.Next(9999, 999999999);
+        int cmtColKeta1 = random.Next(9999, 999999999);
+        int cmtColKeta2 = random.Next(9999, 999999999);
+        int cmtColKeta3 = random.Next(9999, 999999999);
+        int cmtColKeta4 = random.Next(9999, 999999999);
+        int sinYm = 202202;
+        string cmt = "Cmt";
+        string cmtData = "CmtData";
+        string itemCd = "ItemCdUT";
+        ReceCmtModel receCmtModel = new ReceCmtModel(id, ptId, seqNo, sinYm, hokenId, cmtKbn, cmtSbt, cmt, cmtData, itemCd, cmtCol1, cmtCol2, cmtCol3, cmtCol4, cmtColKeta1, cmtColKeta2, cmtColKeta3, cmtColKeta4, true);
+        ReceCmt? receCmt = new()
+        {
+            Id = id,
+            HpId = hpId,
+            PtId = ptId,
+            SinYm = sinYm,
+            HokenId = hokenId,
+            IsDeleted = 0,
+            SeqNo = seqNo,
+        };
+
+        tenant.ReceCmts.Add(receCmt);
+        try
+        {
+            tenant.SaveChanges();
+
+            // Act
+            var result = receiptRepository.SaveReceCmtList(hpId, userId, new List<ReceCmtModel>() { receCmtModel });
+
+            var receCmtAfter = tenant.ReceCmts.FirstOrDefault(item => item.PtId == ptId
+                                                               && item.HpId == hpId
+                                                               && item.SinYm == sinYm
+                                                               && item.HokenId == hokenId
+                                                               && item.IsDeleted == 1);
+            // Assert
+            Assert.IsTrue(result && receCmtAfter != null);
+        }
+        finally
+        {
+            if (receCmt != null)
+            {
+                tenant.ReceCmts.Remove(receCmt);
+                tenant.SaveChanges();
+            }
+        }
+    }
+
+    #endregion SaveReceCmtList
 
     private void SetupTestEnvironment(out ReceiptRepository receiptRepository, out TenantNoTrackingDataContext tenant)
     {
